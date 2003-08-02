867a48ac4b54a02da15c6a1e125d4b75b72506a3
Linus Torvalds <torvalds@home.osdl.org>
2003-08-02 00:42:32 -0700

    Add a type checking validation test-case that shows some of the current brokenness in type generation and checking.
    Run
    
    	./check validation/type1.c
    
    to see these totally bogus error messages:
    
    	warning: validation/type1.c:23:15: incorrect type in initializer (different base types)
    	warning: validation/type1.c:23:15:   expected char const *s
    	warning: validation/type1.c:23:15:   got struct hello *arg
    
    This bug causes several bogus warnings in the kernel.

---
+++ validation/type1.c
@@ -0,0 +1,24 @@
+/*
+ * We get this wrong for some strange reason.
+ *
+ * When evaluating the argument to the inline
+ * function for the array, we don't properly
+ * demote the "char []" to a "char *", but instead
+ * we follow the dereference and get a "struct hello".
+ *
+ * Which makes no sense at all.
+ */
+
+static inline int deref(const char *s)
+{
+	return *s;
+}
+
+struct hello {
+	char array[10];
+};
+
+int test(struct hello *arg)
+{
+	return deref(arg->array);
+}
