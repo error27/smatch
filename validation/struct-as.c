7925d5203a63c8f99764b21f9bc05d505ccdd65d
Linus Torvalds <torvalds@ppc970.osdl.org>
2004-05-24 08:49:53 -0700

    Add a few test-cases that Al Viro noted.

---
+++ validation/struct-as.c
@@ -0,0 +1,16 @@
+/*
+ * Structure members should get the address
+ * space of their pointer.
+ */
+#define __user __attribute__((address_space(1)))
+
+struct hello {
+	int a;
+};
+
+extern int test(int __user *ip);
+
+int broken(struct hello __user *sp)
+{
+	test(&sp->a);
+}
