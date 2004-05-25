93c174b6a74ec82f268c9384ba01058d71f78e23
Linus Torvalds <torvalds@ppc970.osdl.org>
2004-05-25 15:01:24 -0700

    Add more "noderef" validation tests.

--- validation/noderef.c
+++ validation/noderef.c
@@ -4,6 +4,14 @@ void h(void)
 	char __A *p;
 	char __A * * q1;
 	char * __A * q2;
+	int __A *q;
+	int __A *r;
+	
 	q1 = &p;
-	q2 = &p;
+	q2 = &p;	/* This should complain */
+
+	r = &*q;
+	r = q;
+	r = &*(q+1);	/* This should NOT complain */
+	r = q+1;
 }
