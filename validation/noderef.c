2fceb58910900aaa073434537548c5df9e469e3b
Linus Torvalds <torvalds@ppc970.osdl.org>
2004-05-26 11:21:36 -0700

    More examples from Al showing us being lazy on types.
    This time we (again) have a bug where we optimize
    the case of a zero member offset, and we get the
    type wrong.

--- validation/noderef.c
+++ validation/noderef.c
@@ -1,9 +1,16 @@
 # define __A	__attribute__((noderef))
+
+struct x {
+	int a;
+	int b;
+};
+
 void h(void)
 {
 	char __A *p;
 	char __A * * q1;
 	char * __A * q2;
+	struct x __A *xp;
 	int __A *q;
 	int __A *r;
 	
@@ -14,4 +21,7 @@ void h(void)
 	r = q;
 	r = &*(q+1);	/* This should NOT complain */
 	r = q+1;
+
+	r = &xp->a;	/* This should NOT complain */
+	r = &xp->b;
 }
