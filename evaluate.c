e210837400544bd51fd759422f8f2a37c3b395da
Linus Torvalds <torvalds@ppc970.osdl.org>
2004-05-26 17:10:18 -0700

    Oops. When dereferencing something, we sure shouldn't take the new type from the old _node_.
    That old node goes away, and it's the _pointer_ that
    determines the new type. Well duh!

--- evaluate.c
+++ evaluate.c
@@ -871,16 +871,18 @@ static struct symbol *evaluate_dereference(struct expression *expr)
 	struct expression *op = expr->unop;
 	struct symbol *ctype = op->ctype, *sym;
 
-	sym = alloc_symbol(expr->pos, SYM_NODE);
-	sym->ctype = ctype->ctype;
-	if (ctype->type == SYM_NODE) {
+	/* Dereferencing a node drops all the node information. */
+	if (ctype->type == SYM_NODE)
 		ctype = ctype->ctype.base_type;
-		merge_type(sym, ctype);
-	}
+
 	if (ctype->type != SYM_PTR && ctype->type != SYM_ARRAY) {
 		warn(expr->pos, "cannot derefence this type");
 		return 0;
 	}
+
+	/* The new node gets the ctype information from the pointer */
+	sym = alloc_symbol(expr->pos, SYM_NODE);
+	sym->ctype = ctype->ctype;
 
 	ctype = ctype->ctype.base_type;
 	examine_symbol_type(ctype);
