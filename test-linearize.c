8b66cf7adc0b8b6beaad7822d1f9801cf2055ee0
Linus Torvalds <torvalds@ppc970.osdl.org>
2004-11-23 22:53:21 -0700

    Update test-linearize.c to the new world order.

--- test-linearize.c
+++ test-linearize.c
@@ -37,56 +37,6 @@ static void clean_up_symbols(struct symbol_list *list)
 
 int main(int argc, char **argv)
 {
-	int fd;
-	char *filename = NULL, **args;
-	struct token *token;
-	struct symbol_list *list;
-
-	// Initialize symbol stream first, so that we can add defines etc
-	init_symbols();
-
-	args = argv;
-	for (;;) {
-		char *arg = *++args;
-		if (!arg)
-			break;
-		if (arg[0] == '-') {
-			args = handle_switch(arg+1, args);
-			continue;
-		}
-		filename = arg;
-	}
-
-	// Initialize type system
-	init_ctype();
-
-	create_builtin_stream();
-	add_pre_buffer("#define __CHECKER__ 1\n");
-	if (!preprocess_only)
-		declare_builtin_functions();
-
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
-		die("No such file: %s", filename);
-
-	// Tokenize the input stream
-	token = tokenize(filename, fd, NULL, includepath);
-	close(fd);
-
-	// Prepend any "include" file to the stream.
-	if (include_fd >= 0)
-		token = tokenize(include, include_fd, token, includepath);
-
-	// Prepend the initial built-in stream
-	token = tokenize_buffer(pre_buffer, pre_buffer_size, token);
-
-	// Pre-process the stream
-	token = preprocess(token);
-
-	// Parse the resulting C code
-	list = translation_unit(token);
-
-	// Do type evaluation and simplify
-	clean_up_symbols(list);
+	clean_up_symbols(sparse(argc, argv));
 	return 0;
 }
