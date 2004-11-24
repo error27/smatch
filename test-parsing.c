9c70e36c9083eddbad153949885ebb5414bd7e3e
Linus Torvalds <torvalds@ppc970.osdl.org>
2004-11-23 22:55:32 -0700

    Clean up test-parsing too.

--- test-parsing.c
+++ test-parsing.c
@@ -33,47 +33,7 @@ static void clean_up_symbols(struct symbol_list *list)
 
 int main(int argc, char **argv)
 {
-	int fd;
-	char *filename = NULL, **args;
-	struct token *token;
-	struct symbol_list *list;
-
-	// Initialize symbol stream first, so that we can add defines etc
-	init_symbols();
-
-	create_builtin_stream();
-
-	args = argv;
-	for (;;) {
-		char *arg = *++args;
-		if (!arg)
-			break;
-		if (arg[0] == '-') {
-			args = handle_switch(arg + 1, args);
-			continue;
-		}
-		filename = arg;
-	}
-
-	// Initialize type system
-	init_ctype();
-
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
-		die("No such file: %s", filename);
-
-	// Tokenize the input stream
-	token = tokenize(filename, fd, NULL, includepath);
-	close(fd);
-
-	// Prepend the initial built-in stream
-	token = tokenize_buffer(pre_buffer, pre_buffer_size, token);
-
-	// Pre-process the stream
-	token = preprocess(token);
-
-	// Parse the resulting C code
-	list = translation_unit(token);
+	struct symbol_list *list = sparse(argc, argv);
 
 	// Simplification
 	clean_up_symbols(list);
