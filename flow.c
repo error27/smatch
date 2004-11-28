b6100a46cce67257245f18e0ef991a604f50c280
Linus Torvalds <torvalds@ppc970.osdl.org>
2004-11-28 14:40:38 -0700

    Remember to kill the use of the conditional if both arms do the same thing.

--- flow.c
+++ flow.c
@@ -676,6 +676,7 @@ static int rewrite_parent_branch(struct basic_block *bb, struct basic_block *old
 		if (insn->bb_true == insn->bb_false) {
 			remove_bb_from_list(&new->parents, bb, 1);
 			insn->bb_false = NULL;
+			kill_use(&insn->cond);
 		}
 		return 1;
 	case OP_SWITCH: {
