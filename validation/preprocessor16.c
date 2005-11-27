ccdfdab9f98500e49bba62b1d7d588a52a37c6ed
Al Viro <viro@zeniv.linux.org.uk>
2005-11-27 02:33:27 -0500

    [PATCH] Doh...
    Adding description to testcase is a good thing, but doing that without
    checking that testcase is still OK...  preprocess16.c was not - I've
    added description under #if 0 in there, but forgot to comment it out.
    Funny what a few ' do to tokenizer outside of comment...
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- validation/preprocessor16.c
+++ validation/preprocessor16.c
@@ -1,4 +1,5 @@
 #if 0
+/*
 From 6.10.1(5):
 	Each directive's condition is checked in order.  If it evaluates
 	to false (zero), the group it controls is skipped: directives are
@@ -11,6 +12,7 @@ In other words, bogus arguments of directives are silently ignored and
 so are text lines and non-directives (# <something unknown>).  We *do*
 complain about the things like double #else or #elif after #else, since
 they hit before we get to the level of groups.
+*/
 
 #define 1
 #undef 1
