970157685aabf9b8845319180c539ea58ed86539
Al Viro <viro@zeniv.linux.org.uk>
2006-05-09 09:05:42 -0400

    [PATCH] count_array_initializer() needs to know the type of element
    char *s[] = {"123456789"}; gives a single-element array
    char p[] = {"123456789"}; gives a ten-element one
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

---
+++ validation/init-char-array.c
@@ -0,0 +1,15 @@
+/*
+ * for array of char {<string>} gets special treatment in initializer.
+ */
+char *s[] = {"aaaaaaaaa"};
+char t[][10] = {"aaaaaaaaa"};
+char u[] = {"aaaaaaaaa"};
+char v[] = "aaaaaaaaa";
+void f(void)
+{
+	char x[1/(sizeof(s) == sizeof(char*))];
+	char y[1/(sizeof(u) == 10)];
+	char z[1/(sizeof(v) == 10)];
+	char w[1/(sizeof(t) == 10)];
+}
+
