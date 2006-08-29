6ab5eb55d19bb27798c6340215b3c0f039710a18
Josh Triplett <josh@freedesktop.org>
2006-08-29 16:21:29 -0700

    Add -Wno-uninitialized
    Add the ability to turn off warnings for possibly uninitialized variables
    using -Wno-uninitialized, helpful when attempting to track down a particular
    class of warnings only without losing them amongst others.  Warnings for
    possibly uninitialized variables default to on, so this does not change the
    default behavior of sparse.
    
    Signed-off-by: Josh Triplett <josh@freedesktop.org>

--- lib.h
+++ lib.h
@@ -84,6 +84,7 @@ extern int Wdecl;
 extern int Wone_bit_signed_bitfield;
 extern int Wshadow;
 extern int Wcast_truncate;
+extern int Wuninitialized;
 
 extern void declare_builtin_functions(void);
 extern void create_builtin_stream(void);
