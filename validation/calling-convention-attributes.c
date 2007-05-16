b5bd30ffc56a402ab8039eb0a9f38d0afb23efb2
Josh Triplett <josh@freedesktop.org>
2007-05-15 23:13:51 -0700

    Add test case for stdcall and cdecl attributes.
    Signed-off-by: Josh Triplett <josh@freedesktop.org>

---
+++ validation/calling-convention-attributes.c
@@ -0,0 +1,4 @@
+extern void __attribute__((cdecl)) f1(void);
+extern void __attribute__((__cdecl__)) f2(void);
+extern void __attribute__((stdcall)) f3(void);
+extern void __attribute__((__stdcall__)) f4(void);
