7efd55ea22b676c85beaefcae04c66b2be1911cc
Michael Stefaniuc <mstefani@redhat.com>
2007-05-16 13:12:42 +0200

    Add test for typedef on pointer to function with stdcall attribute.
    Signed-off-by: Michael Stefaniuc <mstefani@redhat.com>

--- validation/calling-convention-attributes.c
+++ validation/calling-convention-attributes.c
@@ -2,3 +2,6 @@ extern void __attribute__((cdecl)) f1(void);
 extern void __attribute__((__cdecl__)) f2(void);
 extern void __attribute__((stdcall)) f3(void);
 extern void __attribute__((__stdcall__)) f4(void);
+
+typedef void (__attribute__((__stdcall__)) *f5)(void);
+typedef f5 f5ptr;
