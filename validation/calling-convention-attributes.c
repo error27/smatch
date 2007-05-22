12e4c7936209670106213f71ad8d9269c3dbfeb9
Josh Triplett <josh@freedesktop.org>
2007-05-22 15:59:44 -0700

    Expand calling convention test case to cover fastcall
    Signed-off-by: Josh Triplett <josh@freedesktop.org>

--- validation/calling-convention-attributes.c
+++ validation/calling-convention-attributes.c
@@ -1,7 +1,23 @@
-extern void __attribute__((cdecl)) f1(void);
-extern void __attribute__((__cdecl__)) f2(void);
-extern void __attribute__((stdcall)) f3(void);
-extern void __attribute__((__stdcall__)) f4(void);
+extern void __attribute__((cdecl)) c1(void);
+typedef void (__attribute__((cdecl)) *c2)(void);
+typedef c2 c2ptr;
 
-typedef void (__attribute__((__stdcall__)) *f5)(void);
-typedef f5 f5ptr;
+extern void __attribute__((__cdecl__)) c_1(void);
+typedef void (__attribute__((__cdecl__)) *c_2)(void);
+typedef c_2 c_2ptr;
+
+extern void __attribute__((stdcall)) s1(void);
+typedef void (__attribute__((stdcall)) *s2)(void);
+typedef s2 s2ptr;
+
+extern void __attribute__((__stdcall__)) s_1(void);
+typedef void (__attribute__((__stdcall__)) *s_2)(void);
+typedef s_2 s_2ptr;
+
+extern void __attribute__((fastcall)) f1(void);
+typedef void (__attribute__((fastcall)) *f2)(void);
+typedef f2 f2ptr;
+
+extern void __attribute__((__fastcall__)) f_1(void);
+typedef void (__attribute__((__fastcall__)) *f_2)(void);
+typedef f_2 f_2ptr;
