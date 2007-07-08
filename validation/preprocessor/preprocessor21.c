6a5c659470d124add283878738285c506dba9441
Josh Triplett <josh@freedesktop.org>
2007-07-08 13:54:15 -0700

    Add test-suite comment to preprocessor21.
    Signed-off-by: Josh Triplett <josh@freedesktop.org>

--- validation/preprocessor/preprocessor21.c
+++ validation/preprocessor/preprocessor21.c
@@ -1,2 +1,16 @@
 #if 1
 #if
+/*
+ * check-name: Preprocessor #21
+ * check-description: This used to hang Sparse.
+ * check-command: sparse -E $file
+ * check-exit-value: 1
+ *
+ * check-output-start
+preprocessor/preprocessor21.c:2:2: error: unterminated preprocessor conditional
+
+
+ * check-output-end
+ *
+ * check-known-to-fail
+ */
