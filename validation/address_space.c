2c781a125d1bd52f17db77ea02aaf5001409bdc2
Josh Triplett <josh@freedesktop.org>
2007-07-08 13:57:34 -0700

    Add test-suite comment to address_space.c
    Signed-off-by: Josh Triplett <josh@freedesktop.org>

--- validation/address_space.c
+++ validation/address_space.c
@@ -6,3 +6,15 @@ static int sys_do_stuff(void __user *user_addr)
 {
 	return poke_memory(user_addr);
 }
+/*
+ * check-name: address_space attribute
+ *
+ * check-command: sparse $file
+ * check-exit-value: 0
+ *
+ * check-output-start
+address_space.c:7:21: warning: incorrect type in argument 1 (different address spaces)
+address_space.c:7:21:    expected void *addr
+address_space.c:7:21:    got void *user_addr<asn:1>
+ * check-output-end
+ */
