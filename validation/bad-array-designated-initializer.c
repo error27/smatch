d8d87ca8f0fad00dcfdfc0499886b292ad794648
Josh Triplett <josh@freedesktop.org>
2007-07-08 18:36:53 -0700

    Add test-suite comment to bad-array-designated-initializer.c
    Signed-off-by: Josh Triplett <josh@freedesktop.org>

--- validation/bad-array-designated-initializer.c
+++ validation/bad-array-designated-initializer.c
@@ -2,3 +2,16 @@ static int a[] = {
 	[0] = 0,		// OK
 	[\0] = 1,		// KO
 };
+/*
+ * check-name: Bad array designated initializer
+ * check-command: sparse $file
+ * check-exit-value: 1
+ *
+ * check-output-start
+bad-array-designated-initializer.c:3:3: error: Expected constant expression
+bad-array-designated-initializer.c:3:3: error: Expected } at end of initializer
+bad-array-designated-initializer.c:3:3: error: got \
+ * check-output-end
+ *
+ * check-known-to-fail
+ */
