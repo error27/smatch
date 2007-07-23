3259d842c73dbd08d1b9d062d770582477c4fccc
Josh Triplett <josh@freedesktop.org>
2007-07-22 20:29:31 -0700

    Add test-suite metadata to bad-cast.c
    Signed-off-by: Josh Triplett <josh@freedesktop.org>

--- validation/bad-cast.c
+++ validation/bad-cast.c
@@ -4,3 +4,12 @@ static int foo(int a)
 {
 	return (struct/st *) a;
 }
+/*
+ * check-name: Bad cast syntax
+ *
+ * check-error-start
+bad-cast.c:5:16: error: expected declaration
+bad-cast.c:5:16: error: Expected ) at end of cast operator
+bad-cast.c:5:16: error: got /
+ * check-error-end
+ */
