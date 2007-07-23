481fe8aea3670c9dc99e8424c6b726a2b6e6ebaa
Josh Triplett <josh@freedesktop.org>
2007-07-22 20:31:49 -0700

    Add test-suite metadata to bad-ternary-cond.c, and remove now-redundant comment
    Signed-off-by: Josh Triplett <josh@freedesktop.org>

--- validation/bad-ternary-cond.c
+++ validation/bad-ternary-cond.c
@@ -1,7 +1,12 @@
-// should warn: "warning: Expected : in conditional expression"
-// and should not segfault
-
 static int foo(int a)
 {
 	return a ?? 1 : 0;
 }
+/*
+ * check-name: Bad ternary syntax
+ * check-description: Once caused Sparse to segfault
+ * check-error-start
+bad-ternary-cond.c:3:12: error: Expected : in conditional expression
+bad-ternary-cond.c:3:12: error: got ?
+ * check-error-end
+ */
