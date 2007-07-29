1ffab3d2b572bf182b23747a649bc632c4f31edb
Josh Triplett <josh@freedesktop.org>
2007-07-29 00:43:06 -0700

    Remove explicit restatements of defaults in metadata for comma test
    validation/comma.c restated some defaults explicitly in its test metadata;
    remove the redundant metadata and rely on the defaults instead.
    
    Signed-off-by: Josh Triplett <josh@freedesktop.org>

--- validation/comma.c
+++ validation/comma.c
@@ -3,8 +3,4 @@ static char b[1/(sizeof(a) - sizeof(0,a))];
 /*
  * check-name: Comma and array decay
  * check-description: arguments of comma should degenerate
- * check-command: sparse $file
- *
- * check-output-start
- * check-output-end
  */
