8ec193a995c2cc4b16a2051ebe96f495d8a87e05
Josh Triplett <josh@kernel.org>
2007-08-02 08:55:27 -0700

    Add another test case to validation/comma.c
    Signed-off-by: Josh Triplett <josh@kernel.org>

--- validation/comma.c
+++ validation/comma.c
@@ -1,5 +1,11 @@
 static char a[sizeof(char *) + 1];
 static char b[1/(sizeof(a) - sizeof(0,a))];
+static void f(void)
+{
+        int c[42];
+        typeof((void)0,c) d;
+        d = c;
+}
 /*
  * check-name: Comma and array decay
  * check-description: arguments of comma should degenerate
