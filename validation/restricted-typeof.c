45c552a02342557d007ef86c885f9b5ac5f06f71
Josh Triplett <josh@freedesktop.org>
2007-09-15 00:50:08 -0700

    Add test-suite annotations to restricted-typeof.c
    Signed-off-by: Josh Triplett <josh@freedesktop.org>

--- validation/restricted-typeof.c
+++ validation/restricted-typeof.c
@@ -2,3 +2,7 @@ typedef unsigned __attribute__((bitwise)) A;
 static A x;
 static __typeof__(x) y;
 static A *p = &y;
+/*
+ * check-name: typeof with bitwise types
+ * check-command: sparse -Wbitwise $file
+ */
