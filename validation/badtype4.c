80e1b320d8644fe31b0f31d1ed705706e8e6af36
Josh Triplett <josh@freedesktop.org>
2007-09-15 00:59:58 -0700

    Add test-suite annotations to badtype4.c
    Signed-off-by: Josh Triplett <josh@freedesktop.org>

--- validation/badtype4.c
+++ validation/badtype4.c
@@ -5,3 +5,11 @@ void a(void)
 		break;
 	}
 }
+/*
+ * check-name: switch(bad_type) {...} segfault
+ *
+ * check-error-start
+badtype4.c:3:9: error: undefined identifier 'x'
+badtype4.c:4:7: error: incompatible types for 'case' statement
+ * check-error-end
+ */
