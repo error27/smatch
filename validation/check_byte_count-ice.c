559ae36944df98c2e13e4060d0a2309b24887acf
Josh Triplett <josh@freedesktop.org>
2007-09-15 00:55:12 -0700

    Add test-suite annotations to check_byte_count-ice.c
    Signed-off-by: Josh Triplett <josh@freedesktop.org>

--- validation/check_byte_count-ice.c
+++ validation/check_byte_count-ice.c
@@ -4,3 +4,11 @@ static void foo(void *a)
 {
 	memset(foo, + ', 20);
 }
+/*
+ * check-name: Segfault in check_byte_count after syntax error
+ *
+ * check-error-start
+check_byte_count-ice.c:5:18: error: Bad character constant
+check_byte_count-ice.c:5:8: error: not enough arguments for function memset
+ * check-error-end
+ */
