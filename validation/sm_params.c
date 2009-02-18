eaf1893b956af2faa937cefb6b9ffbd603e4dc04
Dan Carpenter <error27@gmail.com>
2009-02-18 11:59:35 +0300

    Test to see if we are catching cross function derefs.
    Signed-off-by: Dan Carpenter <error27@gmail.com>

---
+++ validation/sm_params.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+struct foo {
+	int a;
+};
+
+static void func (struct foo *aa)
+{
+	aa->a = 1;
+}
+
+void func1 (struct foo *ab)
+{
+	if (!ab)
+		printf("Error ad is not ALLOWED to be NULL\n");
+	ab->a = 1;
+}
+
+void bar (void)
+{
+	struct foo *ac = returns_nonnull(sizeof(*ac));
+	struct foo *ad;
+
+	if(1)
+		ac = 0;
+	func(ac);
+	func1(ad);
+	func(ad);
+}
+/*
+ * check-name: Cross function dereferences
+ * check-command: smatch sm_params.c
+ *
+ * check-output-start
+sm_params.c +16 unchecked param func1 0
+sm_params.c +26 cross_func deref func 0
+sm_params.c +27 cross_func deref func1 0
+sm_params.c +28 cross_func deref func 0
+ * check-output-end
+ */
