dcf3cad5028a12b083377e3cfb6db2c5576426ed
Dan Carpenter <error27@gmail.com>
2009-02-26 22:01:10 +0300

    Take a parameter instead of assuming the output is in warns.txt
    This script still sucks.
    
    Signed-off-by: Dan Carpenter <error27@gmail.com>

--- smatch_scripts/find_null_params.sh
+++ smatch_scripts/find_null_params.sh
@@ -1,10 +1,19 @@
-grep " unchecked " warns.txt | cut -d ' ' -f 5- | sort -u > unchecked
-grep " undefined " warns.txt | cut -d ' ' -f 5- | sort -u > null_calls.txt
+#!/bin/bash
+
+file=$1
+
+if [[ "$file" = "" ]] ; then
+    echo "Usage:  $0 <file with smatch messages>"
+    exit 1
+fi
+
+grep " unchecked " $file | cut -d ' ' -f 5- | sort -u > unchecked
+grep " undefined " $file | cut -d ' ' -f 5- | sort -u > null_calls.txt
 cat null_calls.txt unchecked | sort | uniq -d > null_params.txt
 IFS="
 "
 for i in $(cat null_params.txt) ; do
-	grep "$i" warns.txt | grep -w undefined 
-done | tee null_probs.txt
+	grep "$i" $file | grep -w undefined 
+done
 
 
