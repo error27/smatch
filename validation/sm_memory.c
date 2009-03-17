d4c7d9b63e6c31c9730926a1d78e2bb6c1232736
Dan Carpenter <error27@gmail.com>
2009-03-18 01:06:49 +0300

    check_memory:  add rudimentary validation test.
    Signed-off-by: Dan Carpenter <error27@gmail.com>

---
+++ validation/sm_memory.c
@@ -0,0 +1,31 @@
+struct ture {
+	int *a;
+};
+
+struct ture *a;
+struct ture *b;
+void func (void)
+{
+	struct ture *aa;
+	struct ture *ab;
+	struct ture *ac;
+	aa = kmalloc();
+	ab = kmalloc();
+	ac = kmalloc();
+
+	a = aa;
+	if (ab) {
+		free(ab);
+		return;
+	}
+	free(ac);
+	return;
+}
+/*
+ * check-name: leak test #1
+ * check-command: smatch sm_memory.c
+ *
+ * check-output-start
+sm_memory.c +19 func(12) error: memery leak of ac
+ * check-output-end
+ */
