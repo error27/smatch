fa75482ec2f548cb6d4c799497b49829fb81df5f
Dan Carpenter <error27@gmail.com>
2009-09-06 15:29:56 +0200

    Print an error for this:  if (a | b) { ...
    Doing a bitwise or inside a condition is often a silly thing.  Maybe
    you wanted a bitwise and or a regular or.
    
    Signed-off-by: Dan Carpenter <error27@gmail.com>

--- smatch.c
+++ smatch.c
@@ -39,6 +39,7 @@ void check_err_ptr_deref(int id);
 void check_balanced(int id);
 void check_initializer_deref(int id);
 void check_deref_check(int id);
+void check_bitwise_or(int id);
 /* <- your test goes here */
 /* void register_template(int id); */
 
@@ -61,6 +62,7 @@ static const reg_func reg_funcs[] = {
 	&check_balanced,
 	&check_initializer_deref,
 	&check_deref_check,
+	&check_bitwise_or,
 
 	/* <- your test goes here */
 	/* &register_template, */
