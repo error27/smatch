be3d2dc81c524ce162e746c286f139ad1b8c5bd5
Dan Carpenter <error27@gmail.com>
2010-01-10 11:45:57 +0300

    foo(&bar) means bar is modified.
    It doesn't necessarily mean that of course, but it makes sense to assume
    it does.  At least until the inter-function stuff is working.
    
    Signed-off-by: Dan Carpenter <error27@gmail.com>

--- smatch_modification_hooks.c
+++ smatch_modification_hooks.c
@@ -131,6 +131,28 @@ free:
 	free_string(name);
 }
 
+static void match_call(struct expression *expr)
+{
+	struct mod_cb_list *call_backs;
+	struct expression *arg;
+	struct symbol *sym;
+	char *name;
+
+	FOR_EACH_PTR(expr->args, arg) {
+		arg = strip_expr(arg);
+		if (arg->type != EXPR_PREOP || arg->op != '&')
+			continue;
+		arg = strip_expr(arg->unop);
+		name = get_variable_from_expr(arg, &sym);
+		if (!name)
+			continue;
+		call_backs = get_mcall_backs(name);
+		if (call_backs)
+			call_call_backs(call_backs, name, sym, expr);
+		free_string(name);
+	} END_FOR_EACH_PTR(arg);
+}
+
 static void match_end_func(struct symbol *sym)
 {
 	hdestroy_r(&var_hash);
@@ -142,6 +164,7 @@ void register_modification_hooks(int id)
 	hcreate_r(1000, &var_hash);
 	add_hook(&match_assign, ASSIGNMENT_HOOK);
 	add_hook(&unop_expr, OP_HOOK);
+	add_hook(&match_call, FUNCTION_CALL_HOOK);
 	add_hook(&match_end_func, END_FUNC_HOOK);
 }
 
