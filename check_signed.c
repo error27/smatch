95c874136b7ebf5e3412086e65bc1426e3db6803
Dan Carpenter <error27@gmail.com>
2010-01-15 07:47:03 +0300

    eliminate all the "x can't fit into 0" false positives
    Signed-off-by: Dan Carpenter <error27@gmail.com>

--- check_signed.c
+++ check_signed.c
@@ -58,7 +58,7 @@ static void match_assign(struct expression *expr)
 	if (!get_implied_value(expr->right, &val))
 		return;
 	max = max_size(sym);
-	if (max < val) {
+	if (max && max < val) {
 		name = get_variable_from_expr_complex(expr->left, NULL);
 		sm_msg("error: value %lld can't fit into %lld %s", val, max, name);
 		free_string(name);
