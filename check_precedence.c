5d181725bfcaf823e6cb4251861db767451ca729
Dan Carpenter <error27@gmail.com>
2010-01-16 23:48:17 +0300

    precedence:  make test stricter
    The test now warns if you do:  if (!x == y) {...
    Changed the output format:  warning => warn
    
    Signed-off-by: Dan Carpenter <error27@gmail.com>

--- check_precedence.c
+++ check_precedence.c
@@ -1,7 +1,7 @@
 /*
  * sparse/check_precedence.c
  *
- * Copyright (C) 20XX Your Name.
+ * Copyright (C) 2010 Dan Carpenter.
  *
  * Licensed under the Open Software License version 1.1
  *
@@ -11,13 +11,43 @@
 
 static int my_id;
 
+static int is_bool(struct expression *expr)
+{
+	struct symbol *type;
+
+	type = get_type(expr);
+	if (!type)
+		return 0;
+	if (type->bit_size == 1 && type->ctype.modifiers & MOD_UNSIGNED)
+		return 1;
+	return 0;
+}
+
 static void match_condition(struct expression *expr)
 {
-	if (expr->type != EXPR_BINOP)
-		return;
-	if (expr->left->type == EXPR_COMPARE || 
-		expr->right->type == EXPR_COMPARE)
-		sm_msg("warning: do you want parens here?");
+	int print = 0;
+
+	if (expr->type == EXPR_COMPARE) {
+		if (expr->left->type == EXPR_COMPARE || expr->right->type == EXPR_COMPARE)
+			print = 1;
+		if (expr->left->op == '!') {
+			if (expr->left->type == EXPR_PREOP && expr->left->unop->op == '!')
+				return;
+			if (expr->right->op == '!')
+				return;
+			if (is_bool(expr->right))
+				return;
+			print = 1;
+		}
+	}
+
+	if (expr->type == EXPR_BINOP) {
+		if (expr->left->type == EXPR_COMPARE || expr->right->type == EXPR_COMPARE)
+			print = 1;
+	}
+
+	if (print)
+		sm_msg("warn: add some parenthesis here?");
 }
 
 static void match_binop(struct expression *expr)
@@ -25,9 +55,8 @@ static void match_binop(struct expression *expr)
 	if (expr->op != '&')
 		return;
 	if (expr->left->op == '!')
-		sm_msg("warning: you probably want parens here.");
+		sm_msg("warn: add some parenthesis here?");
 }
-
 
 void check_precedence(int id)
 {
--- validation/sm_precedence.c
+++ validation/sm_precedence.c
@@ -1,21 +1,43 @@
-unsigned int x, y;
+struct foo {
+	unsigned int x:1;
+};
 
-void func (void)
+int frob();
+int a,b,c,d, x, y;
+struct foo *z;
+static int options_write(void)
 {
 	if (x & y == 0)
 		frob();
-	if (x + y == 0)
-		frob();
 	if (x | y == 0)
 		frob();
-	return;
+	if (a == b & c == d)
+		frob();
+	if (a == c == d)
+		frob();
+	if (!a == b)
+		frob();
+	c = !a & b;
+	if (x + y == 0)
+		frob();
+	if (!a == !b)
+		frob();
+	if (!a == z->x)
+		frob();
+	if (!!a == b)
+		frob();
+
 }
 /*
  * check-name: Smatch precedence check
  * check-command: smatch sm_precedence.c
  *
  * check-output-start
-sm_precedence.c +5 func(2) warning: do you want parens here?
-sm_precedence.c +9 func(6) warning: do you want parens here?
+sm_precedence.c +10 options_write(2) warn: add some parenthesis here?
+sm_precedence.c +12 options_write(4) warn: add some parenthesis here?
+sm_precedence.c +14 options_write(6) warn: add some parenthesis here?
+sm_precedence.c +16 options_write(8) warn: add some parenthesis here?
+sm_precedence.c +18 options_write(10) warn: add some parenthesis here?
+sm_precedence.c +20 options_write(12) warn: add some parenthesis here?
  * check-output-end
  */
