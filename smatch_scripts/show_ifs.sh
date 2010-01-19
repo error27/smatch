c9a733591dbb105fd566b5df94574b3fb9e941a6
Dan Carpenter <error27@gmail.com>
2010-01-19 21:32:13 +0300

    show_ifs.sh:  Add the -C option
    Basically I copied the show_unreachable.sh script and changed the grep
    line.
    
    I decided to leave default output the same as before.
    
    Signed-off-by: Dan Carpenter <error27@gmail.com>

--- smatch_scripts/show_ifs.sh
+++ smatch_scripts/show_ifs.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-file=$1
+context=1
+if [ "$1" = "-C" ] ; then
+    shift
+    context=$1
+    shift
+fi
 
+file=$1
 if [[ "$file" = "" ]] ; then
-    echo "Usage:  $0 <file with smatch messages>"
+    echo "Usage:  $0 [-C <lines of context>] <file with smatch messages>"
     exit 1
 fi
 
 grep 'if();' $file | cut -d ' ' -f1,2 | while read code_file line ; do
+    echo "========================================================="
     echo $code_file $line
-    tail -n $line $code_file | head -n 1
+    tail -n +$(($line - ($context - 1))) $code_file | head -n $(($context - 1))
+    if [[ $context -gt 1 ]] ; then
+	echo "---------------------------------------------------------"
+    fi
+    tail -n $line $code_file | head -n $context
 done
 
