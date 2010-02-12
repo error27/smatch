20096030c103b301967d90c065642601d3e9d5e0
Dan Carpenter <error27@gmail.com>
2010-02-12 23:06:46 +0300

    add smatch_scripts/new_bugs.sh: prints the bugs introduced between 2 smatch runs
    Say you want to see the bugs introduced between the -rc1 and -rc2 kernel,
    you could run smatch on each kernel.  Then use new_bugs.sh to show the
    newly introduced bugs.
    
    The advantage of the script over a normal diff is that it doesn't print
    out bugs where only the line number changed but the message is the same.
    
    Signed-off-by: Dan Carpenter <error27@gmail.com>

---
+++ smatch_scripts/new_bugs.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+new=$1
+old=$2
+
+if [ "$old" = "" ] ; then
+    echo "usage:  $0 <new file> <old file>"
+    exit 1
+fi
+
+#
+# If the $old and $new are very similar then we can 
+# filter out a lot of bug just by doing a diff.
+#
+# But the line numbers change quite frequently so
+# really we only want to see if the line numbers
+# have changed inside the function.
+# The 42 in this messae:
+# file.c +123 some_func(42) warn: blah blah blah
+#
+
+IFS="
+"
+for err in $(diff -u $old $new | grep ^+ | cut -b 2- | \
+    egrep '(warn|error):' | grep " +") ; do
+
+    # we are only interested in the last chunk.
+    # "some_func(42) warn: blah blah blah"
+    last=$(echo $err | cut -d ' ' -f 3-)
+
+    # There are some error message which include a second
+    # line number so we crudely chop that off.
+    last=$(echo $last | sed -e 's/line .*//')
+
+    if ! grep -Fq "$last" $old ; then
+	echo $err
+    fi
+done
