242db69f118f40925b14c44ba23209eb10ffc3ad
Dan Carpenter <error27@gmail.com>
2010-03-10 11:11:22 +0300

    update validation/sm_casts2.c
    Smatch has been producing the more accurate output for a while but the
    validation test wasn't updated.
    
    Signed-off-by: Dan Carpenter <error27@gmail.com>

--- validation/sm_casts2.c
+++ validation/sm_casts2.c
@@ -28,9 +28,7 @@ int main(void)
  * check-command: smatch sm_casts2.c
  *
  * check-output-start
-sm_casts2.c +12 main(3) warn: 4294967295 is more than 255 (max '*a' can be) so this is always false.
-sm_casts2.c +16 main(7) warn: 4294967295 is more than 2147483647 (max '*c' can be) so this is always false.
-sm_casts2.c +18 main(9) warn: 4294967295 is more than 2147483647 (max '*d' can be) so this is always false.
-sm_casts2.c +22 main(13) warn: 4294967295 is more than 127 (max '*****d' can be) so this is always false.
+sm_casts2.c +12 main(3) error: *a is never equal to 4294967295 (wrong type 0 - 255).
+sm_casts2.c +22 main(13) error: *****d is never equal to 4294967295 (wrong type -128 - 127).
  * check-output-end
  */
