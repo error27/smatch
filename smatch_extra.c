error27/smatch

commit 8c358883a613f1da29c3a4944b3ece7380dfb25c

equiv: handle comparisons with zero
Signed-off-by: Dan Carpenter <error27@gmail.com>

smatch_extra.c
@@ -877,7 +877,7 @@ void __extra_match_condition(struct expression *expr)
 			false_state = alloc_extra_state(0);
 		else
 			false_state = NULL;
-		set_true_false_states(my_id, name, sym, true_state, false_state);
+		set_extra_true_false(name, sym, true_state, false_state);
 		free_string(name);
 		return;
 	case EXPR_COMPARE:

commit 9d0929ecc6f54f947ea683e8ae5e035a60891357

equiv:  fix crashing bug introduced in 8c358883a
Signed-off-by: Dan Carpenter <error27@gmail.com>

smatch_extra.c
@@ -230,8 +230,10 @@ void set_extra_true_false(const char *name, struct symbol *sym,
 	FOR_EACH_PTR(get_dinfo(orig_state)->equiv, tracker) {
 		set_true_false_states(tracker->owner, tracker->name, tracker->sym, 
 				true_state, false_state);
-		add_equiv(true_state, tracker->name, tracker->sym);
-		add_equiv(false_state, tracker->name, tracker->sym);
+		if (true_state)
+			add_equiv(true_state, tracker->name, tracker->sym);
+		if (false_state)
+			add_equiv(false_state, tracker->name, tracker->sym);
 	} END_FOR_EACH_PTR(tracker);
 }
 