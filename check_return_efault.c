8397130125bb80b286b791e9a680669eda7ee1e3
Dan Carpenter <error27@gmail.com>
2010-06-04 12:54:14 +0200

    return_efault: find more bugs
    The old heuristic was that if a return only came from copy_to_user() and
    there was non-null then complain.
    
    The new heuristic is that if the return is a potentially positive return
    from copy_to_user() and it is potentially negative as well then complain.
    
    Signed-off-by: Dan Carpenter <error27@gmail.com>

--- check_return_efault.c
+++ check_return_efault.c
@@ -13,36 +13,71 @@
  */
 
 #include "smatch.h"
+#include "smatch_slist.h"
 #include "smatch_extra.h"
 
 static int my_id;
 
 STATE(remaining);
-STATE(reassigned);
+STATE(ok);
 
 static void ok_to_use(const char *name, struct symbol *sym, struct expression *expr, void *unused)
 {
-	set_state(my_id, name, sym, &reassigned);
+	set_state(my_id, name, sym, &ok);
 }
 
 static void match_copy(const char *fn, struct expression *expr, void *unused)
 {
+	struct expression *call;
+	struct expression *arg;
+	long long max;
+
 	if (expr->op == SPECIAL_SUB_ASSIGN)
 		return;
 	set_state_expr(my_id, expr->left, &remaining);
+
+	call = strip_expr(expr->right);
+	if (call->type != EXPR_CALL)
+		return;
+	arg = get_argument_from_call_expr(call->args, 2);
+	if (!get_absolute_max(arg, &max))
+		max = whole_range.max;
+	set_extra_expr_mod(expr->left, alloc_extra_state_range(0, max));
+
 }
 
+static void match_condition(struct expression *expr)
+{
+	if (!get_state_expr(my_id, expr))
+		return;
+	/* If the variable is zero that's ok */
+	set_true_false_states_expr(my_id, expr, NULL, &ok);
+}
+
+/*
+ * This function is biased in favour of print out errors.
+ * The heuristic to print is:
+ *    If we have a potentially positive return from copy_to_user
+ *    and there is a possibility that we return negative as well
+ *    then complain.
+ */
 static void match_return(struct expression *ret_value)
 {
 	struct smatch_state *state;
+	struct sm_state *sm;
+	long long min;
 
-	state = get_state_expr(my_id, ret_value);
-	if (!state || state != &remaining)
+	sm = get_sm_state_expr(my_id, ret_value);
+	if (!sm)
+		return;
+	if (!slist_has_state(sm->possible, &remaining))
 		return;
 	state = get_state_expr(SMATCH_EXTRA, ret_value);
 	if (!state)
 		return;
-	if (possibly_true(SPECIAL_EQUAL, get_dinfo(state), 0, 0))
+	if (!get_absolute_min(ret_value, &min))
+		return;
+	if (min == 0)
 		return;
 	sm_msg("warn: maybe return -EFAULT instead of the bytes remaining?");
 }
@@ -58,6 +93,7 @@ void check_return_efault(int id)
 	add_function_assign_hook("copy_from_user", &match_copy, NULL);
 	add_function_assign_hook("__copy_from_user", &match_copy, NULL);
 	add_function_assign_hook("clear_user", &match_copy, NULL);
+	add_hook(&match_condition, CONDITION_HOOK);
 	add_hook(&match_return, RETURN_HOOK);
 	set_default_modification_hook(my_id, ok_to_use);
 }
