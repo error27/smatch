94fc2be8c45d27c2316baa3f8c78f1ca185bffdc
Dan Carpenter <error27@gmail.com>
2010-07-06 13:37:44 +0200

    check_free: create a separate check for misusing freed memory
    check_memory is too messy.  It's better if check_memory only checked for
    leaks and check_free checks for double frees etc.
    
    Signed-off-by: Dan Carpenter <error27@gmail.com>

---
+++ check_free.c
@@ -0,0 +1,97 @@
+/*
+ * sparse/check_free.c
+ *
+ * Copyright (C) 2010 Dan Carpenter.
+ *
+ * Licensed under the Open Software License version 1.1
+ *
+ */
+
+/*
+ * check_memory() is getting too big and messy.
+ *
+ */
+
+#include "smatch.h"
+#include "smatch_slist.h"
+
+static int my_id;
+
+STATE(freed);
+STATE(ok);
+
+static void ok_to_use(const char *name, struct symbol *sym, struct expression *expr, void *unused)
+{
+	set_state(my_id, name, sym, &ok);
+}
+
+static int is_freed(struct expression *expr)
+{
+	struct sm_state *sm;
+
+	sm = get_sm_state_expr(my_id, expr);
+	if (sm && slist_has_state(sm->possible, &freed))
+		return 1;
+	return 0;
+}
+
+static void match_symbol(struct expression *expr)
+{
+	char *name;
+
+	if (!is_freed(expr))
+		return;
+	name = get_variable_from_expr(expr, NULL);
+	sm_msg("warn: '%s' was already freed.", name);
+	free_string(name);
+}
+
+static void match_dereferences(struct expression *expr)
+{
+	char *name;
+
+	if (expr->type != EXPR_PREOP)
+		return;
+	expr = strip_expr(expr->unop);
+
+	if (!is_freed(expr))
+		return;
+	name = get_variable_from_expr(expr, NULL);
+	sm_msg("error: dereferencing freed memory '%s'", name);
+	set_state_expr(my_id, expr, &ok);
+	free_string(name);
+}
+
+static void match_free(const char *fn, struct expression *expr, void *param)
+{
+	struct expression *arg;
+
+	arg = get_argument_from_call_expr(expr->args, (int)param);
+	if (!arg)
+		return;
+	/* option_spammy already prints a warning here */
+	if (!option_spammy && is_freed(arg)) {
+		char *name = get_variable_from_expr(arg, NULL);
+
+		sm_msg("error: double free of '%s'", name);
+		free_string(name);
+	}
+	set_state_expr(my_id, arg, &freed);
+}
+
+void check_free(int id)
+{
+	my_id = id;
+
+	if (option_project == PROJ_KERNEL)
+		add_function_hook("kfree", &match_free, (void *)0);
+	else
+		add_function_hook("free", &match_free, (void *)0);
+
+	if (option_spammy)
+		add_hook(&match_symbol, SYM_HOOK);
+	else
+		add_hook(&match_dereferences, DEREF_HOOK);
+
+	set_default_modification_hook(my_id, ok_to_use);
+}
