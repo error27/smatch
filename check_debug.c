6f75277ce746b9e50f63afcf76959abaa8fc9bf8
Dan Carpenter <dan.carpenter@oracle.com>
2011-10-07 13:35:02 +0300

    debug:  add a __smatch_possible("variable") function
    This prints out a list of the values in sm->possible from SMATCH_EXTRA.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- check_debug.c
+++ check_debug.c
@@ -46,6 +46,37 @@ static void match_print_value(const char *fn, struct expression *expr, void *inf
 	free_slist(&slist);
 }
 
+static void print_possible(struct sm_state *sm)
+{
+	struct sm_state *tmp;
+
+	sm_msg("Possible values for %s", sm->name);
+	FOR_EACH_PTR(sm->possible, tmp) {
+		printf("%s\n", tmp->state->name);
+	} END_FOR_EACH_PTR(tmp);
+	sm_msg("===");
+}
+
+static void match_possible(const char *fn, struct expression *expr, void *info)
+{
+	struct state_list *slist;
+	struct sm_state *tmp;
+	struct expression *arg_expr;
+
+	arg_expr = get_argument_from_call_expr(expr->args, 0);
+	if (arg_expr->type != EXPR_STRING) {
+		sm_msg("error:  the argument to %s is supposed to be a string literal", fn);
+		return;
+	}
+
+	slist = get_all_states(SMATCH_EXTRA);
+	FOR_EACH_PTR(slist, tmp) {
+		if (!strcmp(tmp->name, arg_expr->string->data))
+			print_possible(tmp);
+	} END_FOR_EACH_PTR(tmp);
+	free_slist(&slist);
+}
+
 static void match_note(const char *fn, struct expression *expr, void *info)
 {
 	struct expression *arg_expr;
@@ -72,6 +103,7 @@ void check_debug(int id)
 	my_id = id;
 	add_function_hook("__smatch_all_values", &match_all_values, NULL);
 	add_function_hook("__smatch_value", &match_print_value, NULL);
+	add_function_hook("__smatch_possible", &match_possible, NULL);
 	add_function_hook("__smatch_cur_slist", &match_cur_slist, NULL);
 	add_function_hook("__smatch_note", &match_note, NULL);
 	add_function_hook("__smatch_debug_on", &match_debug_on, NULL);
--- check_debug.h
+++ check_debug.h
@@ -1,6 +1,7 @@
 static inline void __smatch_cur_slist(void){}
 static inline void __smatch_all_values(void){}
 static inline void __smatch_value(const char *unused){}
+static inline void __smatch_possible(const char *unused){}
 static inline void __smatch_print_value(const char *unused){}
 static inline void __smatch_note(const char *note){}
 static inline void __smatch_debug_on(void){}
