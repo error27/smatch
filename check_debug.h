f45a3887e112c6c81f92253f73f1b551f1b2133d
Dan Carpenter <dan.carpenter@oracle.com>
2011-11-01 09:33:03 +0300

    Allow check_debug.h to be included more than once.
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- check_debug.h
+++ check_debug.h
@@ -1,3 +1,6 @@
+#ifndef __SMATCH_CHECK_DEBUG
+#define __SMATCH_CHECK_DEBUG
+
 static inline void __smatch_cur_slist(void){}
 static inline void __smatch_all_values(void){}
 static inline void __smatch_value(const char *unused){}
@@ -6,3 +9,5 @@ static inline void __smatch_print_value(const char *unused){}
 static inline void __smatch_note(const char *note){}
 static inline void __smatch_debug_on(void){}
 static inline void __smatch_debug_off(void){}
+
+#endif
