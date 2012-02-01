ae37783f3f17819cb8a03ffc8444ad2d4882153e
Dan Carpenter <dan.carpenter@oracle.com>
2012-02-01 18:00:00 +0300

    db_info: remove duplicate implementation of show_ranges()
    I guess the point was that printing raw numbers instead of "unknown" would
    be easier to parse.  But actually it's ugly to look at so get rid of it.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- check_db_info.c
+++ check_db_info.c
@@ -12,41 +12,6 @@
 
 static int my_id;
 
-static char *show_num(long long num)
-{
-	static char buff[256];
-
-	if (num < 0) {
-		snprintf(buff, 255, "(%lld)", num);
-	} else {
-		snprintf(buff, 255, "%lld", num);
-	}
-	buff[255] = '\0';
-	return buff;
-}
-
-static char *show_ranges_raw(struct range_list *list)
-{
-	struct data_range *tmp;
-	static char full[256];
-	int i = 0;
-
-	full[0] = '\0';
-	full[255] = '\0';
- 	FOR_EACH_PTR(list, tmp) {
-		if (i++)
-			strncat(full, ",", 254 - strlen(full));
-		if (tmp->min == tmp->max) {
-			strncat(full, show_num(tmp->min), 254 - strlen(full));
-			continue;
-		}
-		strncat(full, show_num(tmp->min), 254 - strlen(full));
-		strncat(full, "-", 254 - strlen(full));
-		strncat(full, show_num(tmp->max), 254 - strlen(full));
-	} END_FOR_EACH_PTR(tmp);
-	return full;
-}
-
 static void match_return(struct expression *ret_value)
 {
 	struct smatch_state *state;
@@ -66,8 +31,7 @@ static void match_return(struct expression *ret_value)
 		sm_msg("info: return_value unknown");
 		return;
 	}
-	rlist = ((struct data_info *)state->data)->value_ranges;
-	sm_msg("info: return_value %s", show_ranges_raw(rlist));
+	sm_msg("info: return_value %s", state->name);
 }
 
 void check_db_info(int id)
