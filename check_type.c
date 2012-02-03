17927c2d89eaf036152a7eaabecba32aae0ee4e8
Dan Carpenter <dan.carpenter@oracle.com>
2012-02-03 12:39:58 +0300

    type: remove a free_netdev() false positive
    There is one function which is allowed to free net_device structs with
    kfree().  It's simple enough to remove that false positive.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- check_type.c
+++ check_type.c
@@ -11,6 +11,17 @@
 
 static int my_id;
 
+static int in_function(const char *fn)
+{
+	char *cur_func = get_function();
+
+	if (!cur_func)
+		return 0;
+	if (!strcmp(cur_func, fn))
+		return 1;
+	return 0;
+}
+
 static void match_free(const char *fn, struct expression *expr, void *data)
 {
 	struct expression *arg_expr;
@@ -29,10 +40,15 @@ static void match_free(const char *fn, struct expression *expr, void *data)
 
 	name = get_variable_from_expr_complex(arg_expr, NULL);
 
-	if (!strcmp("sk_buff", type->ident->name))
+	if (!strcmp("sk_buff", type->ident->name)) {
 		sm_msg("error: use kfree_skb() here instead of kfree(%s)", name);
-	else if (!strcmp("net_device", type->ident->name))
+	} else if (!strcmp("net_device", type->ident->name)) {
+		if (in_function("alloc_netdev"))
+			return;
+		if (in_function("alloc_netdev_mqs"))
+			return;
 		sm_msg("error: use free_netdev() here instead of kfree(%s)", name);
+	}
 
 	free_string(name);
 }
