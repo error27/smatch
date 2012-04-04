81951aab7302308bfd172c4cccb08d5771abcf1c
Dan Carpenter <dan.carpenter@oracle.com>
2012-04-04 12:59:24 +0300

    db: add some smdb_ scripts
    These scripts query the smatch database.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

---
+++ smatch_scripts/db/smdb_return_info.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+usage()
+{
+    echo "Usage $0 <function>"
+    exit 1
+}
+
+func=$1
+
+if [ "$func" = "" ] ; then
+    usage
+fi
+
+echo "select * from return_info where function = '$func';" | sqlite3 smatch_db.sqlite
