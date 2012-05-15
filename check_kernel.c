4fc061ed7a864acce39461173eee3652d89c7d60
Dan Carpenter <dan.carpenter@oracle.com>
2012-05-15 15:23:45 +0300

    check_kernel: ERR_PTR() returns non-NULL (mostly)
    If you have p = ERR_PTR(x) then this says that if we know the value of
    x then p is the same value.  If we don't know the value of x then it's
    some error code.
    
    Having this information eliminates some NULL dereference false positives.
    
    The plan is that eventually everything will be sorted into stack pointers,
    heap pointers, null pointers or error pointers.  Maybe users space
    pointers?
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- check_kernel.c
+++ check_kernel.c
@@ -14,6 +14,21 @@
 #include "smatch.h"
 #include "smatch_extra.h"
 
+static void match_err_cast(const char *fn, struct expression *expr, void *unused)
+{
+	struct expression *arg;
+	struct expression *right;
+	struct range_list *rl;
+
+	right = strip_expr(expr->right);
+	arg = get_argument_from_call_expr(right->args, 0);
+
+	if (get_implied_range_list(arg, &rl))
+		set_extra_expr_mod(expr->left, alloc_estate_range_list(rl));
+	else
+		set_extra_expr_mod(expr->left, alloc_estate_range(-4095, -1));
+}
+
 static void match_param_nonnull(const char *fn, struct expression *call_expr,
 			struct expression *assign_expr, void *_param)
 {
@@ -38,6 +53,9 @@ void check_kernel(int id)
 	if (option_project != PROJ_KERNEL)
 		return;
 
+	add_function_assign_hook("ERR_PTR", &match_err_cast, NULL);
+	add_function_assign_hook("ERR_CAST", &match_err_cast, NULL);
+	add_function_assign_hook("PTR_ERR", &match_err_cast, NULL);
 	return_implies_state("IS_ERR_OR_NULL", 0, 0, &match_param_nonnull, (void *)0);
 	return_implies_state("tomoyo_memory_ok", 1, 1, &match_param_nonnull, (void *)0);
 	add_macro_assign_hook("container_of", &match_container_of, NULL);
