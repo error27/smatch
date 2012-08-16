ea42901676bc6daefd585b6afa745303e6f63f64
Dan Carpenter <dan.carpenter@oracle.com>
2012-08-16 17:25:47 +0300

    logical_instead_of_bitwise: foo = (1 << x) && y; is normally a bug
    There were two cases of this in the kernel and both were bugs.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- check_logical_instead_of_bitwise.c
+++ check_logical_instead_of_bitwise.c
@@ -11,6 +11,17 @@
 
 static int my_id;
 
+static int is_bitshift(struct expression *expr)
+{
+	expr = strip_expr(expr);
+
+	if (expr->type != EXPR_BINOP)
+		return 0;
+	if (expr->op == SPECIAL_LEFTSHIFT || expr->op == SPECIAL_RIGHTSHIFT)
+		return 1;
+	return 0;
+}
+
 static void match_logic(struct expression *expr)
 {
 	long long val;
@@ -32,9 +43,21 @@ static void match_logic(struct expression *expr)
 	sm_msg("warn: should this be a bitwise op?");
 }
 
+static void match_assign(struct expression *expr)
+{
+	struct expression *right;
+
+	right = strip_expr(expr->right);
+	if (right->type != EXPR_LOGICAL)
+		return;
+	if (is_bitshift(right->left) || is_bitshift(right->right))
+		sm_msg("warn: should this be a bitwise op?");
+}
+
 void check_logical_instead_of_bitwise(int id)
 {
 	my_id = id;
 
 	add_hook(&match_logic, LOGIC_HOOK);
+	add_hook(&match_assign, ASSIGNMENT_HOOK);
 }
