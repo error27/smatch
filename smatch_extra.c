b0e645ef0517f0ab530d87dc30020fd13740557a
Dan Carpenter <dan.carpenter@oracle.com>
2012-10-04 16:29:07 +0300

    extra: delete some debug code
    This code was only for debugging and should have been committed in the
    first place.
    
    The debugging was to test that we could convert from a range list to
    text and then back.  It mostly works.  The problem is that some times
    range lists are not connected properly so you get "min-1,2-max" when it
    should be "min-max".  It's not harmful to have a range list like that but
    it's a bit wrong.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- smatch_extra.c
+++ smatch_extra.c
@@ -947,9 +947,6 @@ static void set_param_value(const char *name, struct symbol *sym, char *key, cha
 
 	snprintf(fullname, 256, "%s%s", name, key + 2);
 	get_value_ranges(value, &rl);
-	char *tmp = show_ranges(rl);
-	if (strcmp(tmp, value))
-		sm_msg("value = %s, ranges = %s", value, tmp);
 	state = alloc_estate_range_list(rl);
 	set_state(SMATCH_EXTRA, fullname, sym, state);
 }
