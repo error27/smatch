397837fed6db8b4e2931e91a652defc6920e2188
Dan Carpenter <dan.carpenter@oracle.com>
2012-11-12 22:22:48 +0300

    sval: signed: pretty much re-write the whole thing
    This isn't very nice, because it changes many of the warning messages.  But
    it is sort of easier to use the new sval range lists to do this stuff.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- check_signed.c
+++ check_signed.c
@@ -16,6 +16,7 @@
  */
 
 #include "smatch.h"
+#include "smatch_extra.h"
 
 static int my_id;
 
@@ -28,7 +29,7 @@ static void match_assign(struct expression *expr)
 	sval_t sval;
 	sval_t max;
 	sval_t min;
-	char *name;
+	char *left_name, *right_name;
 
 	if (expr->op == SPECIAL_AND_ASSIGN || expr->op == SPECIAL_OR_ASSIGN)
 		return;
@@ -44,10 +45,11 @@ static void match_assign(struct expression *expr)
 		return;
 	max = sval_type_max(sym);
 	if (sval_cmp(max, sval) < 0 && !(sval.value < 256 && max.value == 127)) {
-		name = get_variable_from_expr_complex(expr->left, NULL);
-		sm_msg("warn: value %s can't fit into %s '%s'",
-		       sval_to_str(sval), sval_to_str(max), name);
-		free_string(name);
+		left_name = get_variable_from_expr_complex(expr->left, NULL);
+		right_name = get_variable_from_expr_complex(expr->right, NULL);
+		sm_msg("warn: '%s' %s can't fit into %s '%s'",
+		       right_name, sval_to_numstr(sval), sval_to_numstr(max), left_name);
+		free_string(left_name);
 	}
 	min = sval_type_min(sym);
 	if (sval_cmp_t(&llong_ctype, min, sval) > 0) {
@@ -57,77 +59,84 @@ static void match_assign(struct expression *expr)
 			return;
 		if (expr->op == SPECIAL_SUB_ASSIGN || expr->op == SPECIAL_ADD_ASSIGN)
 			return;
-		name = get_variable_from_expr_complex(expr->left, NULL);
+		if (sval_positive_bits(sval) == 7)
+			return;
+		left_name = get_variable_from_expr_complex(expr->left, NULL);
 		if (min.value == 0) {
 			sm_msg("warn: assigning %s to unsigned variable '%s'",
-			       sval_to_str(sval), name);
+			       sval_to_str(sval), left_name);
 		} else {
 			sm_msg("warn: value %s can't fit into %s '%s'",
-			       sval_to_str(sval), sval_to_str(min), name);
+			       sval_to_str(sval), sval_to_str(min), left_name);
 		}
-		free_string(name);
+		free_string(left_name);
 	}
 }
 
-static const char *get_tf(sval_t variable, sval_t known, int var_pos, int op)
-{
-	if (op == SPECIAL_EQUAL)
-		return "false";
-	if (op == SPECIAL_NOTEQUAL)
-		return "true";
-	if (var_pos == VAR_ON_LEFT) {
-		if (sval_cmp(variable, known) > 0 && (op == '<' || op == SPECIAL_LTE))
-			return "false";
-		if (sval_cmp(variable, known) > 0 && (op == '>' || op == SPECIAL_GTE))
-			return "true";
-		if (sval_cmp(variable, known) < 0 && (op == '<' || op == SPECIAL_LTE))
-			return "true";
-		if (sval_cmp(variable, known) < 0 && (op == '>' || op == SPECIAL_GTE))
-			return "false";
-	}
-	if (var_pos == VAR_ON_RIGHT) {
-		if (sval_cmp(known, variable) > 0 && (op == '<' || op == SPECIAL_LTE))
-			return "false";
-		if (sval_cmp(known, variable) > 0 && (op == '>' || op == SPECIAL_GTE))
-			return "true";
-		if (sval_cmp(known, variable) < 0 && (op == '<' || op == SPECIAL_LTE))
-			return "true";
-		if (sval_cmp(known, variable) < 0 && (op == '>' || op == SPECIAL_GTE))
-			return "false";
-	}
-	return "the same";
-}
-
-static int compare_against_macro(int lr, struct expression *expr)
-{
-	struct expression *known = expr->left;
-
-	if (lr == VAR_ON_LEFT)
-		known = expr->right;
-
-	return !!get_macro_name(known->pos);
-}
-
-static int cap_both_size(int lr, struct expression *expr)
+static int cap_gt_zero_and_lt(struct expression *expr)
 {
 
 	struct expression *var = expr->left;
 	struct expression *tmp;
 	char *name1 = NULL;
 	char *name2 = NULL;
+	sval_t known;
 	int ret = 0;
 	int i;
 
-	/* screw it.  I am writing this to mark yoda code as buggy.
-	 * Valid comparisons between an unsigned and zero are:
-	 * 1) inside a macro.
-	 * 2) foo < LOWER_BOUND where LOWER_BOUND is a macro.
-	 * 3) foo < 0 || foo > X in exactly this format.  No Yoda.
-	 */
-
-	if (lr != VAR_ON_LEFT)
+	if (!get_value_sval(expr->right, &known) || known.value != 0)
 		return 0;
-	if (expr->op != '<' && expr->op != SPECIAL_UNSIGNED_LT)
+	if (expr->op != SPECIAL_UNSIGNED_GT && expr->op != SPECIAL_UNSIGNED_GTE)
+		return 0;
+
+	i = 0;
+	FOR_EACH_PTR_REVERSE(big_expression_stack, tmp) {
+		if (!i++)
+			continue;
+		if (tmp->op == SPECIAL_LOGICAL_AND) {
+			struct expression *right = strip_expr(tmp->right);
+
+			if (right->op != '<' &&
+			    right->op != SPECIAL_UNSIGNED_LT &&
+			    right->op != SPECIAL_LTE &&
+			    right->op != SPECIAL_UNSIGNED_LTE)
+				return 0;
+
+			name1 = get_variable_from_expr_complex(var, NULL);
+			if (!name1)
+				goto free;
+
+			name2 = get_variable_from_expr_complex(right->left, NULL);
+			if (!name2)
+				goto free;
+			if (!strcmp(name1, name2))
+				ret = 1;
+			goto free;
+
+		}
+		return 0;
+	} END_FOR_EACH_PTR_REVERSE(tmp);
+
+free:
+	free_string(name1);
+	free_string(name2);
+	return ret;
+}
+
+static int cap_lt_zero_or_gt(struct expression *expr)
+{
+
+	struct expression *var = expr->left;
+	struct expression *tmp;
+	char *name1 = NULL;
+	char *name2 = NULL;
+	sval_t known;
+	int ret = 0;
+	int i;
+
+	if (!get_value_sval(expr->right, &known) || known.value != 0)
+		return 0;
+	if (expr->op != SPECIAL_UNSIGNED_LT && expr->op != SPECIAL_UNSIGNED_LTE)
 		return 0;
 
 	i = 0;
@@ -164,89 +173,119 @@ free:
 	return ret;
 }
 
+static int cap_both_sides(struct expression *expr)
+{
+	if (expr->op == SPECIAL_UNSIGNED_LT || expr->op == SPECIAL_UNSIGNED_LTE)
+		return cap_lt_zero_or_gt(expr);
+	if (expr->op == SPECIAL_UNSIGNED_GT || expr->op == SPECIAL_UNSIGNED_GTE)
+		return cap_gt_zero_and_lt(expr);
+	return 0;
+}
+
+static int compare_against_macro(struct expression *expr)
+{
+	sval_t known;
+
+	if (expr->op != SPECIAL_UNSIGNED_LT)
+		return 0;
+
+	if (!get_value_sval(expr->right, &known) || known.value != 0)
+		return 0;
+	return !!get_macro_name(expr->right->pos);
+}
+
+static int print_unsigned_never_less_than_zero(struct expression *expr)
+{
+	sval_t known;
+	char *name;
+
+	if (expr->op != SPECIAL_UNSIGNED_LT)
+		return 0;
+
+	if (!get_value_sval(expr->right, &known) || known.value != 0)
+		return 0;
+
+	name = get_variable_from_expr_complex(expr->left, NULL);
+	sm_msg("warn: unsigned '%s' is never less than zero.", name);
+	free_string(name);
+	return 1;
+}
+
 static void match_condition(struct expression *expr)
 {
-	struct expression *var = NULL;
-	struct symbol *var_type = NULL;
-	struct symbol *known_type = NULL;
+	struct symbol *type;
 	sval_t known;
-	sval_t max;
-	sval_t min;
-	int lr;
-	char *name;
+	sval_t min, max;
+	struct range_list_sval *rl_left_orig, *rl_right_orig;
+	struct range_list_sval *rl_left, *rl_right;
 
 	if (expr->type != EXPR_COMPARE)
 		return;
 
+	type = get_type(expr);
+	if (!type) {
+		sm_msg("debug: could not get condition type");
+		return;
+	}
+
+	/* screw it.  I am writing this to mark yoda code as buggy.
+	 * Valid comparisons between an unsigned and zero are:
+	 * 1) inside a macro.
+	 * 2) foo < LOWER_BOUND where LOWER_BOUND is a macro.
+	 * 3) foo < 0 || foo > X in exactly this format.  No Yoda.
+	 * 4) foo >= 0 && foo < X
+	 */
+	if (get_macro_name(expr->pos))
+		return;
+	if (compare_against_macro(expr))
+		return;
+	if (cap_both_sides(expr))
+		return;
+
+	/* This is a special case for the common error */
+	if (print_unsigned_never_less_than_zero(expr))
+		return;
+
+	/* check that one and only one side is known */
 	if (get_value_sval(expr->left, &known)) {
-		if (get_value_sval(expr->right, &max))
-			return; /* both sides known */
-		lr = VAR_ON_RIGHT;
-		var = expr->right;
-		known_type = get_type(expr->left);
+		if (get_value_sval(expr->right, &known))
+			return;
+		rl_left_orig = alloc_range_list_sval(known, known);
+		rl_left = cast_rl(rl_left_orig, type);
+
+		min = sval_type_min(get_type(expr->right));
+		max = sval_type_max(get_type(expr->right));
+		rl_right_orig = alloc_range_list_sval(min, max);
+		rl_right = cast_rl(rl_right_orig, type);
 	} else if (get_value_sval(expr->right, &known)) {
-		lr = VAR_ON_LEFT;
-		var = expr->left;
-		known_type = get_type(expr->right);
+		rl_right_orig = alloc_range_list_sval(known, known);
+		rl_right = cast_rl(rl_right_orig, type);
+
+		min = sval_type_min(get_type(expr->left));
+		max = sval_type_max(get_type(expr->left));
+		rl_left_orig = alloc_range_list_sval(min, max);
+		rl_left = cast_rl(rl_left_orig, type);
 	} else {
 		return;
 	}
 
-	var_type = get_type(var);
-	if (!var_type)
-		return;
-	if (var_type->bit_size >= 32 && !option_spammy)
-		return;
+	if (!possibly_true_range_lists_sval(rl_left, expr->op, rl_right)) {
+		char *name = get_variable_from_expr_complex(expr, NULL);
 
-	name = get_variable_from_expr_complex(var, NULL);
-
-	if (expr->op == SPECIAL_EQUAL || expr->op == SPECIAL_NOTEQUAL) {
-		sval_t sval_min, sval_max;
-
-		sval_min = sval_type_min(var_type);
-		sval_max = sval_type_max(var_type);
-
-		if (sval_cmp(sval_min, known) > 0 || sval_cmp(sval_max, known) < 0)
-			sm_msg("error: %s is never equal to %s (wrong type %s - %s).",
-				name, sval_to_str(known), sval_to_str(sval_min), sval_to_str(sval_max));
-		goto free;
+		sm_msg("warn: impossible condition '(%s) => (%s %s %s)'", name,
+		       show_ranges_sval(rl_left), show_special(expr->op),
+		       show_ranges_sval(rl_right));
+		free_string(name);
 	}
 
-	max = sval_type_max(var_type);
-	min = sval_type_min(var_type);
+	if (!possibly_false_range_lists_sval(rl_left, expr->op, rl_right)) {
+		char *name = get_variable_from_expr_complex(expr, NULL);
 
-	if (sval_cmp(max, known) < 0) {
-		const char *tf = get_tf(max, known, lr, expr->op);
-
-		sm_msg("warn: %s is more than %s (max '%s' can be) so this is always %s.",
-			sval_to_str(known), sval_to_str(max), name, tf);
+		sm_msg("warn: always true condition '(%s) => (%s %s %s)'", name,
+		       show_ranges_sval(rl_left_orig), show_special(expr->op),
+		       show_ranges_sval(rl_right_orig));
+		free_string(name);
 	}
-
-	if (known.value == 0 && type_unsigned(var_type)) {
-		if ((lr && expr->op == '<') ||
-		    (lr && expr->op == SPECIAL_UNSIGNED_LT) ||
-		    (!lr && expr->op == '>') ||
-		    (!lr && expr->op == SPECIAL_UNSIGNED_GT)) {
-			if (!compare_against_macro(lr, expr) && !cap_both_size(lr, expr)) {
-				sm_msg("warn: unsigned '%s' is never less than zero.", name);
-				goto free;
-			}
-		}
-	}
-
-	if (type_unsigned(var_type) && known_type && !type_unsigned(known_type) && known.value < 0) {
-		sm_msg("warn: unsigned '%s' is never less than zero %s.", name, sval_to_str(known));
-		goto free;
-	}
-
-	if (sval_signed(min) && sval_signed(known) && sval_cmp(min, known) > 0) {
-		const char *tf = get_tf(min, known, lr, expr->op);
-
-		sm_msg("warn: %s is less than %s (min '%s' can be) so this is always %s.",
-			sval_to_str(known), sval_to_str(min), name, tf);
-	}
-free:
-	free_string(name);
 }
 
 void check_signed(int id)
