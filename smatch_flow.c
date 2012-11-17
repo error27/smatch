d36f58de0aae91b09f692104439a650983df7eac
Dan Carpenter <dan.carpenter@oracle.com>
2012-11-17 16:42:14 +0300

    flow: hooks: fix MACRO_ASSIGNMENT_HOOK
    The old logic passed the assignment if an assignment took place inside a
    macro.  What we want are places where a macro is assigned to a variable.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- smatch_flow.c
+++ smatch_flow.c
@@ -158,7 +158,8 @@ void __split_expr(struct expression *expr)
 		tmp = strip_expr(expr->right);
 		if (tmp->type == EXPR_CALL)
 			__pass_to_client(expr, CALL_ASSIGNMENT_HOOK);
-		if (get_macro_name(tmp->pos))
+		if (get_macro_name(tmp->pos) &&
+		    get_macro_name(expr->pos) != get_macro_name(tmp->pos))
 			__pass_to_client(expr, MACRO_ASSIGNMENT_HOOK);
 		__split_expr(expr->left);
 		break;
