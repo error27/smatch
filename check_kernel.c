f88989c3a1bff37c787ec02540facce4df0c9651
Dan Carpenter <dan.carpenter@oracle.com>
2013-02-28 14:05:44 +0300

    kernel: remove some unneeded code
    This code isn't needed anymore because it's handled by the new inline
    parsing work.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- check_kernel.c
+++ check_kernel.c
@@ -14,16 +14,6 @@
 #include "smatch.h"
 #include "smatch_extra.h"
 
-static int implied_err_cast_return(struct expression *call, void *unused, struct range_list **rl)
-{
-	struct expression *arg;
-
-	arg = get_argument_from_call_expr(call->args, 0);
-	if (!get_implied_rl(arg, rl))
-		*rl = alloc_rl(ll_to_sval(-4095), ll_to_sval(-1));
-	return 1;
-}
-
 static int implied_copy_return(struct expression *call, void *unused, struct range_list **rl)
 {
 	struct expression *arg;
@@ -49,33 +39,6 @@ static void match_param_nonnull(const char *fn, struct expression *call_expr,
 	set_extra_expr_nomod(arg, true_state);
 }
 
-static void match_not_err(const char *fn, struct expression *call_expr,
-			struct expression *assign_expr, void *unused)
-{
-	struct expression *arg;
-	struct smatch_state *pre_state;
-	struct smatch_state *new_state;
-
-	arg = get_argument_from_call_expr(call_expr->args, 0);
-	pre_state = get_state_expr(SMATCH_EXTRA, arg);
-	new_state = estate_filter_range(pre_state, sval_type_min(&long_ctype), ll_to_sval(-1));
-	set_extra_expr_nomod(arg, new_state);
-}
-
-static void match_err(const char *fn, struct expression *call_expr,
-			struct expression *assign_expr, void *unused)
-{
-	struct expression *arg;
-	struct smatch_state *pre_state;
-	struct smatch_state *new_state;
-
-	arg = get_argument_from_call_expr(call_expr->args, 0);
-	pre_state = get_state_expr(SMATCH_EXTRA, arg);
-	new_state = estate_filter_range(pre_state, sval_type_min(&long_ctype), ll_to_sval(-4096));
-	new_state = estate_filter_range(new_state, ll_to_sval(0), sval_type_max(&long_ctype));
-	set_extra_expr_nomod(arg, new_state);
-}
-
 static void match_container_of(const char *fn, struct expression *expr, void *unused)
 {
 	set_extra_expr_mod(expr->left, alloc_estate_range(valid_ptr_min_sval, valid_ptr_max_sval));
@@ -86,12 +49,6 @@ void check_kernel(int id)
 	if (option_project != PROJ_KERNEL)
 		return;
 
-	add_implied_return_hook("ERR_PTR", &implied_err_cast_return, NULL);
-	add_implied_return_hook("ERR_CAST", &implied_err_cast_return, NULL);
-	add_implied_return_hook("PTR_ERR", &implied_err_cast_return, NULL);
-	return_implies_state("IS_ERR_OR_NULL", 0, 0, &match_param_nonnull, (void *)0);
-	return_implies_state("IS_ERR", 0, 0, &match_not_err, NULL);
-	return_implies_state("IS_ERR", 1, 1, &match_err, NULL);
 	return_implies_state("tomoyo_memory_ok", 1, 1, &match_param_nonnull, (void *)0);
 	add_macro_assign_hook_extra("container_of", &match_container_of, NULL);
 
