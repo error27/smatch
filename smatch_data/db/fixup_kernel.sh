db342feba3b4e62b06520d397e64f5666b4d219b
Dan Carpenter <dan.carpenter@oracle.com>
2013-03-06 13:45:02 +0300

    db: fixup_kernel.sh: only track the main read/write() functions
    The problem here is that we have places which call ->read() and ->write()
    recursively.  It pollutes the interesting information with confusing math
    and causes false positives.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- smatch_data/db/fixup_kernel.sh
+++ smatch_data/db/fixup_kernel.sh
@@ -5,5 +5,9 @@ cat << EOF | sqlite3 smatch_db.sqlite
 insert into caller_info values ('userspace', '', 'compat_sys_ioctl', 0, 0, 3, 0, '$$', '1');
 insert into caller_info values ('userspace', '', 'compat_sys_ioctl', 0, 0, 3, 1, '$$', '1');
 insert into caller_info values ('userspace', '', 'compat_sys_ioctl', 0, 0, 3, 2, '$$', '1');
+
+delete from caller_info where function = '(struct file_operations)->read' and file != 'fs/read_write.c';
+delete from caller_info where function = '(struct file_operations)->write' and file != 'fs/read_write.c';
+
 EOF
 
