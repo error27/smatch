5a21e9aa43569ffa29ee87c766222d85bc83ff7c
Dan Carpenter <dan.carpenter@oracle.com>
2013-03-06 13:46:42 +0300

    db: fixup_kernel.sh: discard caller_info from ->notifier_call
    There is a lot of information passed to ->notifier_call.  It's all
    subsystem specific and Smatch doesn't separate it out properly.  It just
    causes false positives.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- smatch_data/db/fixup_kernel.sh
+++ smatch_data/db/fixup_kernel.sh
@@ -9,5 +9,7 @@ insert into caller_info values ('userspace', '', 'compat_sys_ioctl', 0, 0, 3, 2,
 delete from caller_info where function = '(struct file_operations)->read' and file != 'fs/read_write.c';
 delete from caller_info where function = '(struct file_operations)->write' and file != 'fs/read_write.c';
 
+delete from caller_info where function = '(struct notifier_block)->notifier_call';
+
 EOF
 
