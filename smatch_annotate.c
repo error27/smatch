8749d5dabed38b50d76ef77b9155ddc215adc340
Dan Carpenter <dan.carpenter@oracle.com>
2013-03-25 11:00:40 +0300

    introduce smatch_annotate.c: annotate some common functions
    I've put in that strnlen() and snprintf() return values are limited by
    one of the parameters.  It's not actually true for snprintf() but it's
    close enough.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

---
+++ smatch_annotate.c
@@ -0,0 +1,40 @@
+/*
+ * smatch/smatch_annotate.c
+ *
+ * Copyright (C) 2013 Oracle.
+ *
+ * Licensed under the Open Software License version 1.1
+ *
+ */
+
+/*
+ * A place to add function annotations for common functions.
+ *
+ */
+
+#include "smatch.h"
+#include "smatch_extra.h"
+
+static int param_caps_return(struct expression *call, void *_arg, struct range_list **res)
+{
+	int arg = PTR_INT(_arg);
+	struct expression *expr;
+	struct range_list *rl;
+
+	expr = get_argument_from_call_expr(call->args, arg);
+	if (get_implied_rl(expr, &rl) && rl_max(rl).value != 0)
+		*res = alloc_rl(sval_type_val(rl_type(rl), 0), rl_max(rl));
+	return 1;
+}
+
+void register_annotate(int id)
+{
+	add_implied_return_hook("strnlen", &param_caps_return, INT_PTR(1));
+	/*
+	 * Technically snprintf() returns the number of bytes which *would* have
+	 * been printed.  I do try caclulating that in check_snprintf().  But
+	 * it probably works better to assume the limitter is accurate.
+	 */
+	add_implied_return_hook("snprintf", &param_caps_return, INT_PTR(1));
+
+}
