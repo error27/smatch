0e27e6b96d4c0436b501bfcd699050036fee5ac0
Dan Carpenter <dan.carpenter@oracle.com>
2013-04-10 10:42:15 +0300

    locking: add local_bh_disable/enable
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- check_locking.c
+++ check_locking.c
@@ -304,6 +304,8 @@ static struct lock_info kernel_lock_table[] = {
 	{"__write_lock_irqsave",       LOCK,   "irqsave", RETURN_VAL, ret_any},
 	{"__write_unlock_irqrestore",  UNLOCK, "irqsave", 1, ret_any},
 
+	{"local_bh_disable",	LOCK,	"bottom_half", NO_ARG, ret_any},
+	{"local_bh_enable",	UNLOCK,	"bottom_half", NO_ARG, ret_any},
 	{"spin_lock_bh",        LOCK,   "bottom_half", NO_ARG, ret_any},
 	{"spin_unlock_bh",      UNLOCK, "bottom_half", NO_ARG, ret_any},
 	{"_spin_lock_bh",       LOCK,   "bottom_half", NO_ARG, ret_any},
