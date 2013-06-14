d9b6e5c4c993db928ac106fb62fbc8f16069ce1d
Dan Carpenter <dan.carpenter@oracle.com>
2013-06-14 12:46:58 +0300

    db: fixup_kernel.sh: add comments
    I also moved all the USER_DATA bits to the same section.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- smatch_data/db/fixup_kernel.sh
+++ smatch_data/db/fixup_kernel.sh
@@ -2,33 +2,41 @@
 
 # mark some paramaters as coming from user space
 cat << EOF | sqlite3 smatch_db.sqlite
-insert into caller_info values ('userspace', '', 'compat_sys_ioctl', 0, 0, 3, 0, '$$', '1');
-insert into caller_info values ('userspace', '', 'compat_sys_ioctl', 0, 0, 3, 1, '$$', '1');
-insert into caller_info values ('userspace', '', 'compat_sys_ioctl', 0, 0, 3, 2, '$$', '1');
-
+/* we only care about the main ->read/write() functions. */
 delete from caller_info where function = '(struct file_operations)->read' and file != 'fs/read_write.c';
 delete from caller_info where function = '(struct file_operations)->write' and file != 'fs/read_write.c';
 
+/* delete these function pointers which cause false positives */
 delete from caller_info where function = '(struct notifier_block)->notifier_call' and type != 0;
 delete from caller_info where function = '(struct mISDNchannel)->send' and type != 0;
 
+/* type 3 is USER_DATA */
 delete from caller_info where caller = 'hid_input_report' and type = 3;
 delete from caller_info where caller = 'nes_process_iwarp_aeqe' and type = 3;
 delete from caller_info where caller = 'oz_process_ep0_urb' and type = 3;
 delete from caller_info where function = 'dev_hard_start_xmit' and key = '\$\$' and type = 3;
 delete from caller_info where function like '%->ndo_start_xmit' and key = '\$\$' and type = 3;
 delete from caller_info where caller = 'packet_rcv_fanout' and function = '(struct packet_type)->func' and parameter = 1 and type = 3;
-
 delete from caller_info where caller = 'hptiop_probe' and type = 3;
+
+insert into caller_info values ('userspace', '', 'compat_sys_ioctl', 0, 0, 3, 0, '$$', '1');
+insert into caller_info values ('userspace', '', 'compat_sys_ioctl', 0, 0, 3, 1, '$$', '1');
+insert into caller_info values ('userspace', '', 'compat_sys_ioctl', 0, 0, 3, 2, '$$', '1');
 
 delete from caller_info where function = '(struct timer_list)->function' and parameter = 0;
 
+/*
+ * rw_verify_area is a very central function for the kernel.  The 1000000 isn't
+ * accurate but I've picked it so that we can add "pos + count" without wrapping
+ * on 32 bits.
+ */
 delete from return_states where function = 'rw_verify_area';
 insert into return_states values ('faked', 'rw_verify_area', 0, 1, '0-1000000', 0, 0, -1, '', '');
 insert into return_states values ('faked', 'rw_verify_area', 0, 1, '0-1000000', 0, 11, 2, '*\$\$', '0-1000000');
 insert into return_states values ('faked', 'rw_verify_area', 0, 1, '0-1000000', 0, 11, 3, '\$\$', '0-1000000');
 insert into return_states values ('faked', 'rw_verify_area', 0, 2, '(-4095)-(-1)', 0, 0, -1, '', '');
 
+/* store a bunch of capped functions */
 update return_states set return = '0-u32max[<=p2]' where function = 'copy_to_user';
 update return_states set return = '0-u32max[<=p2]' where function = '_copy_to_user';
 update return_states set return = '0-u32max[<=p2]' where function = '__copy_to_user';
