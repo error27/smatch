c9be20a01c7ffe2655fd660592866ddcc965572f
Dan Carpenter <dan.carpenter@oracle.com>
2013-07-05 11:54:17 +0300

    db/fixup_kernel.sh: filter ->read/write() functions
    Read and write are so important and I hand edit the caller_info a bit.
    Don't allow random callers from outside fs/read_write.c.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- smatch_data/db/fixup_kernel.sh
+++ smatch_data/db/fixup_kernel.sh
@@ -5,6 +5,8 @@ cat << EOF | sqlite3 smatch_db.sqlite
 /* we only care about the main ->read/write() functions. */
 delete from caller_info where function = '(struct file_operations)->read' and file != 'fs/read_write.c';
 delete from caller_info where function = '(struct file_operations)->write' and file != 'fs/read_write.c';
+delete from function_ptr where function = '(struct file_operations)->read';
+delete from function_ptr where function = '(struct file_operations)->write';
 
 /* delete these function pointers which cause false positives */
 delete from caller_info where function = '(struct notifier_block)->notifier_call' and type != 0;
