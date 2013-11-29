error27/smatch

commit 47f53396a1d62719c44941f84370ead80181728e

If two implied casts end up undoing each other, just remove them.
This happens for implied cast simplification.

Currently only a binary not will be simplified, but it's conceptually
right for things like "char a,b,c; c = a+b;" too, where we should end
up simplifying away the thing across a binop too..). Some day..

evaluate.c
@@ -181,6 +181,11 @@ left:
 	return left;
 }
 
+static int same_cast_type(struct symbol *orig, struct symbol *new)
+{
+	return orig->bit_size == new->bit_size && orig->bit_offset == orig->bit_offset;
+}
+
 /*
  * This gets called for implicit casts in assignments and
  * integer promotion. We often want to try to move the
@@ -206,6 +211,9 @@ static struct expression * cast_to(struct expression *old, struct symbol *type)
 
 	case EXPR_IMPLIED_CAST:
 		if (old->ctype->bit_size >= type->bit_size) {
+			struct expression *orig = old->cast_expression;
+			if (same_cast_type(orig->ctype, type))
+				return orig;
 			if (old->ctype->bit_offset == type->bit_offset) {
 				old->ctype = type;
 				old->cast_type = type;

commit b1c2600e2ce164fcebc507722905a008f8e2a442

sparse: Also check bit_offset when checking implicit casts
The comparison in same_cast_type only checked the bit_size of the new and the
old symbol. The bit_offset was only compared with itself and thus would always
be true. Instead Linus most likely wanted to compare the bit_offset of the new
and the old symbol. This regression was introduced in
47f53396a1d62719c44941f84370ead80181728e ("If two implied casts end up undoing
each other, just remove them.").

Reported-by: James Westby <jw+debian@jameswestby.net>
Signed-off-by: Franz Schrober <franzschrober@yahoo.de>
Signed-off-by: Christopher Li <sparse@chrisli.org>

evaluate.c
@@ -191,7 +191,8 @@ left:
 
 static int same_cast_type(struct symbol *orig, struct symbol *new)
 {
-	return orig->bit_size == new->bit_size && orig->bit_offset == orig->bit_offset;
+	return orig->bit_size == new->bit_size &&
+	       orig->bit_offset == new->bit_offset;
 }
 
 static struct symbol *base_type(struct symbol *node, unsigned long *modp, unsigned long *asp)