d81f48fb390cf1e16435178d794fdcfc17dc7ff9
Dan Carpenter <dan.carpenter@oracle.com>
2014-02-21 16:08:56 +0300

    avl: remove some white space
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- avl.c
+++ avl.c
@@ -1,16 +1,16 @@
 /*
  * Copyright (C) 2010 Joseph Adams <joeyadams3.14159@gmail.com>
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -65,9 +65,9 @@ static int sign(int cmp)
 AVL *avl_new(AvlCompare compare)
 {
 	AVL *avl = malloc(sizeof(*avl));
-	
+
 	assert(avl != NULL);
-	
+
 	avl->compare = compare;
 	avl->root = NULL;
 	avl->count = 0;
@@ -106,9 +106,9 @@ bool avl_insert(AVL *avl, const void *key, const void *value)
 bool avl_remove(AVL *avl, const void *key)
 {
 	AvlNode *node = NULL;
-	
+
 	remove(avl, &avl->root, key, &node);
-	
+
 	if (node == NULL) {
 		return false;
 	} else {
@@ -120,9 +120,9 @@ bool avl_remove(AVL *avl, const void *key)
 static AvlNode *mkNode(const void *key, const void *value)
 {
 	AvlNode *node = malloc(sizeof(*node));
-	
+
 	assert(node != NULL);
-	
+
 	node->key = key;
 	node->value = value;
 	node->lr[0] = NULL;
@@ -143,12 +143,12 @@ static void freeNode(AvlNode *node)
 static AvlNode *lookup(const AVL *avl, AvlNode *node, const void *key)
 {
 	int cmp;
-	
+
 	if (node == NULL)
 		return NULL;
-	
+
 	cmp = avl->compare(key, node->key);
-	
+
 	if (cmp < 0)
 		return lookup(avl, node->lr[0], key);
 	if (cmp > 0)
@@ -170,16 +170,16 @@ static bool insert(AVL *avl, AvlNode **p, const void *key, const void *value)
 	} else {
 		AvlNode *node = *p;
 		int      cmp  = sign(avl->compare(key, node->key));
-		
+
 		if (cmp == 0) {
 			node->key = key;
 			node->value = value;
 			return false;
 		}
-		
+
 		if (!insert(avl, &node->lr[side(cmp)], key, value))
 			return false;
-		
+
 		/* If tree's balance became -1 or 1, it means the tree's height grew due to insertion. */
 		return sway(p, cmp) != 0;
 	}
@@ -199,47 +199,47 @@ static bool remove(AVL *avl, AvlNode **p, const void *key, AvlNode **ret)
 	} else {
 		AvlNode *node = *p;
 		int      cmp  = sign(avl->compare(key, node->key));
-		
+
 		if (cmp == 0) {
 			*ret = node;
 			avl->count--;
-			
+
 			if (node->lr[0] != NULL && node->lr[1] != NULL) {
 				AvlNode *replacement;
 				int      side;
 				bool     shrunk;
-				
+
 				/* Pick a subtree to pull the replacement from such that
 				 * this node doesn't have to be rebalanced. */
 				side = node->balance <= 0 ? 0 : 1;
-				
+
 				shrunk = removeExtremum(&node->lr[side], 1 - side, &replacement);
-				
+
 				replacement->lr[0]   = node->lr[0];
 				replacement->lr[1]   = node->lr[1];
 				replacement->balance = node->balance;
 				*p = replacement;
-				
+
 				if (!shrunk)
 					return false;
-				
+
 				replacement->balance -= bal(side);
-				
+
 				/* If tree's balance became 0, it means the tree's height shrank due to removal. */
 				return replacement->balance == 0;
 			}
-			
+
 			if (node->lr[0] != NULL)
 				*p = node->lr[0];
 			else
 				*p = node->lr[1];
-			
+
 			return true;
-			
+
 		} else {
 			if (!remove(avl, &node->lr[side(cmp)], key, ret))
 				return false;
-			
+
 			/* If tree's balance became 0, it means the tree's height shrank due to removal. */
 			return sway(p, -cmp) == 0;
 		}
@@ -258,16 +258,16 @@ static bool remove(AVL *avl, AvlNode **p, const void *key, AvlNode **ret)
 static bool removeExtremum(AvlNode **p, int side, AvlNode **ret)
 {
 	AvlNode *node = *p;
-	
+
 	if (node->lr[side] == NULL) {
 		*ret = node;
 		*p = node->lr[1 - side];
 		return true;
 	}
-	
+
 	if (!removeExtremum(&node->lr[side], side, ret))
 		return false;
-	
+
 	/* If tree's balance became 0, it means the tree's height shrank due to removal. */
 	return sway(p, -bal(side)) == 0;
 }
@@ -287,7 +287,7 @@ static int sway(AvlNode **p, int sway)
 		(*p)->balance += sway;
 	else
 		balance(p, side(sway));
-	
+
 	return (*p)->balance;
 }
 
@@ -303,34 +303,34 @@ static void balance(AvlNode **p, int side)
 	         *child = node->lr[side];
 	int opposite    = 1 - side;
 	int bal         = bal(side);
-	
+
 	if (child->balance != -bal) {
 		/* Left-left (side == 0) or right-right (side == 1) */
 		node->lr[side]      = child->lr[opposite];
 		child->lr[opposite] = node;
 		*p = child;
-		
+
 		child->balance -= bal;
 		node->balance = -child->balance;
-		
+
 	} else {
 		/* Left-right (side == 0) or right-left (side == 1) */
 		AvlNode *grandchild = child->lr[opposite];
-		
+
 		node->lr[side]           = grandchild->lr[opposite];
 		child->lr[opposite]      = grandchild->lr[side];
 		grandchild->lr[side]     = child;
 		grandchild->lr[opposite] = node;
 		*p = grandchild;
-		
+
 		node->balance       = 0;
 		child->balance      = 0;
-		
+
 		if (grandchild->balance == bal)
 			node->balance  = -bal;
 		else if (grandchild->balance == -bal)
 			child->balance = bal;
-		
+
 		grandchild->balance = 0;
 	}
 }
@@ -341,7 +341,7 @@ static void balance(AvlNode **p, int side)
 bool avl_check_invariants(AVL *avl)
 {
 	int    dummy;
-	
+
 	return checkBalances(avl->root, &dummy)
 	    && checkOrder(avl)
 	    && countNode(avl->root) == avl->count;
@@ -351,15 +351,15 @@ static bool checkBalances(AvlNode *node, int *height)
 {
 	if (node) {
 		int h0, h1;
-		
+
 		if (!checkBalances(node->lr[0], &h0))
 			return false;
 		if (!checkBalances(node->lr[1], &h1))
 			return false;
-		
+
 		if (node->balance != h1 - h0 || node->balance < -1 || node->balance > 1)
 			return false;
-		
+
 		*height = (h0 > h1 ? h0 : h1) + 1;
 		return true;
 	} else {
@@ -373,14 +373,14 @@ static bool checkOrder(AVL *avl)
 	AvlIter     i;
 	const void *last     = NULL;
 	bool        last_set = false;
-	
+
 	avl_foreach(i, avl) {
 		if (last_set && avl->compare(last, i.key) >= 0)
 			return false;
 		last     = i.key;
 		last_set = true;
 	}
-	
+
 	return true;
 }
 
@@ -398,22 +398,22 @@ static size_t countNode(AvlNode *node)
 void avl_iter_begin(AvlIter *iter, AVL *avl, AvlDirection dir)
 {
 	AvlNode *node = avl->root;
-	
+
 	iter->stack_index = 0;
 	iter->direction   = dir;
-	
+
 	if (node == NULL) {
 		iter->key      = NULL;
 		iter->value    = NULL;
 		iter->node     = NULL;
 		return;
 	}
-	
+
 	while (node->lr[dir] != NULL) {
 		iter->stack[iter->stack_index++] = node;
 		node = node->lr[dir];
 	}
-	
+
 	iter->key   = (void*) node->key;
 	iter->value = (void*) node->value;
 	iter->node  = node;
@@ -423,10 +423,10 @@ void avl_iter_next(AvlIter *iter)
 {
 	AvlNode     *node = iter->node;
 	AvlDirection dir  = iter->direction;
-	
+
 	if (node == NULL)
 		return;
-	
+
 	node = node->lr[1 - dir];
 	if (node != NULL) {
 		while (node->lr[dir] != NULL) {
@@ -441,7 +441,7 @@ void avl_iter_next(AvlIter *iter)
 		iter->node     = NULL;
 		return;
 	}
-	
+
 	iter->node  = node;
 	iter->key   = (void*) node->key;
 	iter->value = (void*) node->value;
--- avl.h
+++ avl.h
@@ -1,16 +1,16 @@
 /*
  * Copyright (C) 2010 Joseph Adams <joeyadams3.14159@gmail.com>
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -88,7 +88,7 @@ struct AvlIter {
 	void         *key;
 	void         *value;
 	AvlNode      *node;
-	
+
 	/* private */
 	AvlNode      *stack[100];
 	int           stack_index;
@@ -114,7 +114,7 @@ struct AVL {
 struct AvlNode {
 	const void *key;
 	const void *value;
-	
+
 	AvlNode    *lr[2];
 	int         balance; /* -1, 0, or 1 */
 };
