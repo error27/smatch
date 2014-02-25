3d00c5f4cb95feadf887fb2c1b07211d5797f066
Dan Carpenter <dan.carpenter@oracle.com>
2014-02-25 15:07:01 +0300

    start_states: partial update to stree
    I need to update all the places which call get_start_states() to finish
    this work.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- smatch_start_states.c
+++ smatch_start_states.c
@@ -26,33 +26,33 @@
 
 static int my_id;
 
-static struct state_list *start_states;
-static struct state_list_stack *saved_stack;
+static struct AVL *start_states;
+static struct stree_stack *saved_stack;
 static void save_start_states(struct statement *stmt)
 {
-	start_states = clone_slist(__get_cur_slist());
+	start_states = clone_stree(__get_cur_stree());
 }
 
 static void match_save_states(struct expression *expr)
 {
-	push_slist(&saved_stack, start_states);
+	push_stree(&saved_stack, start_states);
 	start_states = NULL;
 }
 
 static void match_restore_states(struct expression *expr)
 {
-	free_slist(&start_states);
-	start_states = pop_slist(&saved_stack);
+	free_stree(&start_states);
+	start_states = pop_stree(&saved_stack);
 }
 
 static void match_end_func(void)
 {
-	free_slist(&start_states);
+	free_stree(&start_states);
 }
 
 struct state_list *get_start_states(void)
 {
-	return start_states;
+	return stree_to_slist(start_states);
 }
 
 void register_start_states(int id)
