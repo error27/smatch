7ed702257441b6c56c62ebe7811621eb8a941f52
Dan Carpenter <dan.carpenter@oracle.com>
2014-05-14 12:50:03 +0300

    *new* check_no_if_block: sometimes the if block is missing
    For example, I found some code like this:
    
    	if (foo)
    		/* empty */
    	frob();
    
    Most of the code this finds is just crap code which isn't indented
    correctly.  There are some cases where the unusual indenting was done
    deliberately and eventually I may try to eliminate those false positives.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- check_list.h
+++ check_list.h
@@ -99,6 +99,7 @@ CK(check_cast_assign)
 CK(check_readl_infinite_loops)
 CK(check_double_checking)
 CK(check_unreachable)
+CK(check_no_if_block)
 
 /* <- your test goes here */
 /* CK(register_template) */
