e346ec4d6b2d46713043dd1ca24df58fa0e2f020
Dan Carpenter <dan.carpenter@oracle.com>
2014-08-29 13:45:15 +0300

    extra: record global declarations
    We are now saving an stree with global declarations.  No one is using it
    yet, but it should help when we have const values.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- smatch_extra.c
+++ smatch_extra.c
@@ -1642,6 +1642,7 @@ void register_smatch_extra_late(int id)
 	select_call_implies_hook(DEREFERENCE, &set_param_dereferenced);
 	add_hook(&match_function_call, FUNCTION_CALL_HOOK);
 	add_hook(&match_assign, ASSIGNMENT_HOOK);
+	add_hook(&match_assign, GLOBAL_ASSIGNMENT_HOOK);
 	add_hook(&unop_expr, OP_HOOK);
 	add_hook(&asm_expr, ASM_HOOK);
 
