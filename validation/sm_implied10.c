836c87a862137eee6e47a04173081fb0fe73ca57
Dan Carpenter <dan.carpenter@oracle.com>
2014-12-23 17:27:03 +0300

    validation: update to current pointer ranges 0,4096-2117777777777777777
    These days valid pointers generally go from 4096-2117777777777777777.  That
    way "ptr++" is another valid pointer instead of potentially wrapping to
    NULL.
    
    Update the validation/ directory to match.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- validation/sm_implied10.c
+++ validation/sm_implied10.c
@@ -22,8 +22,8 @@ void func(int *y)
  * check-command: smatch -I.. -m64 sm_implied10.c
  *
  * check-output-start
-sm_implied10.c:10 func() y = 0,4096-s64max
-sm_implied10.c:12 func() y = 4096-s64max
+sm_implied10.c:10 func() y = 0,4096-2117777777777777777
+sm_implied10.c:12 func() y = 4096-2117777777777777777
 sm_implied10.c:15 func() offset = s32min-s32max
 sm_implied10.c:17 func() offset = s32min-9
  * check-output-end
