305ea4ab0a22e12917efee4cb3bd91bd510ccba8
Dan Carpenter <dan.carpenter@oracle.com>
2015-03-30 13:10:47 +0300

    struct_assignment: save a lot more information
    This was not saving nested struct members like foo->bar-baz.  Also unions
    members were not saved.
    
    The disadvantage of this change is that it means smatch uses more memory
    and is slower.  I have worked pretty hard to cut down on memory usage to
    prepare for this change, and I've been running this code myself for a
    while and it basically works.  In the old code we saved memory but by
    ignoring stuff randomly, the right way is to ignore stuff in a deliberate
    way if necessary.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- smatch_struct_assignment.c
+++ smatch_struct_assignment.c
@@ -80,12 +80,11 @@ static struct symbol *get_struct_type(struct expression *expr)
 	return NULL;
 }
 
-static struct expression *get_matching_member_expr(struct symbol *left_type, struct expression *right, struct symbol *left_member)
+static struct expression *get_right_base_expr(struct symbol *left_type, struct expression *right)
 {
 	struct symbol *struct_type;
-	int op = '.';
 
-	if (!left_member->ident)
+	if (!right)
 		return NULL;
 
 	struct_type = get_struct_type(right);
@@ -97,12 +96,13 @@ static struct expression *get_matching_member_expr(struct symbol *left_type, str
 	if (right->type == EXPR_PREOP && right->op == '&')
 		right = strip_expr(right->unop);
 
-	if (is_pointer(right)) {
-		right = deref_expression(right);
-		op = '*';
-	}
+	if (right->type == EXPR_CALL)
+		return NULL;
 
-	return member_expression(right, op, left_member->ident);
+	if (is_pointer(right))
+		right = deref_expression(right);
+
+	return right;
 }
 
 static struct expression *remove_addr(struct expression *expr)
@@ -122,13 +122,65 @@ struct expression *get_faked_expression(void)
 	return faked_expression;
 }
 
+static void set_inner_struct_members(int mode, struct expression *faked, struct expression *left, struct expression *right, struct symbol *member)
+{
+	struct expression *left_member, *right_member, *assign;
+	struct symbol *base = get_real_base_type(member);
+	struct symbol *tmp;
+
+	if (member->ident) {
+		left = member_expression(left, '.', member->ident);
+		if (mode != COPY_MEMSET && right)
+			right = member_expression(right, '.', member->ident);
+	}
+
+	FOR_EACH_PTR(base->symbol_list, tmp) {
+		struct symbol *type;
+
+		type = get_real_base_type(tmp);
+		if (!type)
+			continue;
+
+		if (type->type == SYM_ARRAY)
+			continue;
+		if (type->type == SYM_UNION || type->type == SYM_STRUCT) {
+			set_inner_struct_members(mode, faked, left, right, tmp);
+			continue;
+		}
+		if (!tmp->ident)
+			continue;
+
+		left_member = member_expression(left, '.', tmp->ident);
+
+		switch (mode) {
+		case COPY_NORMAL:
+		case COPY_MEMCPY:
+			if (right)
+				right_member = member_expression(right, '.', tmp->ident);
+			else
+				right_member = unknown_value_expression(left_member);
+			break;
+		case COPY_MEMSET:
+			right_member = right;
+			break;
+		}
+
+		assign = assign_expression(left_member, right_member);
+		__in_fake_assign++;
+		__split_expr(assign);
+		__in_fake_assign--;
+	} END_FOR_EACH_PTR(tmp);
+
+
+}
+
 static void __struct_members_copy(int mode, struct expression *faked,
 				  struct expression *left,
 				  struct expression *right)
 {
 	struct symbol *struct_type, *tmp, *type;
 	struct expression *left_member;
-	struct expression *right_member = NULL;
+	struct expression *right_member;
 	struct expression *assign;
 	int op = '.';
 
@@ -168,25 +220,43 @@ static void __struct_members_copy(int mode, struct expression *faked,
 		left = deref_expression(left);
 		op = '*';
 	}
+	if (mode != COPY_MEMSET)
+		right = get_right_base_expr(struct_type, right);
 
 	FOR_EACH_PTR(struct_type->symbol_list, tmp) {
 		type = get_real_base_type(tmp);
-		if (type && type->type == SYM_ARRAY)
+		if (!type)
+			continue;
+		if (type->type == SYM_ARRAY)
+			continue;
+
+		if (type->type == SYM_UNION || type->type == SYM_STRUCT) {
+			set_inner_struct_members(mode, faked, left, right, tmp);
+			continue;
+		}
+
+		if (!tmp->ident)
 			continue;
 
 		left_member = member_expression(left, op, tmp->ident);
+		right_member = NULL;
 
 		switch (mode) {
 		case COPY_NORMAL:
 		case COPY_MEMCPY:
-			right_member = get_matching_member_expr(struct_type, right, tmp);
+			if (right)
+				right_member = member_expression(right, op, tmp->ident);
+			else
+				right_member = unknown_value_expression(left_member);
 			break;
 		case COPY_MEMSET:
 			right_member = right;
 			break;
 		}
-		if (!right_member)
-			right_member = unknown_value_expression(left_member);
+		if (!right_member) {
+			sm_msg("internal.  No right member");
+			continue;
+		}
 		assign = assign_expression(left_member, right_member);
 		__in_fake_assign++;
 		__split_expr(assign);
