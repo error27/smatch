error27/smatch

commit 1dcfc9fdad6d68a13cd39815943361b64c4a0d6c

extra: shuffle match_assign a bit
This is a small cleanup.  It does change things slightly because I've
used strip_expr() instead of strip_parens() which changes how casts are
handled.  But I think it probably is an improvement.

Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

smatch_extra.c
@@ -499,18 +499,20 @@ static void match_assign(struct expression *expr)
 	sval_t right_min, right_max;
 	sval_t tmp;
 
-	if (is_condition(expr->right))
-		return;
 	left = strip_expr(expr->left);
+
+	right = strip_expr(expr->right);
+	while (right->type == EXPR_ASSIGNMENT && right->op == '=')
+		right = strip_expr(right->left);
+
+	if (is_condition(expr->right))
+		return; /* handled in smatch_condition.c */
+	if (expr->op == '=' && right->type == EXPR_CALL)
+		return; /* handled in smatch_function_hooks.c */
+
 	name = expr_to_var_sym(left, &sym);
 	if (!name)
 		return;
-	right = strip_parens(expr->right);
-	while (right->type == EXPR_ASSIGNMENT && right->op == '=')
-		right = strip_parens(right->left);
-
-	if (expr->op == '=' && strip_expr(right)->type == EXPR_CALL)
-		goto free;  /* these are handled in smatch_function_hooks.c */
 
 	left_type = get_type(left);
 	right_type = get_type(right);

commit 10fe85a486a5e587beb06165c0756fc8c035f44b

extra: handle x = (u8)y; correctly
If we do a truncate cast then that should be reflected when we handle
the assignment.  Currently we just strip the cast away.

When this was introduced, I said that I changed "how casts are handled" but
I think it is an "improvement".  Which is terribly vague and it's obviously
not correct to ignore casts...

Fixes: 1dcfc9fdad6d ('extra: shuffle match_assign a bit')
Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

smatch_extra.c
@@ -669,9 +669,9 @@ static void match_assign(struct expression *expr)
 
 	left = strip_expr(expr->left);
 
-	right = strip_expr(expr->right);
+	right = strip_parens(expr->right);
 	while (right->type == EXPR_ASSIGNMENT && right->op == '=')
-		right = strip_expr(right->left);
+		right = strip_parens(right->left);
 
 	if (expr->op == '=' && is_condition(expr->right))
 		return; /* handled in smatch_condition.c */

validation/sm_casts7.c
@@ -1,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include "check_debug.h"
+
+int a;
+int main(void)
+{
+	int x;
+
+	a = (unsigned short)x;
+	__smatch_implied(a);
+
+	return 0;
+}
+
+
+/*
+ * check-name: smatch: casts #7
+ * check-command: smatch -I.. sm_casts7.c
+ *
+ * check-output-start
+sm_casts7.c:11 main() implied: a = '0-65535'
+ * check-output-end
+ */