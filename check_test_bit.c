f0257afdb439eb0f1e30d7d0fa045c787065a6bc
Dan Carpenter <dan.carpenter@oracle.com>
2015-07-01 13:36:32 +0300

    test_bit: add set_bit() and clear_bit() functions
    Those two functions also assume a bit number instead of a mask.  This
    doesn't catch many new because because if you use set_bit() then you likely
    use test_bit() as well and we already have that.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- check_test_bit.c
+++ check_test_bit.c
@@ -47,5 +47,6 @@ void check_test_bit(int id)
 
 	add_function_hook("test_bit", &match_test_bit, NULL);
 	add_function_hook("variable_test_bit", &match_test_bit, NULL);
-
+	add_function_hook("set_bit", &match_test_bit, NULL);
+	add_function_hook("clear_bit", &match_test_bit, NULL);
 }
