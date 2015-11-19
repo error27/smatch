713d1be6f742861744f60bdb08dacba9aaafba91
Dan Carpenter <dan.carpenter@oracle.com>
2015-11-19 08:40:31 +0300

    *new* impossible_mask: complain if people do "if ((x & 2) == 1"
    David Binderman reported one of these bugs for a staging driver.  The code
    is clearly wrong but we don't know how to fix.  Let's track it in Smatch so
    we don't forget.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- check_list.h
+++ check_list.h
@@ -118,6 +118,7 @@ CK(check_indenting)
 CK(check_unreachable)
 CK(check_no_if_block)
 CK(check_buffer_too_small_for_struct)
+CK(check_impossible_mask)
 
 /* <- your test goes here */
 /* CK(register_template) */
