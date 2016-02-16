error27/smatch

commit 6e9820f295e2efe970024ed3dd3401c61dcf6746

states: __set_sm_cur_stree() should not change the fake_cur_stree()
The __set_sm_cur_stree() function is used internally to, among other
things, reset the cur_stree() back to the original state.  If it changes
the fake_cur_stree that means we assume the user set those settings which
is not what we want.

Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

smatch_states.c
@@ -247,9 +247,6 @@ void __set_sm_cur_stree(struct sm_state *sm)
 	if (unreachable())
 		return;
 
-	if (fake_cur_stree_stack)
-		overwrite_sm_state_stree_stack(&fake_cur_stree_stack, sm);
-
 	overwrite_sm_state_stree(&cur_stree, sm);
 }
 

commit 7fec8c11ae9d545004d6e94cea0ac33a892a3e87

implied: fix PARAM_LIMIT implications again
When we're figuring out PARAM_LIMIT implications then we want to set the
fake_cur_stree.  But I changed it so that __set_sm_cur_stree() doesn't do
that anymore.  Create a new function that *only* sets the fake_cur_stree.

I want to start being more specific in how setting specific strees works.

Fixes: 6e9820f295e2 ('states: __set_sm_cur_stree() should not change the fake_cur_stree()')
Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

smatch.h
@@ -553,6 +553,7 @@ void __merge_stree_into_cur(struct stree *stree);
 int unreachable(void);
 void __set_sm(struct sm_state *sm);
 void __set_sm_cur_stree(struct sm_state *sm);
+void __set_sm_fake_stree(struct sm_state *sm);
 void __use_orig_if_not_set(struct sm_state *sm);
 void __set_true_false_sm(struct sm_state *true_state,
 			struct sm_state *false_state);

smatch_implied.c
@@ -649,7 +649,7 @@ void param_limit_implications(struct expression *expr, int param, char *key, cha
 	separate_and_filter(sm, SPECIAL_EQUAL, rl, __get_cur_stree(), &implied_true, &implied_false);
 
 	FOR_EACH_SM(implied_true, tmp) {
-		__set_sm_cur_stree(tmp);
+		__set_sm_fake_stree(tmp);
 	} END_FOR_EACH_SM(tmp);
 
 

smatch_states.c
@@ -250,6 +250,32 @@ void __set_sm_cur_stree(struct sm_state *sm)
 	overwrite_sm_state_stree(&cur_stree, sm);
 }
 
+void __set_sm_fake_stree(struct sm_state *sm)
+{
+	if (read_only)
+		sm_msg("Smatch Internal Error: cur_stree is read only.");
+
+	if (option_debug ||
+	    strcmp(check_name(sm->owner), option_debug_check) == 0) {
+		struct smatch_state *s;
+
+		s = get_state(sm->owner, sm->name, sm->sym);
+		if (!s)
+			sm_msg("new state. name='%s' [%s] %s",
+				sm->name, check_name(sm->owner),
+				show_state(sm->state));
+		else
+			sm_msg("state change name='%s' [%s] %s => %s",
+				sm->name, check_name(sm->owner), show_state(s),
+				show_state(sm->state));
+	}
+
+	if (unreachable())
+		return;
+
+	overwrite_sm_state_stree_stack(&fake_cur_stree_stack, sm);
+}
+
 void __use_orig_if_not_set(struct sm_state *sm)
 {
 	if (read_only)