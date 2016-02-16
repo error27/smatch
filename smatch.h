error27/smatch

commit 6e9820f295e2efe970024ed3dd3401c61dcf6746

states: __set_sm_cur_stree() should not change the fake_cur_stree()
The __set_sm_cur_stree() function is used internally to, among other
things, reset the cur_stree() back to the original state.  If it changes
the fake_cur_stree that means we assume the user set those settings which
is not what we want.

Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

smatch_states.c
@@ -247,9 +247,6 @@ void __set_sm_cur_stree(struct sm_state *sm)
 	if (unreachable())
 		return;
 
-	if (fake_cur_stree_stack)
-		overwrite_sm_state_stree_stack(&fake_cur_stree_stack, sm);
-
 	overwrite_sm_state_stree(&cur_stree, sm);
 }
 

commit a54ac829a66d7a74e967cd3915091e03547606bf

implied: fall out from fake_cur_stree changes
This basically only affect check_leaks.c.  That check wants to set the true
false states but then the implication code overwrites it with something
less specific.  This wasn't an issue before when we used to set the cur
stree whenever we set the true path, but now it is.

Fixes: 6e9820f295e2 ('states: __set_sm_cur_stree() should not change the fake_cur_stree()')
Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

smatch.h
@@ -556,6 +556,7 @@ void __set_sm_cur_stree(struct sm_state *sm);
 void __use_orig_if_not_set(struct sm_state *sm);
 void __set_true_false_sm(struct sm_state *true_state,
 			struct sm_state *false_state);
+void __set_true_false_sm_if_not_already_set(struct sm_state *true_sm, struct sm_state *false_sm);
 void nullify_path(void);
 void __match_nullify_path_hook(const char *fn, struct expression *expr,
 			       void *unused);

smatch_implied.c
@@ -596,12 +596,12 @@ static void set_implied_states(struct expression *expr)
 	} END_FOR_EACH_SM(sm);
 
 	FOR_EACH_SM(saved_implied_true, sm) {
-		__set_true_false_sm(sm, NULL);
+		__set_true_false_sm_if_not_already_set(sm, NULL);
 	} END_FOR_EACH_SM(sm);
 	free_stree(&saved_implied_true);
 
 	FOR_EACH_SM(saved_implied_false, sm) {
-		__set_true_false_sm(NULL, sm);
+		__set_true_false_sm_if_not_already_set(NULL, sm);
 	} END_FOR_EACH_SM(sm);
 	free_stree(&saved_implied_false);
 }

smatch_slist.c
@@ -449,6 +449,19 @@ void overwrite_sm_state_stree_stack(struct stree_stack **stack,
 	push_stree(stack, stree);
 }
 
+void set_sm_state_stree_stack_if_not_already_set(struct stree_stack **stack,
+		struct sm_state *sm)
+{
+	struct stree *stree;
+
+	stree = pop_stree(stack);
+	if (get_state_stree(stree, sm->owner, sm->name, sm->sym))
+		goto push;
+	overwrite_sm_state_stree(&stree, sm);
+push:
+	push_stree(stack, stree);
+}
+
 struct sm_state *set_state_stree(struct stree **stree, int owner, const char *name,
 		     struct symbol *sym, struct smatch_state *state)
 {

smatch_slist.h
@@ -44,6 +44,8 @@ struct sm_state *get_sm_state_stree(struct stree *stree, int owner, const char *
 
 void overwrite_sm_state_stree(struct stree **stree, struct sm_state *sm);
 void overwrite_sm_state_stree_stack(struct stree_stack **stack, struct sm_state *sm);
+void set_sm_state_stree_stack_if_not_already_set(struct stree_stack **stack,
+		struct sm_state *sm);
 struct sm_state *set_state_stree(struct stree **stree, int owner, const char *name,
 		     struct symbol *sym, struct smatch_state *state);
 void set_state_stree_perm(struct stree **stree, int owner, const char *name,

smatch_states.c
@@ -479,6 +479,22 @@ void __set_true_false_sm(struct sm_state *true_sm, struct sm_state *false_sm)
 		overwrite_sm_state_stree_stack(&cond_false_stack, false_sm);
 }
 
+void __set_true_false_sm_if_not_already_set(struct sm_state *true_sm, struct sm_state *false_sm)
+{
+	if (unreachable())
+		return;
+
+	if (!cond_false_stack || !cond_true_stack) {
+		printf("Error:  missing true/false stacks\n");
+		return;
+	}
+
+	if (true_sm)
+		set_sm_state_stree_stack_if_not_already_set(&cond_true_stack, true_sm);
+	if (false_sm)
+		set_sm_state_stree_stack_if_not_already_set(&cond_false_stack, false_sm);
+}
+
 void nullify_path(void)
 {
 	free_stree(&cur_stree);