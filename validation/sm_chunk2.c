87fdb5104a1a67c86e116f1fdca649bfa1966b6d
Dan Carpenter <dan.carpenter@oracle.com>
2016-03-24 17:14:54 +0300

    validation/sm_chunk2.c: update validation test
    This test stopped working because Smatch became more accurate.
    Unfortunately, I see another bug in the test.  The code has never worked
    correctly on this so it's not a regression...
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- validation/sm_chunk2.c
+++ validation/sm_chunk2.c
@@ -5,10 +5,11 @@ void initialize(void *p);
 int main(int x)
 {
 	unsigned int aaa[10];
-	int y;
+	int y, z;
 
 	initialize(&aaa);
 	initialize(&y);
+	initialize(&z);
 
 	if (aaa[5] > 3)
 		return 0;
@@ -17,11 +18,11 @@ int main(int x)
 	__smatch_implied(aaa[5]);
 	aaa[y] = 10;
 	__smatch_implied(aaa[5]);
-	if (aaa[y] > 4)
+	if (aaa[z] > 4)
 		return 0;
-	__smatch_implied(aaa[y]);
-	y = 3;
-	__smatch_implied(aaa[y]);
+	__smatch_implied(aaa[z]);
+	z = 3;
+	__smatch_implied(aaa[z]);
 
 	return 0;
 }
@@ -31,10 +32,10 @@ int main(int x)
  * check-command: smatch -I.. sm_chunk2.c
  *
  * check-output-start
-sm_chunk2.c:16 main() implied: aaa[0] = '42'
-sm_chunk2.c:17 main() implied: aaa[5] = '0-3'
-sm_chunk2.c:19 main() implied: aaa[5] = '0-u32max'
-sm_chunk2.c:22 main() implied: aaa[y] = '0-4'
-sm_chunk2.c:24 main() implied: aaa[y] = '0-u32max'
+sm_chunk2.c:17 main() implied: aaa[0] = '42'
+sm_chunk2.c:18 main() implied: aaa[5] = '0-3'
+sm_chunk2.c:20 main() implied: aaa[5] = '0-u32max'
+sm_chunk2.c:23 main() implied: aaa[z] = '0-4'
+sm_chunk2.c:25 main() implied: aaa[z] = '0-u32max'
  * check-output-end
  */
