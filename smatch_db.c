error27/smatch

commit 3a882fb2024d5eae869a15a5bc8fc34f1ee8f737

db: fix bug in print_struct_members
I added a test to bail early if we are not dealing with struct members.

But the other test is a bugfix.  Say you have a state "foobar" and a
param "foo" then it would print that "$bar" was set.  We only want to deal
with struct members where "foo->bar" becomes "$-bar".  This bug is harmless
since the bogus output is ignored but it looks worrisome and is a waste.

Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

smatch_db.c
@@ -19,6 +19,7 @@
 #include <errno.h>
 #include <sqlite3.h>
 #include <unistd.h>
+#include <ctype.h>
 #include "smatch.h"
 #include "smatch_slist.h"
 #include "smatch_extra.h"
@@ -482,6 +483,7 @@ static void match_call_marker(struct expression *expr)
 static void print_struct_members(struct expression *call, struct expression *expr, int param, struct stree *stree,
 	void (*callback)(struct expression *call, int param, char *printed_name, struct sm_state *sm))
 {
+	struct symbol *type;
 	struct sm_state *sm;
 	char *name;
 	struct symbol *sym;
@@ -494,6 +496,10 @@ static void print_struct_members(struct expression *call, struct expression *exp
 		expr = strip_expr(expr->unop);
 		is_address = 1;
 	}
+
+	type = get_type(expr);
+	if (type && (type->type != SYM_PTR && type->type != SYM_STRUCT))
+		return;
 
 	name = expr_to_var_sym(expr, &sym);
 	if (!name || !sym)
@@ -511,6 +517,8 @@ static void print_struct_members(struct expression *call, struct expression *exp
 		} else if (sm->name[0] == '*' && strcmp(name, sm->name + 1) == 0) {
 			snprintf(printed_name, sizeof(printed_name), "*$");
 		} else if (strncmp(name, sm->name, len) == 0) {
+			if (isalnum(sm->name[len]))
+				continue;
 			if (is_address)
 				snprintf(printed_name, sizeof(printed_name), "$->%s", sm->name + len + 1);
 			else

commit c48abbe3c8f1162a413616e77c79a9d2f5ef1322

db: revert a "speedup"
I was trying to speed things up but I broke validation/sm_inline1.c

Fixes: 3a882fb2024d ('db: fix bug in print_struct_members')
Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

smatch_db.c
@@ -495,7 +495,6 @@ static void match_call_marker(struct expression *expr)
 static void print_struct_members(struct expression *call, struct expression *expr, int param, struct stree *stree,
 	void (*callback)(struct expression *call, int param, char *printed_name, struct sm_state *sm))
 {
-	struct symbol *type;
 	struct sm_state *sm;
 	char *name;
 	struct symbol *sym;
@@ -508,10 +507,6 @@ static void print_struct_members(struct expression *call, struct expression *exp
 		expr = strip_expr(expr->unop);
 		is_address = 1;
 	}
-
-	type = get_type(expr);
-	if (type && (type->type != SYM_PTR && type->type != SYM_STRUCT))
-		return;
 
 	name = expr_to_var_sym(expr, &sym);
 	if (!name || !sym)