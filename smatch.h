error27/smatch

commit 0384c14a42b8bda8e18390f2b6d1ca02bf2344c6

function_hooks: don't process condition functions twice
When we had a function that was used as a condition then it was first
processed as a normal function call then as part of a condition.  This is
a waste of time, but the real reason for this commit is because I want to
use the implications of the function call and the first processing was
overwriting them.

Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

smatch_function_hooks.c
@@ -887,9 +887,25 @@ static void db_return_states(struct expression *expr)
 	call_return_states_after_hooks();
 }
 
+static int is_condition_call(struct expression *expr)
+{
+	struct expression *tmp;
+
+	FOR_EACH_PTR_REVERSE(big_condition_stack, tmp) {
+		if (expr == tmp)
+			return 1;
+		if (tmp->pos.line < expr->pos.line)
+			return 0;
+	} END_FOR_EACH_PTR_REVERSE(tmp);
+
+	return 0;
+}
+
 static void db_return_states_call(struct expression *expr)
 {
 	if (is_assigned_call(expr))
+		return;
+	if (is_condition_call(expr))
 		return;
 	db_return_states(expr);
 }

commit 0d60a567521244c5757bfb288715f16a996dce49

hooks: rename CALL_HOOK_AFTER to CALL_HOOK_AFTER_DB and change it a bit
This is a bugfix.  Back in the day, when there was a function call used in
a condition we used to query the database twice and so
smatch_untracked_param.c worked but now we only do it once.  The problem is
that we call FUNCTION_CALL_HOOK_AFTER before we query the DB but that needs
to be after.

There is the only place that uses CALL_HOOK_AFTER and it really wants to
know what the states are after we'd queried the database.  So I have
renamed it and changed where it is called.

Fixes: 0384c14a42b8 ('function_hooks: don't process condition functions twice')
Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

smatch.h
@@ -108,7 +108,7 @@ enum hook_type {
 	WHOLE_CONDITION_HOOK,
 	FUNCTION_CALL_HOOK,
 	CALL_HOOK_AFTER_INLINE,
-	FUNCTION_CALL_HOOK_AFTER,
+	FUNCTION_CALL_HOOK_AFTER_DB,
 	CALL_ASSIGNMENT_HOOK,
 	MACRO_ASSIGNMENT_HOOK,
 	BINOP_HOOK,

smatch_flow.c
@@ -389,8 +389,6 @@ void __split_expr(struct expression *expr)
 		tmp = strip_expr(expr->right);
 		if (expr->op == '=' && tmp->type == EXPR_CALL) {
 			__pass_to_client(expr, CALL_ASSIGNMENT_HOOK);
-			if (!is_fake_call(tmp))
-				__pass_to_client(tmp, FUNCTION_CALL_HOOK_AFTER);
 		}
 		if (get_macro_name(tmp->pos) &&
 		    get_macro_name(expr->pos) != get_macro_name(tmp->pos))
@@ -465,8 +463,6 @@ void __split_expr(struct expression *expr)
 			parse_inline(expr);
 		}
 		__pass_to_client(expr, CALL_HOOK_AFTER_INLINE);
-		if (!is_assigned_call(expr))
-			__pass_to_client(expr, FUNCTION_CALL_HOOK_AFTER);
 		if (is_noreturn_func(expr->fn))
 			nullify_path();
 		break;

smatch_function_hooks.c
@@ -169,13 +169,14 @@ static void call_return_states_before_hooks(void)
 	} END_FOR_EACH_PTR(fn);
 }
 
-static void call_return_states_after_hooks(void)
+static void call_return_states_after_hooks(struct expression *expr)
 {
 	void_fn **fn;
 
 	FOR_EACH_PTR(return_states_after, fn) {
 		(*fn)();
 	} END_FOR_EACH_PTR(fn);
+	__pass_to_client(expr, FUNCTION_CALL_HOOK_AFTER_DB);
 }
 
 static int call_call_backs(struct call_back_list *list, int type,
@@ -592,7 +593,7 @@ static void compare_db_return_states_callbacks(struct expression *left, int comp
 	free_stree(&true_states);
 	free_stree(&false_states);
 
-	call_return_states_after_hooks();
+	call_return_states_after_hooks(call_expr);
 
 	FOR_EACH_SM(implied_true, sm) {
 		__set_true_false_sm(sm, NULL);
@@ -732,7 +733,7 @@ static int db_return_states_assign(struct expression *expr)
 	} END_FOR_EACH_SM(sm);
 
 	free_stree(&db_info.stree);
-	call_return_states_after_hooks();
+	call_return_states_after_hooks(right);
 
 	return handled;
 }
@@ -887,7 +888,7 @@ static void db_return_states(struct expression *expr)
 	} END_FOR_EACH_SM(sm);
 
 	free_stree(&db_info.stree);
-	call_return_states_after_hooks();
+	call_return_states_after_hooks(expr);
 }
 
 static int is_condition_call(struct expression *expr)

smatch_hooks.c
@@ -112,7 +112,7 @@ void add_hook(void *func, enum hook_type type)
 	case CALL_HOOK_AFTER_INLINE:
 		container->data_type = EXPR_PTR;
 		break;
-	case FUNCTION_CALL_HOOK_AFTER:
+	case FUNCTION_CALL_HOOK_AFTER_DB:
 		container->data_type = EXPR_PTR;
 		break;
 	case DEREF_HOOK:

smatch_untracked_param.c
@@ -246,7 +246,7 @@ void register_untracked_param(int id)
 
 	select_return_states_hook(INTERNAL, &assume_tracked);
 	select_return_states_hook(UNTRACKED_PARAM, &mark_untracked);
-	add_hook(&match_after_call, FUNCTION_CALL_HOOK_AFTER);
+	add_hook(&match_after_call, FUNCTION_CALL_HOOK_AFTER_DB);
 
 	add_split_return_callback(&print_untracked_params);
 