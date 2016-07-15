4a7ab79aa6554bae027462c7c292b97b6d40b81c
Dan Carpenter <dan.carpenter@oracle.com>
2016-07-15 17:12:37 +0300

    continue_vs_break: add it checklist.h
    Oops, I forgot.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- check_list.h
+++ check_list.h
@@ -129,6 +129,7 @@ CK(check_indenting)
 CK(check_unreachable)
 CK(check_no_if_block)
 CK(check_buffer_too_small_for_struct)
+CK(check_continue_vs_break)
 CK(check_impossible_mask)
 
 /* <- your test goes here */
