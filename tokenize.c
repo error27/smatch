4ea04a3a37c04b862152844170e371a8220664c6
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2017-02-07 18:45:07 +0100

    remove 'Escape' from token character class
    Now that diagnostic on unknown escape sequence
    have moved to post-preprocessing phase this is not more used.
    
    Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
    Tested-by: Stephen Boyd <sboyd@codeaurora.org>
    Signed-off-by: Christopher Li <sparse@chrisli.org>

--- tokenize.c
+++ tokenize.c
@@ -481,11 +481,10 @@ enum {
 	Dot = 16,
 	ValidSecond = 32,
 	Quote = 64,
-	Escape = 128,
 };
 
 static const long cclass[257] = {
-	['0' + 1 ... '7' + 1] = Digit | Hex | Escape,	/* \<octal> */
+	['0' + 1 ... '7' + 1] = Digit | Hex,	/* \<octal> */
 	['8' + 1 ... '9' + 1] = Digit | Hex,
 	['A' + 1 ... 'D' + 1] = Letter | Hex,
 	['E' + 1] = Letter | Hex | Exp,	/* E<exp> */
@@ -493,22 +492,22 @@ static const long cclass[257] = {
 	['G' + 1 ... 'O' + 1] = Letter,
 	['P' + 1] = Letter | Exp,	/* P<exp> */
 	['Q' + 1 ... 'Z' + 1] = Letter,
-	['a' + 1 ... 'b' + 1] = Letter | Hex | Escape, /* \a, \b */
+	['a' + 1 ... 'b' + 1] = Letter | Hex, /* \a, \b */
 	['c' + 1 ... 'd' + 1] = Letter | Hex,
-	['e' + 1] = Letter | Hex | Exp | Escape,/* \e, e<exp> */
-	['f' + 1] = Letter | Hex | Escape,	/* \f */
+	['e' + 1] = Letter | Hex | Exp,/* \e, e<exp> */
+	['f' + 1] = Letter | Hex,	/* \f */
 	['g' + 1 ... 'm' + 1] = Letter,
-	['n' + 1] = Letter | Escape,	/* \n */
+	['n' + 1] = Letter,	/* \n */
 	['o' + 1] = Letter,
 	['p' + 1] = Letter | Exp,	/* p<exp> */
 	['q' + 1] = Letter,
-	['r' + 1] = Letter | Escape,	/* \r */
+	['r' + 1] = Letter,	/* \r */
 	['s' + 1] = Letter,
-	['t' + 1] = Letter | Escape,	/* \t */
+	['t' + 1] = Letter,	/* \t */
 	['u' + 1] = Letter,
-	['v' + 1] = Letter | Escape,	/* \v */
+	['v' + 1] = Letter,	/* \v */
 	['w' + 1] = Letter,
-	['x' + 1] = Letter | Escape,	/* \x<hex> */
+	['x' + 1] = Letter,	/* \x<hex> */
 	['y' + 1 ... 'z' + 1] = Letter,
 	['_' + 1] = Letter,
 	['.' + 1] = Dot | ValidSecond,
@@ -520,10 +519,8 @@ static const long cclass[257] = {
 	['&' + 1] = ValidSecond,
 	['|' + 1] = ValidSecond,
 	['#' + 1] = ValidSecond,
-	['\'' + 1] = Quote | Escape,
-	['"' + 1] = Quote | Escape,
-	['\\' + 1] = Escape,
-	['?' + 1] = Escape,
+	['\'' + 1] = Quote,
+	['"' + 1] = Quote,
 };
 
 /*
