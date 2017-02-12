cac6d8d34ba6f1617b69134bf7528f4a9768097b
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2017-02-13 00:28:52 +0100

    testsuite: make tests known to fail effectively fail
    These tests must fail because some errors which should
    be detected are not.
    But the test suite lack to see that because those tests
    don't have the check-error-start/end section.
    
    Add such a section (with madeup error message) so that the
    testsuite now properly see and report the failure.
    
    Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
    Signed-off-by: Christopher Li <sparse@chrisli.org>

--- validation/badtype1.c
+++ validation/badtype1.c
@@ -3,4 +3,8 @@ static void foo(enum bar baz);
 /*
  * check-name: enum not in scope
  * check-known-to-fail
+ *
+ * check-error-start
+badtype1.c:1:22: warning: bad scope for 'enum bar'
+ * check-error-end
  */
