d1b011ec36bf6763a8c87664fc466ada2a7eb6db
Dan Carpenter <dan.carpenter@oracle.com>
2017-05-08 12:11:32 +0300

    free: don't complain when people do frob(&freed_variable)
    They're probably going to set the freed variable so don't complain when
    people pass the address of it.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- check_free.c
+++ check_free.c
@@ -58,6 +58,12 @@ static void match_symbol(struct expression *expr)
 	if (is_impossible_path())
 		return;
 
+	parent = expr_get_parent_expr(expr);
+	while (parent && parent->type == EXPR_PREOP && parent->op == '(')
+		parent = expr_get_parent_expr(parent);
+	if (parent && parent->type == EXPR_PREOP && parent->op == '&')
+		return;
+
 	if (!is_freed(expr))
 		return;
 	name = expr_to_var(expr);
