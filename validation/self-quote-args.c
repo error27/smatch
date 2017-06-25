5fec039b49c135e9bd56efd413d5273f2ee734dd
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2017-06-25 21:51:06 +0200

    testsuite: add test case for quoting of command's arguments
    Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

---
+++ validation/self-quote-args.c
@@ -0,0 +1,8 @@
+/*
+ * check-name: self-quote-args
+ * check-description: This is testing that the test-suite
+ *	respect the quoting of the command's arguments.
+ * check-command: sparse '-foption with-spaces' empty-file
+ * check-known-to-fail
+ * check-output-ignore
+ */
