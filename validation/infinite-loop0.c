d9ff34f2e0d52351ec9e5c5b33002b7be3937123
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2017-08-09 13:22:17 +0200

    testsuite: add support for commands with timeout
    Tests taking a huge or infinite amout of time is
    a problem for the testsuite. Furthermore, it's most
    often the sign of a problem somewhere.
    
    Fix this by adding some support to use an optional
    timeout for each testcase command.
    
    Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

---
+++ validation/infinite-loop0.c
@@ -0,0 +1,12 @@
+void foo(void)
+{
+        int a = a || 0;
+        if (a) ;
+}
+
+/*
+ * check-name: internal infinite loop (0)
+ * check-command: sparse -Wno-decl $file
+ * check-timeout:
+ * check-known-to-fail
+ */
