c18aec74fd9fd77f2234dd11dd6444339c66d99e
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2017-08-20 23:45:38 +0200

    add testcase for constant bitfield dereference
    Reported-by: Dibyendu Majumdar <mobile@majumdar.org.uk>
    Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

---
+++ validation/linear/bitfield-expand-deref.c
@@ -0,0 +1,28 @@
+struct s {
+	int a:8;
+	int b:8;
+};
+
+int foo(void)
+{
+	struct s x = { .a = 12, .b = 34, };
+
+	return x.b;
+}
+
+int bar(int a)
+{
+	struct s x = { .a = 12, .b = a, };
+
+	return x.b;
+}
+
+/*
+ * check-name: bitfield expand deref
+ * check-command: test-linearize -Wno-decl $file
+ * check-known-to-fail
+ *
+ * check-output-ignore
+ * check-output-excludes: ret\..*\$12
+ * check-output-contains: ret\..*\$34
+ */
