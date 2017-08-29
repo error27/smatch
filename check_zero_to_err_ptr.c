be7a8132719b02be173c5bc34149be39059b64d0
Dan Carpenter <dan.carpenter@oracle.com>
2017-08-29 14:58:57 +0300

    zero_to_err_ptr: make this check more strict
    Before it only complained if you did something like this:
    
    	if (!foo)
    		return PTR_ERR(foo);
    
    But now it it complains if you merge a bunch of values together and one of
    them is NULL.  Or if you pass a valid pointer to PTR_ERR() that will also
    generate a warning.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- check_zero_to_err_ptr.c
+++ check_zero_to_err_ptr.c
@@ -16,20 +16,103 @@
  */
 
 #include "smatch.h"
+#include "smatch_extra.h"
+#include "smatch_slist.h"
 
 static int my_id;
+
+static int is_comparison_call(struct expression *expr)
+{
+	expr = expr_get_parent_expr(expr);
+	if (!expr || expr->type != EXPR_COMPARE)
+		return 0;
+	if (expr->op != SPECIAL_EQUAL && expr->op != SPECIAL_NOTEQUAL)
+		return 0;
+	return 1;
+}
+
+static int next_line_is_if(struct expression *expr)
+{
+	struct expression *next;
+
+	if (!__next_stmt || __next_stmt->type != STMT_IF)
+		return 0;
+
+	next = strip_expr(__next_stmt->if_conditional);
+	while (next->type == EXPR_PREOP && next->op == '!')
+		next = strip_expr(next->unop);
+	if (expr_equiv(expr, next))
+		return 1;
+	return 0;
+}
+
+static int next_line_checks_IS_ERR(struct expression *arg)
+{
+	struct expression *next;
+
+	if (!__next_stmt || __next_stmt->type != STMT_IF)
+		return 0;
+
+	next = strip_expr(__next_stmt->if_conditional);
+	while (next->type == EXPR_PREOP && next->op == '!')
+		next = strip_expr(next->unop);
+	if (!next || next->type != EXPR_CALL)
+		return 0;
+	if (next->fn->type != EXPR_SYMBOL || !next->fn->symbol->ident ||
+	    (strcmp(next->fn->symbol->ident->name, "IS_ERR") != 0 &&
+	     strcmp(next->fn->symbol->ident->name, "IS_ERR_OR_NULL") != 0))
+		return 0;
+	next = get_argument_from_call_expr(next->args, 0);
+	return expr_equiv(next, arg);
+}
+
+static int is_valid_ptr(sval_t sval)
+{
+	if (sval.type == &int_ctype &&
+	    (sval.value == INT_MIN || sval.value == INT_MAX))
+		return 0;
+
+	if (sval_cmp(valid_ptr_min_sval, sval) <= 0 &&
+	    sval_cmp(valid_ptr_max_sval, sval) >= 0)
+		return 1;
+	return 0;
+}
 
 static void match_err_ptr(const char *fn, struct expression *expr, void *data)
 {
 	struct expression *arg_expr;
+	struct sm_state *sm, *tmp;
 	sval_t sval;
 
 	arg_expr = get_argument_from_call_expr(expr->args, 0);
-	if (!get_implied_value(arg_expr, &sval))
+	sm = get_sm_state_expr(SMATCH_EXTRA, arg_expr);
+	if (!sm)
 		return;
-	if (sval.value != 0)
+
+	if (is_comparison_call(expr))
 		return;
-	sm_msg("warn: passing zero to '%s'", fn);
+
+	if (next_line_checks_IS_ERR(arg_expr))
+		return;
+	if (strcmp(fn, "ERR_PTR") == 0 &&
+	    next_line_is_if(arg_expr))
+		return;
+
+	FOR_EACH_PTR(sm->possible, tmp) {
+		if (!estate_rl(tmp->state))
+			continue;
+		if (is_valid_ptr(estate_min(tmp->state)) &&
+		    is_valid_ptr(estate_max(tmp->state))) {
+			sm_msg("warn: passing a valid pointer to '%s'", fn);
+			return;
+		}
+		if (!rl_to_sval(estate_rl(tmp->state), &sval))
+			continue;
+		if (sval.value != 0)
+			continue;
+		sm_msg("warn: passing zero to '%s'", fn);
+		return;
+	} END_FOR_EACH_PTR(tmp);
 }
 
 void check_zero_to_err_ptr(int id)
