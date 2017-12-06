error27/smatch

commit f1e4ba13d1499407a72349b50052ae818c8d8553

build: disable sparse-llvm on non-x86
sparse-llvm doesn't have support for targets other than
x86 or x86-64.

Disable sparse-llvm on other archs as it create problems during
build, selftest, ...

Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
Signed-off-by: Christopher Li <sparse@chrisli.org>

Makefile
@@ -90,6 +90,7 @@ $(warning Your system does not have gtk3/gtk2, disabling test-inspect)
 endif
 
 ifeq ($(HAVE_LLVM),yes)
+ifeq ($(shell uname -m | grep -q '\(i386\|x86\)' && echo ok),ok)
 LLVM_VERSION:=$(shell $(LLVM_CONFIG) --version)
 ifeq ($(shell expr "$(LLVM_VERSION)" : '[3-9]\.'),2)
 LLVM_PROGS := sparse-llvm
@@ -104,6 +105,9 @@ sparse-llvm.o: BASIC_CFLAGS += $(LLVM_CFLAGS)
 sparse-llvm_EXTRA_OBJS := $(LLVM_LIBS) $(LLVM_LDFLAGS)
 else
 $(warning LLVM 3.0 or later required. Your system has version $(LLVM_VERSION) installed.)
+endif
+else
+$(warning sparse-llvm disabled on $(shell uname -m))
 endif
 else
 $(warning Your system does not have llvm, disabling sparse-llvm)

commit 8d781d16644e826476b6f410acd5a48e1d7b98d6

fix: build sparse-llvm on i686s too.
In commit "f1e4ba13d (build: disable sparse-llvm on non-x86)",
we wanted to disable the build of sparse-llvm on non-x86 archs
but the pattern used also excluded machines like 'i686'.

Fix this by using the pattern 'i[3456]86' instead.

Fixes: f1e4ba13d1499407a72349b50052ae818c8d8553
Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

Makefile
@@ -142,7 +142,7 @@ endif
 LLVM_CONFIG:=llvm-config
 HAVE_LLVM:=$(shell $(LLVM_CONFIG) --version >/dev/null 2>&1 && echo 'yes')
 ifeq ($(HAVE_LLVM),yes)
-ifeq ($(shell uname -m | grep -q '\(i386\|x86\)' && echo ok),ok)
+ifeq ($(shell uname -m | grep -q '\(i[3456]86\|x86\)' && echo ok),ok)
 LLVM_VERSION:=$(shell $(LLVM_CONFIG) --version)
 ifeq ($(shell expr "$(LLVM_VERSION)" : '[3-9]\.'),2)
 LLVM_PROGS := sparse-llvm