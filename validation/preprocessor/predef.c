b6f0362baae2c8fdd5ef42eaae3bb05f8bcd8fea
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2017-12-09 15:53:55 +0100

    testsuite: test predef macros on LP32/LP64/LLP64
    Now these tests should succeed and be meaningful on
    all archs.
    
    Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

---
+++ validation/preprocessor/predef.c
@@ -0,0 +1,42 @@
+#define BITS(T)		(sizeof(T) * 8)
+#define SIGN_BIT(T)	(1ULL << (BITS(T) - 1))
+#define SMASK(T)	(SIGN_BIT(T) - 1)
+#define UMASK(T)	(SIGN_BIT(T) | SMASK(T))
+
+int test(void);
+int test(void)
+{
+#define TEST_BIT(X, T)	if (__ ## X ## _BIT__  != BITS(T)) return 1
+	TEST_BIT(CHAR, char);
+
+#define TEST_MAX(X, M)	if (__ ## X ## _MAX__ != M) return 1
+#define TEST_SMAX(X, T)	TEST_MAX(X, SMASK(T))
+#define TEST_UMAX(X, T)	TEST_MAX(X, UMASK(T))
+	TEST_SMAX(SCHAR, signed char);
+	TEST_SMAX(SHRT, short);
+	TEST_SMAX(INT, int);
+	TEST_SMAX(LONG, long);
+	TEST_SMAX(LONG_LONG, long long);
+
+#define TEST_SIZEOF(X, T) if (__SIZEOF_ ## X ## __ != sizeof(T)) return 1
+	TEST_SIZEOF(SHORT, short);
+	TEST_SIZEOF(INT, int);
+	TEST_SIZEOF(LONG, long);
+	TEST_SIZEOF(LONG_LONG, long long);
+	TEST_SIZEOF(INT128, __int128);
+	TEST_SIZEOF(SIZE_T, __SIZE_TYPE__);
+	TEST_SIZEOF(POINTER, void*);
+	TEST_SIZEOF(FLOAT, float);
+	TEST_SIZEOF(DOUBLE, double);
+	TEST_SIZEOF(LONG_DOUBLE, long double);
+
+	return 0;
+}
+
+/*
+ * check-name: predefined macros: __SIZEOF_<type>__, ...
+ * check-command: test-linearize -Wno-decl $file
+ * check-output-ignore
+ *
+ * check-output-contains: ret\\..*\\$0
+ */
