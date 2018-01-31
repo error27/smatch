35e71b0279ae0b58518a52bf21ee66b57f965c0f
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2018-01-31 11:20:33 +0100

    remove warning "call with no type"
    This warning is issued when trying to linearize a call
    expression with a null expr->ctype. However, such null
    ctypes are a consequence of earlier errors detected
    during evaluation and for which a warning, specific
    about the nature of the problem, have already been issued.
    
    In short, this "call with no type" is non-informative
    and redundant, so avoid useless noise and remove this
    warning.
    
    Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

--- linearize.c
+++ linearize.c
@@ -1273,10 +1273,8 @@ static pseudo_t linearize_call_expression(struct entrypoint *ep, struct expressi
 	struct symbol *fntype;
 	struct context *context;
 
-	if (!expr->ctype) {
-		warning(expr->pos, "call with no type!");
+	if (!expr->ctype)
 		return VOID;
-	}
 
 	fn = expr->fn;
 	fntype = fn->ctype;
