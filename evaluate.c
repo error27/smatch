error27/smatch

commit b5867a33b62c04811784c6fc233c601a4f2b0841

fix evaluation of a function or array symbol in conditionals
Functions and arrays degenerate as pointers in most context
but this wasn't done in the case of conditionals.

As a result the value of the conditional after linearization
was invalid.

Fix this by calling degenerate() in evaluate_conditional().

Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

evaluate.c
@@ -881,6 +881,7 @@ static struct symbol *evaluate_conditional(struct expression *expr, int iterator
 			ctype = NULL;
 		}
 	}
+	ctype = degenerate(expr);
 
 	return ctype;
 }

validation/cond-address.c
@@ -1,0 +1,14 @@
+extern void f(void);
+extern int a[];
+
+int foo(void) { if (f) return 1; return 0; }
+int bar(void) { if (a) return 1; return 0; }
+int qux(void) { if (f && a) return 1; return 0; }
+
+/*
+ * check-name: cond-address.c
+ * check-command: test-linearize -Wno-decl $file
+ * check-output-ignore
+ *
+ * check-excludes: VOID
+ */

commit 0687a6f961540a472161b183cc927262454d2186

fix error in bad conditional
Commit "b5867a33b (fix evaluation of a function or array symbol in conditionals)"
added a missing call to degenerate(epxr) but this must not be done
if the expression is erroneous.

fix this by bypassing the call to degenerate() if the ctype is NULL.

Fixes: b5867a33b62c04811784c6fc233c601a4f2b0841
Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

evaluate.c
@@ -894,7 +894,9 @@ static struct symbol *evaluate_conditional(struct expression *expr, int iterator
 			ctype = NULL;
 		}
 	}
-	ctype = degenerate(expr);
+
+	if (ctype)
+		ctype = degenerate(expr);
 
 	return ctype;
 }

validation/bad-type-twice0.c
@@ -5,7 +5,6 @@ static int foo(a)
 
 /*
  * check-name: bad-type-twice0
- * check-known-to-fail
  *
  * check-error-start
 bad-type-twice0.c:3:16: error: incorrect type in conditional