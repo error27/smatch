88fbab0c7465b682c386f759d4b6ecc3339cd08e
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2018-03-11 10:41:58 +0100

    optim: add some more optimization tests
    Add some tests showing missed optimization opportunities.
    
    Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

---
+++ validation/optim/dup-cond0.c
@@ -0,0 +1,20 @@
+struct s {
+	int f;
+};
+
+static int foo(struct s *s)
+{
+	if (s->f)
+		return 0;
+	else if (!s->f)
+		return 4;
+}
+
+/*
+ * check-name: dup-cond0
+ * check-command: test-linearize -Wno-decl $file
+ * check-known-to-fail
+ *
+ * check-output-ignore
+ * check-output-contains: select
+ */
