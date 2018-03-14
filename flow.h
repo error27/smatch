error27/smatch

commit 917d37ad7681c88fc4d8df484f7b10d550190df8

introduce REPEAT_CFG_CLEANUP
Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

flow.h
@@ -7,6 +7,7 @@ extern unsigned long bb_generation;
 
 #define REPEAT_CSE		1
 #define REPEAT_SYMBOL_CLEANUP	2
+#define REPEAT_CFG_CLEANUP	3
 
 struct entrypoint;
 struct instruction;

commit 0e49c8b5a06e95b10b6e3986535325edf319fbb5

optim: fix REPEAT_CFG_CLEANUP
The REPEAT_... flags are used as bitfield but the last one
introduced (by myself): REPEAT_CFG_CLEANUP was wrongly defined
to the next value instead of the next bit.

Fix this by defining it to the right value.
Also change all the definition to use '1 << ...' to clearly
show we expect uniq bit values here.

Fixes: 917d37ad7681c88fc4d8df484f7b10d550190df8
Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

flow.h
@@ -5,9 +5,9 @@
 
 extern unsigned long bb_generation;
 
-#define REPEAT_CSE		1
-#define REPEAT_SYMBOL_CLEANUP	2
-#define REPEAT_CFG_CLEANUP	3
+#define REPEAT_CSE		(1 << 0)
+#define REPEAT_SYMBOL_CLEANUP	(1 << 1)
+#define REPEAT_CFG_CLEANUP	(1 << 2)
 
 struct entrypoint;
 struct instruction;