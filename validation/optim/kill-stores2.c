80b345a468092d78a422ee0c1cdaad46d0f6fa81
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2018-03-18 15:16:11 +0100

    kds: add testcases for kill_dead_stores()
    Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

---
+++ validation/optim/kill-stores2.c
@@ -0,0 +1,18 @@
+extern void def(int *);
+
+static void foo(void)
+{
+	int c;
+	def(&c);
+	if (c)
+		c = c;
+}
+
+/*
+ * check-name: kill-stores2
+ * check-command: test-linearize $file
+ * check-known-to-fail
+ *
+ * check-output-ignore
+ * check-output-excludes: store\\.
+ */
