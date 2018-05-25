error27/smatch

commit 215c7ada006d3badc4fc28bd1b8f5f03fddf04b3

return_to_param: fix a crashing bug
This started happening when I upgraded to the latest Sparse.

So I'm not totally sure why I did this HACK ALERT...  And I also can't
swear that this fix is correct because the test cases aren't there...

But what happens is that we use the left_sym, because it gets passed to
smatch_extra mod expression.  So the hack says we don't use it but that's
wrong.  Then check_rosenberg.c hooks into smatch_extra and looks up the
type of the symbol which doesn't work since it's an expression pointer.
And it cause a crash.

I think this fix will work 99 times out of a hundred and it's probably
harmless the remaining time.

Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

smatch_return_to_param.c
@@ -256,17 +256,14 @@ void __add_return_to_param_mapping(struct expression *expr, const char *return_s
 	    expr_get_parent_stmt(expr) &&
 	    expr_get_parent_stmt(expr)->type == STMT_RETURN) {
 		call = strip_expr(expr);
+		left_sym = expr_to_sym(call->fn);
+		if (!left_sym)
+			return;
 		left_name = expr_to_str(call);
 		if (!left_name)
-			goto free;
+			return;
 
-		/*
-		 * HACK ALERT: The symbol pointer is basically used as a cookie
-		 * and not used as a pointer so we can pass expr here without
-		 * causing an issue.
-		 *
-		 */
-		store_mapping_helper(left_name, (struct symbol *)expr, call, return_string);
+		store_mapping_helper(left_name, left_sym, call, return_string);
 		goto free;
 
 	}

commit df66df1ede596eeaef87cfdf7481ef05ba421a72

return_to_param: fix parsing calls
This is to handle code like spi_get_drvdata().  I think that it
used to partially work, but then I broke it in commit 215c7ada006d
("return_to_param: fix a crashing bug").

The problem there was that I was using an expression pointer instead
of a sym pointer for the sm_state.  That caused a use after free.
The fix was to use the sym from the call->fn instead.  But I never
updated the get_state() call so that was still using the expression
pointer so it didn't work.

But even before I broke it, the code to add to combine "&spi->dev"
with "$0->driver_data" was buggy.  These days we have a function to
take a string and convert it to an expression which works for
spi_get_drvdata().  So I've used that instead.

Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

smatch_return_to_param.c
@@ -172,16 +172,22 @@ char *map_long_to_short_name_sym_nostack(const char *name, struct symbol *sym, s
 char *map_call_to_param_name_sym(struct expression *expr, struct symbol **sym)
 {
 	char *name;
+	struct symbol *start_sym;
 	struct smatch_state *state;
 
 	*sym = NULL;
 
-	name = expr_to_str(expr);
+	name = expr_to_str_sym(expr, &start_sym);
 	if (!name)
 		return NULL;
-	state = get_state(my_id, name, (struct symbol *)expr);
+	if (expr->type == EXPR_CALL)
+		start_sym = expr_to_sym(expr->fn);
+
+	state = get_state(my_id, name, start_sym);
+	free_string(name);
 	if (!state || !state->data)
 		return NULL;
+
 	*sym = state->data;
 	return alloc_string(state->name);
 }
@@ -189,48 +195,45 @@ char *map_call_to_param_name_sym(struct expression *expr, struct symbol **sym)
 static void store_mapping_helper(char *left_name, struct symbol *left_sym, struct expression *call, const char *return_string)
 {
 	const char *p = return_string;
-	const char *end;
+	char *close;
 	int param;
-	struct expression *arg;
-	char *name = NULL;
+	struct expression *arg, *new;
+	char *right_name;
 	struct symbol *right_sym;
 	char buf[256];
 
 	while (*p && *p != '[')
 		p++;
 	if (!*p)
-		goto free;
+		return;
 	p++;
 	if (*p != '$')
-		goto free;
-	p++;
-	param = atoi(p);
-	p++;
-	if (*p != '-' || *(p + 1) != '>')
-		goto free;
-	end = strchr(p, ']');
-	if (!end)
-		goto free;
+		return;
 
+	snprintf(buf, sizeof(buf), "%s", p);
+	close = strchr(buf, ']');
+	if (!close)
+		return;
+	*close = '\0';
+
+	param = atoi(buf + 1);
 	arg = get_argument_from_call_expr(call->args, param);
 	if (!arg)
-		goto free;
-	name = expr_to_var_sym(arg, &right_sym);
-	if (!name || !right_sym)
-		goto free;
-	snprintf(buf, sizeof(buf), "%s", name);
+		return;
 
-	if (end - p + strlen(buf) >= sizeof(buf))
+	new = gen_expression_from_key(arg, buf);
+	if (!new)
+		return;
+
+	right_name = expr_to_var_sym(new, &right_sym);
+	if (!right_name || !right_sym)
 		goto free;
-	strncat(buf, p, end - p);
 
-	set_state(my_id, left_name, left_sym, alloc_my_state(buf, right_sym));
-	//set_state(my_id, buf, right_sym, alloc_my_state(left_name, left_sym));
-
-	store_link(link_id, buf, right_sym, left_name, left_sym);
+	set_state(my_id, left_name, left_sym, alloc_my_state(right_name, right_sym));
+	store_link(link_id, right_name, right_sym, left_name, left_sym);
 
 free:
-	free_string(name);
+	free_string(right_name);
 }
 
 void __add_return_to_param_mapping(struct expression *expr, const char *return_string)