8efcdf62178fd8cb97d1626443dcf560c1d895b8
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2018-05-26 18:19:19 +0200

    label: add testcase for label redefinition
    Redefined labels create inconsistencies in BB processing.
    Add a testcase for it.
    
    Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

---
+++ validation/label-redefined.c
@@ -0,0 +1,18 @@
+extern void fun(void);
+
+static void foo(int p)
+{
+l:
+	if (p)
+l:
+		fun();
+}
+
+/*
+ * check-name: label-redefined
+ * check-known-to-fail
+ *
+ * check-error-start
+label-redefined.c:7:1: error: label 'l' redefined
+ * check-error-end
+ */
