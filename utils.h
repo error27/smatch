a697a21927bddb091ba1a6d88c4db01ef2f58092
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2018-06-08 23:31:18 +0200

    utils: add xasprintf() & xvasprintf()
    Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

--- utils.h
+++ utils.h
@@ -6,6 +6,7 @@
 // -----------------------
 
 #include <stddef.h>
+#include <stdarg.h>
 
 ///
 // duplicate a memory buffer in a newly allocated buffer.
@@ -21,5 +22,22 @@ void *xmemdup(const void *src, size_t len);
 // @return: a pointer to a copy of @str allocated via
 //	:func:`__alloc_bytes()`.
 char *xstrdup(const char *src);
+
+///
+// printf to allocated string
+// @fmt: the format followed by its arguments.
+// @return: the allocated & formatted string.
+// This function is similar to asprintf() but the resulting string
+// is allocated with __alloc_bytes().
+char *xasprintf(const char *fmt, ...);
+
+///
+// vprintf to allocated string
+// @fmt: the format
+// @ap: the variadic arguments
+// @return: the allocated & formatted string.
+// This function is similar to asprintf() but the resulting string
+// is allocated with __alloc_bytes().
+char *xvasprintf(const char *fmt, va_list ap);
 
 #endif
