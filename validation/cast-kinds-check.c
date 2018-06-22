e46af66b3c2b1cb32c6394ac5e6c7ce632d905a9
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2018-06-22 17:25:39 +0200

    cast: reorganize testcases for cast optimization
    validation/linear/* should not contain testcases that are
    optimization dependent and validation/*.c should not contain
    tests using 'test-linearize', only those using 'sparse'.
    
    Move some cast-related testcases accordingly.
    
    Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

--- validation/cast-kinds-check.c
+++ validation/cast-kinds-check.c
@@ -1,19 +1,19 @@
-#include "linear/cast-kinds.c"
+#include "optim/cast-kinds.c"
 
 /*
  * check-name: cast-kinds check
  * check-command: sparse -m64 -v -Wno-pointer-to-int-cast $file
  *
  * check-error-start
-linear/cast-kinds.c:5:45: warning: cast drops bits
-linear/cast-kinds.c:6:47: warning: cast drops bits
-linear/cast-kinds.c:7:46: warning: cast drops bits
-linear/cast-kinds.c:8:45: warning: cast drops bits
-linear/cast-kinds.c:12:48: warning: cast drops bits
-linear/cast-kinds.c:13:50: warning: cast drops bits
-linear/cast-kinds.c:14:49: warning: cast drops bits
-linear/cast-kinds.c:15:48: warning: cast drops bits
-linear/cast-kinds.c:37:42: warning: non size-preserving integer to pointer cast
-linear/cast-kinds.c:38:44: warning: non size-preserving integer to pointer cast
+optim/cast-kinds.c:5:45: warning: cast drops bits
+optim/cast-kinds.c:6:47: warning: cast drops bits
+optim/cast-kinds.c:7:46: warning: cast drops bits
+optim/cast-kinds.c:8:45: warning: cast drops bits
+optim/cast-kinds.c:12:48: warning: cast drops bits
+optim/cast-kinds.c:13:50: warning: cast drops bits
+optim/cast-kinds.c:14:49: warning: cast drops bits
+optim/cast-kinds.c:15:48: warning: cast drops bits
+optim/cast-kinds.c:37:42: warning: non size-preserving integer to pointer cast
+optim/cast-kinds.c:38:44: warning: non size-preserving integer to pointer cast
  * check-error-end
  */
