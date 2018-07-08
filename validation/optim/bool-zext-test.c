0fc8a1eebbb1c7adeef5900288203b24a3aa4f7d
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2018-07-08 23:59:51 +0200

    testcase for SET{EQ,NE}([SZ]EXT(x, N),{0,1})'s simplification
    Add some basic testcase for these relatively common
    simplification opportunities.
    
    Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

---
+++ validation/optim/bool-zext-test.c
@@ -0,0 +1,13 @@
+_Bool equ0(unsigned char a) { return a == 0; }
+_Bool equ1(unsigned char a) { return a == 1; }
+_Bool neu0(unsigned char a) { return a != 0; }
+_Bool neu1(unsigned char a) { return a != 1; }
+
+/*
+ * check-name: bool-zext-test
+ * check-command: test-linearize -Wno-decl $file
+ * check-known-to-fail
+ *
+ * check-output-ignore
+ * check-output-excludes: zext\\.
+ */
