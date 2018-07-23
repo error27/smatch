error27/smatch

commit c64d1972a5b775b9d7169dc8db96ee0556af7b26

cast: prepare for more cast simplifications
Currently, the only simplification of casts is CAST(AND(X))
but more simplification are possible.

Prepare for additional simplification by unsing a switch-case
for OP_AND instead of the current 'if'.

Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

simplify.c
@@ -946,30 +946,33 @@ static int simplify_memop(struct instruction *insn)
 
 static int simplify_cast(struct instruction *insn)
 {
+	struct instruction *def;
 	pseudo_t src;
 	int size;
 
 	if (dead_insn(insn, &insn->src, NULL, NULL))
 		return REPEAT_CSE;
 
-	size = insn->size;
 	src = insn->src;
 
 	/* A cast of a constant? */
 	if (constant(src))
 		return simplify_constant_unop(insn);
 
-	/* A cast of a "and" might be a no-op.. */
-	if (src->type == PSEUDO_REG) {
-		struct instruction *def = src->def;
-		if (def->opcode == OP_AND && def->size >= size) {
+	// can merge with the previous instruction?
+	size = insn->size;
+	def = src->def;
+	switch (def_opcode(src)) {
+	case OP_AND:
+		/* A cast of a AND might be a no-op.. */
+		if (def->size >= size) {
 			pseudo_t val = def->src2;
 			if (val->type == PSEUDO_VAL) {
-				unsigned long long value = val->value;
-				if (!(value >> (size-1)))
+				if (!(val->value >> (size-1)))
 					goto simplify;
 			}
 		}
+		break;
 	}
 
 	return 0;

commit a89f2e3f544c91d2645f97292321750232fdf1f4

cast: fix shift signedness in cast simplification
In commit c64d1972a ("cast: prepare for more cast simplifications")
a unsigned right shift was inadvertently changed to a signed one.

Restore the correct signedness.

Fixes: c64d1972a ("cast: prepare for more cast simplifications")
Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

simplify.c
@@ -995,7 +995,8 @@ static int simplify_cast(struct instruction *insn)
 		if (def->size >= size) {
 			pseudo_t val = def->src2;
 			if (val->type == PSEUDO_VAL) {
-				if (!(val->value >> (size-1)))
+				unsigned long long value = val->value;
+				if (!(value >> (size-1)))
 					goto simplify;
 			}
 		}