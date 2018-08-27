86556e1537cb094ddd33e30d1ee451d8930246c6
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2018-08-27 14:51:42 +0200

    add tescase for unreachable label in switch
    or more exactly, an unreachable switch statement but containing a
    reachable label. This is valid code but is curently wrongly linearized.
    
    So, add a testcase for it.
    
    Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

---
+++ validation/linear/unreachable-label0.c
@@ -0,0 +1,20 @@
+static int foo(int a)
+{
+	goto label;
+	switch(a) {
+	default:
+label:
+		break;
+	}
+	return 0;
+}
+
+/*
+ * check-name: unreachable-label0
+ * check-command: test-linearize $file
+ * check-known-to-fail
+ *
+ * check-output-ignore
+ * check-output-contains: ret\\.
+ * check-output-excludes: END
+ */
