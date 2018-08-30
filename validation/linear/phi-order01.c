45c5f24a17690d2d4276fadb6c02a78e44843b23
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2018-08-30 14:41:54 +0200

    add testcases for wrong ordering in phi-nodes
    In valid SSA there is a 1-to-1 correspondance between
    each operand of a phi-node and the parents BB.
    However, currently, this is not always respected.
    
    Add testcases for the known problems.
    
    Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

---
+++ validation/linear/phi-order01.c
@@ -0,0 +1,17 @@
+int fun(void);
+
+static int foo(int a)
+{
+	return a && fun();
+}
+
+static int bar(int a)
+{
+	return a || fun();
+}
+
+/*
+ * check-name: phi-order01
+ * check-command: sparse -vir -flinearize=last $file
+ * check-known-to-fail
+ */
---
+++ validation/linear/phi-order03.c
@@ -0,0 +1,9 @@
+int fun(void);
+
+static int foo(void) { return ((0 || fun()) && fun()); }
+
+/*
+ * check-name: phi-order03
+ * check-command: sparse -vir -flinearize=last $file
+ * check-known-to-fail
+ */
