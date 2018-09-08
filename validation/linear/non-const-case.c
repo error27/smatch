f43d375657f22cf69668a417f555fcc7a0a780d5
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2018-09-08 12:23:21 +0200

    add testcase for non-constant switch-case
    Switches with non-constant cases are currently linearized
    using as value the bit pattern present in the expression,
    creating more or less random multijmps.
    
    Add a basic testcase to catch this.
    
    Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

---
+++ validation/linear/non-const-case.c
@@ -0,0 +1,38 @@
+static int foo(int a)
+{
+	switch (a) {
+	case 0:
+		return a;
+	case a:
+		return 0;
+	case (a - a):
+		return 1;
+	default:
+		return a;
+	}
+}
+
+static int bar(int a)
+{
+	switch (a) {
+	case 0:
+		break;
+	case a:
+		a++;
+label:
+		return a;
+	}
+
+	goto label;
+}
+
+
+/*
+ * check-name: non-const-case
+ * check-command: test-linearize -Wno-decl $file
+ * check-known-to-fail
+ *
+ * check-error-ignore
+ * check-output-ignore
+ * check-output-excludes:switch \\.
+ */
