error27/smatch

commit b598c1d75a9c455c85a894172329941300fcfb9f

[PATCH] enum handling
a) we allow enum have an equivalent type other than int (it is kept in
   ->ctype.base_type for SYM_ENUM types).  Code adjusted.

b) enum declaration parsing tries to determine the equivalent type; if
   such type does not exist we set it to &bad_enum_ctype and
   examine_symbol_type() generates a warning when it meets such beast.
   The rules for equivalent type follow:

	1) if all members have the same bitwise type, it is the
	   equivalent type of enum.
	2) if all members have integer types and there is a single
	   integer type capable of representing all their values, we pick
	   the first such type (in the same ordering that is used for e.g.
	   integer constants). 
	3) anything else => bad_enum_ctype.

c) enum is compatible with its equivalent type. 

d) it is an error to have implied initializer for a member following
   that of bitwise type.

There are still issues with float in enums, but since gcc would choke on
those unconditionally, we can be sure that there's no instances in any
code we could deal with (unless we start playing with Plan 9 codebase,
and that would require more work in other places - float enum is not the
only C extension in there).  Separate patch...

parse.c
@@ -155,10 +155,54 @@ struct token *struct_or_union_specifier(enum type type, struct token *token, str
 	return struct_union_enum_specifier(type, token, ctype, parse_struct_declaration);
 }
 
+typedef struct {
+	int x;
+	unsigned long long y;
+} Num;
+
+static void upper_boundary(Num *n, Num *v)
+{
+	if (n->x > v->x)
+		return;
+	if (n->x < v->x) {
+		*n = *v;
+		return;
+	}
+	if (n->y < v->y)
+		n->y = v->y;
+}
+
+static void lower_boundary(Num *n, Num *v)
+{
+	if (n->x < v->x)
+		return;
+	if (n->x > v->x) {
+		*n = *v;
+		return;
+	}
+	if (n->y > v->y)
+		n->y = v->y;
+}
+
+static int type_is_ok(struct symbol *type, Num *upper, Num *lower)
+{
+	int shift = type->bit_size;
+	int is_unsigned = type->ctype.modifiers & MOD_UNSIGNED;
+
+	if (!is_unsigned)
+		shift--;
+	if (upper->x == 0 && upper->y >> shift)
+		return 0;
+	if (lower->x == 0 || (!is_unsigned && (~lower->y >> shift) == 0))
+		return 1;
+	return 0;
+}
+
 static struct token *parse_enum_declaration(struct token *token, struct symbol *parent)
 {
-	unsigned long long nextval = 0;
-	struct symbol *ctype = &int_ctype;
+	unsigned long long lastval = 0;
+	struct symbol *ctype = NULL, *base_type = NULL;
+	Num upper = {-1, 0}, lower = {1, 0};
 
 	while (token_type(token) == TOKEN_IDENT) {
 		struct token *next = token->next;
@@ -166,23 +210,72 @@ static struct token *parse_enum_declaration(struct token *token, struct symbol *
 
 		sym = alloc_symbol(token->pos, SYM_ENUM);
 		bind_symbol(sym, token->ident, NS_SYMBOL);
-		parent->ctype.base_type = &int_ctype;
 
 		if (match_op(next, '=')) {
 			struct expression *expr;
 			next = constant_expression(next->next, &expr);
-			nextval = get_expression_value(expr);
+			lastval = get_expression_value(expr);
 			ctype = expr->ctype;
+		} else if (!ctype) {
+			ctype = &int_ctype;
+		} else if (is_int_type(ctype)) {
+			lastval++;
+		} else {
+			error_die(token->pos, "can't increment the last enum member");
 		}
-		sym->value = nextval;
+
+		sym->value = lastval;
 		sym->ctype.base_type = ctype;
 
+		if (base_type != &bad_enum_ctype) {
+			if (ctype->type == SYM_NODE)
+				ctype = ctype->ctype.base_type;
+			if (ctype->type == SYM_ENUM)
+				ctype = ctype->ctype.base_type;
+			if (!base_type) {
+				base_type = ctype;
+			} else if (ctype == base_type) {
+				/* nothing */
+			} else if (is_restricted_type(ctype)) {
+				base_type = &bad_enum_ctype;
+			} else if (is_restricted_type(base_type)) {
+				base_type = &bad_enum_ctype;
+			}
+		}
+		if (is_int_type(base_type)) {
+			Num v = {.y = lastval};
+			if (ctype->ctype.modifiers & MOD_UNSIGNED)
+				v.x = 0;
+			else if ((long long)lastval >= 0)
+				v.x = 0;
+			else
+				v.x = -1;
+			upper_boundary(&upper, &v);
+			lower_boundary(&lower, &v);
+		}
 		token = next;
 		if (!match_op(token, ','))
 			break;
 		token = token->next;
-		nextval = nextval + 1;
 	}
+	if (!base_type)
+		parent->ctype.base_type = &bad_enum_ctype;
+	else if (!is_int_type(base_type))
+		parent->ctype.base_type = base_type;
+	else if (type_is_ok(&int_ctype, &upper, &lower))
+		parent->ctype.base_type = &int_ctype;
+	else if (type_is_ok(&uint_ctype, &upper, &lower))
+		parent->ctype.base_type = &uint_ctype;
+	else if (type_is_ok(&long_ctype, &upper, &lower))
+		parent->ctype.base_type = &long_ctype;
+	else if (type_is_ok(&ulong_ctype, &upper, &lower))
+		parent->ctype.base_type = &ulong_ctype;
+	else if (type_is_ok(&llong_ctype, &upper, &lower))
+		parent->ctype.base_type = &llong_ctype;
+	else if (type_is_ok(&ullong_ctype, &upper, &lower))
+		parent->ctype.base_type = &ullong_ctype;
+	else
+		parent->ctype.base_type = &bad_enum_ctype;
 	return token;
 }
 

evaluate.c
@@ -57,7 +57,7 @@ static struct symbol *evaluate_symbol_expression(struct expression *expr)
 	/* The type of a symbol is the symbol itself! */
 	expr->ctype = sym;
 
-	/* enum's can be turned into plain values */
+	/* enums can be turned into plain values */
 	if (sym->type != SYM_ENUM) {
 		struct expression *addr = alloc_expression(expr->pos, EXPR_SYMBOL);
 		addr->symbol = sym;
@@ -67,9 +67,18 @@ static struct symbol *evaluate_symbol_expression(struct expression *expr)
 		expr->op = '*';
 		expr->unop = addr;
 		return sym;
+	} else if (base_type->bit_size < bits_in_int) {
+		/* ugly - we need to force sizeof for these guys */
+		struct expression *e = alloc_expression(expr->pos, EXPR_VALUE);
+		e->value = sym->value;
+		e->ctype = base_type;
+		expr->type = EXPR_PREOP;
+		expr->op = '+';
+		expr->unop = e;
+	} else {
+		expr->type = EXPR_VALUE;
+		expr->value = sym->value;
 	}
-	expr->type = EXPR_VALUE;
-	expr->value = sym->value;
 	expr->ctype = base_type;
 	return base_type;
 }
@@ -113,9 +122,11 @@ static inline struct symbol *integer_promotion(struct symbol *type)
 	unsigned long mod =  type->ctype.modifiers;
 	int width;
 
-	if (type->type == SYM_ENUM)
-		return &int_ctype;
-	else if (type->type == SYM_BITFIELD) {
+	if (type->type == SYM_ENUM) {
+		type = type->ctype.base_type;
+		mod = type->ctype.modifiers;
+	}
+	if (type->type == SYM_BITFIELD) {
 		mod = type->ctype.base_type->ctype.modifiers;
 		width = type->fieldwidth;
 	} else if (mod & (MOD_CHAR | MOD_SHORT))
@@ -319,6 +330,8 @@ static int restricted_unop(int op, struct symbol *type)
 {
 	if (op == '~' && type->bit_size >= bits_in_int)
 		return 0;
+	if (op == '+')
+		return 0;
 	return 1;
 }
 
@@ -330,7 +343,11 @@ static struct symbol *compatible_restricted_binop(int op, struct expression **lp
 
 	if (ltype->type == SYM_NODE)
 		ltype = ltype->ctype.base_type;
+	if (ltype->type == SYM_ENUM)
+		ltype = ltype->ctype.base_type;
 	if (rtype->type == SYM_NODE)
+		rtype = rtype->ctype.base_type;
+	if (rtype->type == SYM_ENUM)
 		rtype = rtype->ctype.base_type;
 	if (is_restricted_type(ltype)) {
 		if (is_restricted_type(rtype)) {
@@ -485,6 +502,16 @@ const char * type_difference(struct symbol *target, struct symbol *source,
 			}
 			mod2 |= source->ctype.modifiers;
 			as2 |= source->ctype.as; 
+		}
+		if (target->type == SYM_ENUM) {
+			target = target->ctype.base_type;
+			if (!target)
+				return "bad types";
+		}
+		if (source->type == SYM_ENUM) {
+			source = source->ctype.base_type;
+			if (!source)
+				return "bad types";
 		}
 
 		if (target == source)

symbol.c
@@ -256,10 +256,19 @@ struct symbol *examine_symbol_type(struct symbol * sym)
 			merge_type(sym, base_type);
 		return sym;
 	case SYM_ENUM:
-		if (!sym->bit_size)
-			sym->bit_size = bits_in_enum;
-		if (!sym->ctype.alignment)
-			sym->ctype.alignment = enum_alignment;
+		base_type = sym->ctype.base_type;
+		base_type = examine_symbol_type(base_type);
+		if (base_type == &bad_enum_ctype) {
+			warning(sym->pos, "invalid enum type");
+			sym->bit_size = -1;
+			return sym;
+		}
+		sym->bit_size = bits_in_enum;
+		if (base_type->bit_size > sym->bit_size)
+			sym->bit_size = base_type->bit_size;
+		sym->ctype.alignment = enum_alignment;
+		if (base_type->ctype.alignment > sym->ctype.alignment)
+			sym->ctype.alignment = base_type->ctype.alignment;
 		return sym;
 	case SYM_BITFIELD:
 		examine_bitfield_type(sym);
@@ -560,7 +569,7 @@ struct symbol	bool_ctype, void_ctype, type_ctype,
 		llong_ctype, sllong_ctype, ullong_ctype,
 		float_ctype, double_ctype, ldouble_ctype,
 		string_ctype, ptr_ctype, lazy_ptr_ctype,
-		incomplete_ctype, label_ctype;
+		incomplete_ctype, label_ctype, bad_enum_ctype;
 
 
 #define __INIT_IDENT(str) { .len = sizeof(str)-1, .name = str }
@@ -609,6 +618,7 @@ static const struct ctype_declare {
 	{ &void_ctype,	    SYM_BASETYPE, 0,			    NULL,		     NULL,		 NULL },
 	{ &type_ctype,	    SYM_BASETYPE, MOD_TYPE,		    NULL,		     NULL,		 NULL },
 	{ &incomplete_ctype,SYM_BASETYPE, 0,			    NULL,		     NULL,		 NULL },
+	{ &bad_enum_ctype,	SYM_BAD, 0,			    NULL,		     NULL,		 NULL },
 
 	{ &char_ctype,	    SYM_BASETYPE, MOD_SIGNED | MOD_CHAR,    &bits_in_char,	     &max_int_alignment, &int_type },
 	{ &schar_ctype,	    SYM_BASETYPE, MOD_ESIGNED | MOD_CHAR,   &bits_in_char,	     &max_int_alignment, &int_type },

symbol.h
@@ -51,6 +51,7 @@ enum type {
 	SYM_BITFIELD,
 	SYM_LABEL,
 	SYM_RESTRICT,
+	SYM_BAD,
 };
 
 struct ctype {
@@ -171,7 +172,7 @@ extern struct symbol	bool_ctype, void_ctype, type_ctype,
 			llong_ctype, sllong_ctype, ullong_ctype,
 			float_ctype, double_ctype, ldouble_ctype,
 			string_ctype, ptr_ctype, lazy_ptr_ctype,
-			incomplete_ctype, label_ctype;
+			incomplete_ctype, label_ctype, bad_enum_ctype;
 
 
 #define __IDENT(n,str) \
@@ -212,14 +213,17 @@ static inline int is_int_type(const struct symbol *type)
 {
 	if (type->type == SYM_NODE)
 		type = type->ctype.base_type;
-	return (type->type == SYM_ENUM) ||
-	       (type->type == SYM_BITFIELD) ||
+	if (type->type == SYM_ENUM)
+		type = type->ctype.base_type;
+	return type->type == SYM_BITFIELD ||
 	       type->ctype.base_type == &int_type;
 }
 
 static inline int get_sym_type(struct symbol *type)
 {
 	if (type->type == SYM_NODE)
+		type = type->ctype.base_type;
+	if (type->type == SYM_ENUM)
 		type = type->ctype.base_type;
 	return type->type;
 }

commit bff011270111c3fcdc0b7a5490d7d0c92c0c50ea

enum: fix UB when rshifting by full width
Shifting by an amount greater or equal than the width
of the type is Undefined Behaviour. In the present case,
when type_is_ok() is called with a type as wide as an
ullong (64 bits here), the bounds are shifted by 64
which is UB and at execution (on x86) the value is simply
unchanged (since the shift is done with the amount modulo 63).

This, of course, doesn't give the expected result and
as consequence valid enums can have an invalid base type
(bad_ctype).

Fix this by doing the shift with a small helper which
return 0 if the amount is equal to the maximum width.

NB. Doing the shift in two steps could also be a solution,
  as maybe some clever trick, but since this code is in
  no way critical performance-wise, the solution here
  has the merit to be very explicit.

Fixes: b598c1d75a9c455c85a894172329941300fcfb9f
Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

parse.c
@@ -805,6 +805,19 @@ static void lower_boundary(Num *n, Num *v)
 		n->y = v->y;
 }
 
+///
+// safe right shift
+//
+// This allow to use a shift amount as big (or bigger)
+// than the width of the value to be shifted, in which case
+// the result is, of course, 0.
+static unsigned long long rshift(unsigned long long val, unsigned int n)
+{
+	if (n >= (sizeof(val) * 8))
+		return 0;
+	return val >> n;
+}
+
 static int type_is_ok(struct symbol *type, Num *upper, Num *lower)
 {
 	int shift = type->bit_size;
@@ -812,9 +825,9 @@ static int type_is_ok(struct symbol *type, Num *upper, Num *lower)
 
 	if (!is_unsigned)
 		shift--;
-	if (upper->x == 0 && upper->y >> shift)
+	if (upper->x == 0 && rshift(upper->y, shift))
 		return 0;
-	if (lower->x == 0 || (!is_unsigned && (~lower->y >> shift) == 0))
+	if (lower->x == 0 || (!is_unsigned && rshift(~lower->y, shift) == 0))
 		return 1;
 	return 0;
 }

validation/bug-rshift-ub.c
@@ -6,7 +6,6 @@ static enum a a = A;
 
 /*
  * check-name: bug-rshift-ub
- * check-known-to-fail
  * check-description:
  *	This test trigger(ed) a bug on x86 caused by a
  *	full width shift (which is UB), expecting to get