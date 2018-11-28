ea5e74baf1bdac9607cfc49d7ea893d1d17b8380
Dan Carpenter <dan.carpenter@oracle.com>
2018-11-28 16:04:55 +0300

    test_bit: add test_and_set_bit()
    It's just another function with takes a bit number instead of a mask.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- check_test_bit.c
+++ check_test_bit.c
@@ -50,5 +50,5 @@ void check_test_bit(int id)
 	add_function_hook("set_bit", &match_test_bit, NULL);
 	add_function_hook("clear_bit", &match_test_bit, NULL);
 	add_function_hook("test_and_clear_bit", &match_test_bit, NULL);
-
+	add_function_hook("test_and_set_bit", &match_test_bit, NULL);
 }
