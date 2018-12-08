485700db8ffc3fe0cba6cd8464eef020da18bc53
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2018-12-08 16:01:47 +0100

    add testcase for missing deliminator ' or "
    Add a testcase for "Newline in string or character constant" vs.
    "missing delimitator" upcoming change.
    
    Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

---
+++ validation/preprocessor/missing-delim.c
@@ -0,0 +1,18 @@
+static int  c = 'a;
+
+static char s[] = "abc;
+static char t[] = "xyz";
+
+extern void foo(void);
+
+/*
+ * check-name: missing-delim
+ * check-command: sparse -E $file
+ * check-output-ignore
+ * check-known-to-fail
+ *
+ * check-error-start
+preprocessor/missing-delim.c:2:0: warning: missing delimitator '
+preprocessor/missing-delim.c:4:0: warning: missing delimitator "
+ * check-error-end
+ */
