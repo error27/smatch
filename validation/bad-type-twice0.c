fa232154d6717107d2941498103f95e1e3ee4252
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2018-12-29 14:10:06 +0100

    explain cause of 'incorrect type in conditional'
    A conditional only make sense on a scalar type. If not, an
    error is issued but the message doesn't explain the cause.
    
    Fix this by adding the cause to the error message.
    
    Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

--- validation/bad-type-twice0.c
+++ validation/bad-type-twice0.c
@@ -7,7 +7,7 @@ static int foo(a)
  * check-name: bad-type-twice0
  *
  * check-error-start
-bad-type-twice0.c:3:16: error: incorrect type in conditional
+bad-type-twice0.c:3:16: error: incorrect type in conditional (non-scalar type)
 bad-type-twice0.c:3:16:    got incomplete type a
  * check-error-end
  */
