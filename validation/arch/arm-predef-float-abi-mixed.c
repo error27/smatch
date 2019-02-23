16df5da6daa81ee91fda2fe7316fab7d966c1ec7
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2019-02-23 23:50:22 +0100

    fp-abi: add tests for ARM's -mfloat-abi=... & -msoft-float
    Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

---
+++ validation/arch/arm-predef-float-abi-mixed.c
@@ -0,0 +1,9 @@
+#if defined(__ARM_PCS_VFP) || defined(__SOFTFP__) || !defined(__ARM_PCS)
+#error
+#endif
+
+/*
+ * check-name: arm-predef-float-abi-mixed
+ * check-command: sparse --arch=arm -mfloat-abi=softfp $file
+ * check-known-to-fail
+ */
