b33d7be65a33d0d18d0c0aa979889bdf8d370308
Dan Carpenter <dan.carpenter@oracle.com>
2019-03-18 14:24:49 +0300

    mtag: remove duplicative code
    The smatch_extra.c file is already saving all the mtag information
    (because it's basically a sort of pointer) so there is no need to do it
    here.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- smatch_mtag.c
+++ smatch_mtag.c
@@ -50,20 +50,6 @@
 
 static int my_id;
 
-static struct smatch_state *alloc_tag_state(mtag_t tag)
-{
-	struct smatch_state *state;
-	char buf[64];
-
-	state = __alloc_smatch_state(0);
-	snprintf(buf, sizeof(buf), "%lld", tag);
-	state->name = alloc_sname(buf);
-	state->data = malloc(sizeof(mtag_t));
-	*(mtag_t *)state->data = tag;
-
-	return state;
-}
-
 static mtag_t str_to_tag(const char *str)
 {
 	unsigned char c[MD5_DIGEST_LENGTH];
@@ -156,8 +142,6 @@ struct smatch_state *swap_mtag_return(struct expression *expr, struct smatch_sta
 
 	sql_insert_mtag_about(tag, left_name, buf);
 
-	set_state(my_id, left_name, left_sym, alloc_tag_state(tag));
-
 	free_string(left_name);
 	free_string(right_name);
 
@@ -234,82 +218,6 @@ static void global_variable(struct symbol *sym)
 	sql_insert_mtag_about(tag,
 			      sym->ident->name,
 			      (sym->ctype.modifiers & MOD_STATIC) ? get_filename() : "extern");
-}
-
-static void db_returns_buf_size(struct expression *expr, int param, char *unused, char *math)
-{
-	struct expression *call;
-	struct range_list *rl;
-
-	if (expr->type != EXPR_ASSIGNMENT)
-		return;
-	call = strip_expr(expr->right);
-
-	if (!parse_call_math_rl(call, math, &rl))
-		return;
-//	rl = cast_rl(&int_ctype, rl);
-//	set_state_expr(my_size_id, expr->left, alloc_estate_rl(rl));
-}
-
-static void db_returns_memory_tag(struct expression *expr, int param, char *key, char *value)
-{
-	struct expression *call, *arg;
-	mtag_t tag, alias;
-	char *name;
-	struct symbol *sym;
-
-	call = strip_expr(expr);
-	while (call->type == EXPR_ASSIGNMENT)
-		call = strip_expr(call->right);
-	if (call->type != EXPR_CALL)
-		return;
-
-	tag = strtoul(value, NULL, 10);
-
-	if (!create_mtag_alias(tag, call, &alias))
-		return;
-
-	arg = get_argument_from_call_expr(call->args, param);
-	if (!arg)
-		return;
-
-	name = get_variable_from_key(arg, key, &sym);
-	if (!name || !sym)
-		goto free;
-
-	set_state(my_id, name, sym, alloc_tag_state(alias));
-free:
-	free_string(name);
-}
-
-static void match_call_info(struct expression *expr)
-{
-	struct smatch_state *state;
-	struct expression *arg;
-	int i = -1;
-
-	FOR_EACH_PTR(expr->args, arg) {
-		i++;
-		state = get_state_expr(my_id, arg);
-		if (!state || !state->data)
-			continue;
-		sql_insert_caller_info(expr, MEMORY_TAG, i, "$", state->name);
-	} END_FOR_EACH_PTR(arg);
-}
-
-static void save_caller_info(const char *name, struct symbol *sym, char *key, char *value)
-{
-	struct smatch_state *state;
-	char fullname[256];
-	mtag_t tag;
-
-	if (strncmp(key, "$", 1) != 0)
-		return;
-
-	tag = atoll(value);
-	snprintf(fullname, 256, "%s%s", name, key + 1);
-	state = alloc_tag_state(tag);
-	set_state(my_id, fullname, sym, state);
 }
 
 static int get_array_mtag_offset(struct expression *expr, mtag_t *tag, int *offset)
@@ -574,31 +482,6 @@ int get_mtag_addr_sval(struct expression *expr, sval_t *sval)
 	return get_mtag_sval(remove_dereference(expr), sval);
 }
 
-static void print_stored_to_mtag(int return_id, char *return_ranges, struct expression *expr)
-{
-	struct sm_state *sm;
-	char buf[256];
-	const char *param_name;
-	int param;
-
-	FOR_EACH_MY_SM(my_id, __get_cur_stree(), sm) {
-		if (!sm->state->data)
-			continue;
-
-		param = get_param_num_from_sym(sm->sym);
-		if (param < 0)
-			continue;
-		param_name = get_param_name(sm);
-		if (!param_name)
-			continue;
-		if (strcmp(param_name, "$") == 0)
-			continue;
-
-		snprintf(buf, sizeof(buf), "%lld", *(mtag_t *)sm->state->data);
-		sql_insert_return_states(return_id, return_ranges, MEMORY_TAG, param, param_name, buf);
-	} END_FOR_EACH_SM(sm);
-}
-
 void register_mtag(int id)
 {
 	my_id = id;
@@ -616,11 +499,4 @@ void register_mtag(int id)
 		return;
 
 	add_hook(&global_variable, BASE_HOOK);
-
-	select_return_states_hook(BUF_SIZE, &db_returns_buf_size);
-
-	add_hook(&match_call_info, FUNCTION_CALL_HOOK);
-	select_caller_info_hook(save_caller_info, MEMORY_TAG);
-	add_split_return_callback(&print_stored_to_mtag);
-	select_return_states_hook(MEMORY_TAG, db_returns_memory_tag);
 }
