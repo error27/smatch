6d37de7ad9804f1a606839346101f2425ed86a13
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2019-09-25 20:47:28 +0200

    add test for enum sign extension
    In a declaration like:
    	enum {
    	        a = 0x80000000,
    	        b = -1,
    	}
    the underlying type should be long and b's value should be
    0xffffffffffffffff (on a 64-bit machine) but is 0xffffffff.
    
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

---
+++ validation/enum-sign-extend.c
@@ -0,0 +1,13 @@
+enum num {
+	a = 0x80000000,
+	b = -1,
+};
+
+_Static_assert([typeof(b)] == [long], "type");
+_Static_assert(b == -1L,              "value");
+
+/*
+ * check-name: enum-sign-extend
+ * check-command: sparse -m64 $file
+ * check-known-to-fail
+ */
