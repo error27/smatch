error27/smatch

commit 604a148a73af94bd271bbfb0d36dbe77e42bbc4f

enum: fix cast_enum_list()
Sparse want that an enum's enumerators have all the same type.
This is done by first determining the common type and then
calling cast_enum_list() which use cast_value() on each member
to cast them to the common type.

However, cast_value() doesn't create a new expression and doesn't
change the ctype of the target: the target expression is supposed
to have already the right type and it's just the value that is
transfered from the source expression and size adjusted.
It's seems that in cast_enum_list() this has been overlooked
with the result that the value is correctly adjusted but keep
it's original type.

Fix this by updating, for each member, the desired type.

Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

parse.c
@@ -855,6 +855,7 @@ static void cast_enum_list(struct symbol_list *list, struct symbol *base_type)
 		if (expr->type != EXPR_VALUE)
 			continue;
 		ctype = expr->ctype;
+		expr->ctype = base_type;
 		if (ctype->bit_size == base_type->bit_size)
 			continue;
 		cast_value(expr, base_type, expr, ctype);

validation/enum-same-type.c
@@ -11,5 +11,4 @@ _Static_assert([typeof(DUO)] == [typeof(NEG)], "enum same type");
 
 /*
  * check-name: enum-same-type
- * check-known-to-fail
  */

commit 1a38b82ebc4f41d5cfb3ea69c0924bbb01ba1358

fix sign extension in casting enums
The function cast_value() needs the exact type of the old expression
but when called via cast_enum_list() this type is incorrect because:
- the same struct is used for the new and the old expression
- the type of the new expression is adjusted before cast_value()
  is called.

Fix this by adjusting the type of the new expression only after
cast_value() has been called.

Fixes: 604a148a73af ("enum: fix cast_enum_list()")
Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

parse.c
@@ -897,8 +897,8 @@ static void cast_enum_list(struct symbol_list *list, struct symbol *base_type)
 			expr->ctype = &int_ctype;
 			continue;
 		}
-		expr->ctype = base_type;
 		cast_value(expr, base_type, expr, ctype);
+		expr->ctype = base_type;
 	} END_FOR_EACH_PTR(sym);
 }
 

validation/enum-sign-extend.c
@@ -9,5 +9,4 @@ _Static_assert(b == -1L,              "value");
 /*
  * check-name: enum-sign-extend
  * check-command: sparse -m64 $file
- * check-known-to-fail
  */