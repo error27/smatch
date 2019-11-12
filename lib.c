error27/smatch

commit 3c3881cf1c68f1b4ec4e5a68fa0aa92e88eb275c

cgcc: let sparse define _BIG_ENDIAN
In addition of __BIG_ENDIAN__, gcc defines _BIG_ENDIAN on
powerpc (32 & 64-bit). cgcc does that too but this may also
beeneeded when using sparse itself.

So, move this define the the sparse code.

Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

lib.c
@@ -1514,6 +1514,8 @@ static void predefined_macros(void)
 		predefine("__powerpc", 1, "1");
 		predefine("__ppc__", 1, "1");
 		predefine("__PPC__", 1, "1");
+		if (arch_big_endian)
+			predefine("_BIG_DEBIAN", 1, "1");
 		break;
 	case MACH_RISCV64:
 	case MACH_RISCV32:

cgcc
@@ -276,8 +276,7 @@ sub add_specs {
 	return (' --arch=x86_64' .
 		&float_types (1, 1, 33, [24,8], [53,11], [113,15]));
     } elsif ($spec eq 'ppc') {
-	return (' -D_BIG_ENDIAN' .
-		' --arch=ppc' .
+	return (' --arch=ppc' .
 		&float_types (1, 1, 21, [24,8], [53,11], [113,15]));
     } elsif ($spec eq 'ppc64') {
 	return (

commit cb75739a5e07b0f435b9e2685033e1ba9ebc31cf

lib.c: fix spelling of _BIG_ENDIAN
Commit 3c3881cf1c ("cgcc: let sparse define _BIG_ENDIAN", 2019-11-08)
moved the definition of the _BIG_ENDIAN macro from cgcc to sparse (for
the powerpc platforms). Unfortunately, the macro was inadvertently spelt
incorrectly (as _BIG_DEBIAN). Correct the spelling.

Fixes: 3c3881cf1c68f1b4ec4e5a68fa0aa92e88eb275c
Signed-off-by: Ramsay Jones <ramsay@ramsayjones.plus.com>
Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

lib.c
@@ -1515,7 +1515,7 @@ static void predefined_macros(void)
 		predefine("__ppc__", 1, "1");
 		predefine("__PPC__", 1, "1");
 		if (arch_big_endian)
-			predefine("_BIG_DEBIAN", 1, "1");
+			predefine("_BIG_ENDIAN", 1, "1");
 		break;
 	case MACH_RISCV64:
 	case MACH_RISCV32: