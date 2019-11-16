2094267c7d36d8696897c509558fc63e8a695765
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2019-11-16 15:52:31 +0100

    improve diagnostic message about wrong redeclaration
    The current message is very long (in most cases the position
    of the previous declaration is past the 80th column) and,
    while saying that the types differ, doesn't show these types.
    
    Change this by splitting the message in 2 parts:
    - first, on the current position, the main message
      and the type of the current declaration.
    - then the type of the previous declaration on its
      own position.
    
    Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

--- validation/typedef-redef.c
+++ validation/typedef-redef.c
@@ -8,6 +8,9 @@ typedef long ko_t;
  * check-name: typedef-redef
  *
  * check-error-start
-typedef-redef.c:5:14: error: symbol 'ko_t' redeclared with different type (originally declared at typedef-redef.c:4) - different type sizes
+typedef-redef.c:5:14: error: symbol 'ko_t' redeclared with different type (different type sizes):
+typedef-redef.c:5:14:    long [usertype] ko_t
+typedef-redef.c:4:14: note: previously declared as:
+typedef-redef.c:4:14:    int [usertype] ko_t
  * check-error-end
  */
