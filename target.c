c5a34b42943dd3d6d3f1d3c64a2e0c9cd8158902
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2019-11-23 15:17:46 +0100

    arch: sparc32 on SunOS/Solaris uses 128-bit long doubles
    On 32-bit sparc running SunOS or Solaris, long doubles
    are 128-bit, not 64-bit.
    
    Add a special case to handle this.
    
    Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

--- target.c
+++ target.c
@@ -116,11 +116,14 @@ void init_target(void)
 	}
 
 	switch (arch_mach) {
+	case MACH_SPARC32:
+		if (arch_os == OS_SUNOS)
+			break;
+		/* fall through */
 	case MACH_ARM:
 	case MACH_MIPS32:
 	case MACH_S390:
 	case MACH_S390X:
-	case MACH_SPARC32:
 		bits_in_longdouble = 64;
 		max_fp_alignment = 8;
 		break;
