613a671f31d2a44aa2e04ce05b77d1daad972f20
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2019-12-06 01:51:04 +0100

    add testcase for addressability of 'complex' symbols
    Once a symbol has its address taken, a lot of simplifications
    must be avoided because the symbol can now be modified via
    a pointer.
    
    This is currently done but the symbol addressability
    does not take in account the fact that a symbol can be
    accessed via one of its subfields.
    
    Add a testcase to illustrate this.
    
    Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

---
+++ validation/eval/addressable-complex.c
@@ -0,0 +1,24 @@
+extern void def(void *);
+
+struct s1 {
+	int a;
+};
+
+int use1(void)
+{
+	struct s1 s = { 3 };
+
+	def(&s.a);
+
+	return s.a;
+}
+
+/*
+ * check-name: eval/addressable-complex
+ * check-command: test-linearize -Wno-decl -fdump-ir $file
+ * check-known-to-fail
+ *
+ * check-output-ignore
+ * check-output-contains: load\\.
+ * check-output-excludes: return\\..*\\$3
+ */
