2a6c80fdd0616e20d6227eb4ab51feb84ba57c4e
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2019-12-08 03:26:40 +0100

    add test for dereference cost of symbol with complex type
    Currently, in expand_dereference(), the dereference of a symbol with
    a complex type is considered as costing as high as a non-symbol
    because it's not recognised it's a symbol.
    
    Add a testcase for this.
    
    Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

---
+++ validation/expand/cost-deref-nested.c
@@ -0,0 +1,21 @@
+struct s {
+	struct {
+		int u, v;
+	} a, b;
+};
+
+static const struct s s;
+
+static int foo(int c)
+{
+	return c && s.b.v;
+}
+
+/*
+ * check-name: cost-deref-nested
+ * check-command: test-linearize -fdump-ir $file
+ * check-known-to-fail
+ *
+ * check-output-ignore
+ * check-output-excludes: cbr
+ */
