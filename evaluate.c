error27/smatch

commit 696b243a5ae0b532c5505d53ae176fc3ac501579

fix: evaluate_dereference() unexamined base type
Examination of a pointer type doesn't examine the corresponding
base type (this base type may not yet be complete). So, this
examination must be done later, when the base type is needed.

However, in some cases it's possible to call evaluate_dereference()
while the base type is still unexamined.

Fix this by adding the missing examine_symbol_type() on the base type.

Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

evaluate.c
@@ -1778,6 +1778,7 @@ static struct symbol *evaluate_dereference(struct expression *expr)
 
 	node = alloc_symbol(expr->pos, SYM_NODE);
 	target = ctype->ctype.base_type;
+	examine_symbol_type(target);
 
 	switch (ctype->type) {
 	default:

validation/linear/deref-ptr-ptr.c
@@ -6,7 +6,6 @@ char *foo(char **pfmt)
 /*
  * check-name: deref-ptr-ptr
  * check-command: test-linearize -m64 -Wno-decl $file
- * check-known-to-fail
  *
  * check-output-excludes: load[^.]
  * check-output-contains: load\.

validation/linear/unexamined-base-type.c
@@ -28,7 +28,6 @@ static void bar(struct s *d, struct s *s1, struct s *s2)
  *		                   ^^^		!! WRONG !!
  *		and.32      %r6 <- %r5, $1
  *		ret.32      %r6
- * check-known-to-fail
  *
  * check-output-ignore
  * check-output-excludes: load[^.]

commit 82d8c05866c84a04cc5ecba74365573b4c29fcbc

fix premature examination of dereferenced object
in the fixes 696b243a5ae0 ("fix: evaluate_dereference() unexamined base type"),
the pointer's examination was done prematurely, before the undereferenceable
types are filtered out. This allows to examine the base abstract types when
the expression was in fact not dereferenceable.

Fix that by moving the examination to the top of the SYM_PTR's case
since only pointers are concerned.

Fixes: 696b243a5ae0 ("fix: evaluate_dereference() unexamined base type")
Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

evaluate.c
@@ -1817,7 +1817,6 @@ static struct symbol *evaluate_dereference(struct expression *expr)
 		ctype = ctype->ctype.base_type;
 
 	target = ctype->ctype.base_type;
-	examine_symbol_type(target);
 
 	switch (ctype->type) {
 	default:
@@ -1827,6 +1826,7 @@ static struct symbol *evaluate_dereference(struct expression *expr)
 		*expr = *op;
 		return expr->ctype;
 	case SYM_PTR:
+		examine_symbol_type(target);
 		node = alloc_symbol(expr->pos, SYM_NODE);
 		node->ctype.modifiers = target->ctype.modifiers & MOD_SPECIFIER;
 		merge_type(node, ctype);

validation/eval/premature-examination.c
@@ -1,0 +1,27 @@
+extern int i;
+
+int foo(void)
+{
+	return *i;
+}
+
+int bar(void)
+{
+	return i[0];
+}
+
+int *qux(void)
+{
+	return &i[0];
+}
+
+/*
+ * check-name: premature-examination
+ * check-command: sparse -Wno-decl $file
+ *
+ * check-error-start
+eval/premature-examination.c:5:16: error: cannot dereference this type
+eval/premature-examination.c:10:17: error: cannot dereference this type
+eval/premature-examination.c:15:18: error: cannot dereference this type
+ * check-error-end
+ */