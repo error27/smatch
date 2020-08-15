c73a5a4bbcb30f176be5b82969b5d681b8481bc5
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2020-08-15 16:49:17 +0200

    union-cast: add some testcases
    Casts to union type are a GCC extension and are similar to
    compound literals.
    
    However, sparse doesn't know about them and treats them like
    other casts to non-scalars.
    
    Add some testcases for this and its upcoming warning flag.
    
    Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

---
+++ validation/eval/union-cast.c
@@ -0,0 +1,25 @@
+union u {
+	int	i;
+	char	x[8];
+};
+
+static union u foo(int a)
+{
+	return (union u)a;
+}
+
+static union u bar(long a)
+{
+	return (union u)a;
+}
+
+/*
+ * check-name: union-cast
+ * check-command: sparse -Wunion-cast $file
+ * check-known-to-fail
+ *
+ * check-error-start
+eval/union-cast.c:8:17: warning: cast to union type
+eval/union-cast.c:13:17: warning: cast to non-scalar
+ * check-error-end
+ */
