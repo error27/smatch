error27/smatch

commit 5425db10d4d35895ba3ca390478c624233ec027d

simplify '~(~x)' and '-(-x)' to 'x'
Currently those double operations are not simplified.

This patch add those simplifications and some small test cases.

Note: the 'boolean not': '!(!x)' is not handled by this patch
because this operator is processed differently (it doesn't generate
an unop instruction but directly generates 'seteq' operations).

Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
Signed-off-by: Christopher Li <sparse@chrisli.org>

simplify.c
@@ -651,6 +651,23 @@ static int simplify_unop(struct instruction *insn)
 		return REPEAT_CSE;
 	if (constant(insn->src1))
 		return simplify_constant_unop(insn);
+
+	switch (insn->opcode) {
+		struct instruction *def;
+
+	case OP_NOT:
+		def = insn->src->def;
+		if (def && def->opcode == OP_NOT)
+			return replace_with_pseudo(insn, def->src);
+		break;
+	case OP_NEG:
+		def = insn->src->def;
+		if (def && def->opcode == OP_NEG)
+			return replace_with_pseudo(insn, def->src);
+		break;
+	default:
+		return 0;
+	}
 	return 0;
 }
 

validation/optim/double-unop.c
@@ -1,0 +1,15 @@
+typedef unsigned int u32;
+
+u32 unotnot(u32 a) { return ~(~a); }
+int snotnot(int a) { return ~(~a); }
+u32 unegneg(int a) { return -(-a); }
+int snegneg(int a) { return -(-a); }
+
+/*
+ * check-name: double-unop
+ * check-command: test-linearize -Wno-decl $file
+ * check-output-ignore
+ *
+ * check-output-excludes: not\\.
+ * check-output-excludes: neg\\.
+ */

commit bf0e2bd3c86ae40a88575a565f176030b7fc63b6

unop: fix access to defining instruction in simplify_unop()
Only pseudos of type PSEUDO_REG have a defining instruction.

However, in commit 5425db10d4d3 ("simplify '~(~x)' and '-(-x)' to 'x'"),
this defining instruction of the 'src' of the outer unop
was accessed without checking the type.

Fixes: 5425db10d4d35895ba3ca390478c624233ec027d
Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

simplify.c
@@ -1311,13 +1311,11 @@ static int simplify_unop(struct instruction *insn)
 		struct instruction *def;
 
 	case OP_NOT:
-		def = insn->src->def;
-		if (def && def->opcode == OP_NOT)
+		if (DEF_OPCODE(def, insn->src) == OP_NOT)
 			return replace_with_pseudo(insn, def->src);
 		break;
 	case OP_NEG:
-		def = insn->src->def;
-		if (def && def->opcode == OP_NEG)
+		if (DEF_OPCODE(def, insn->src) == OP_NEG)
 			return replace_with_pseudo(insn, def->src);
 		break;
 	default: