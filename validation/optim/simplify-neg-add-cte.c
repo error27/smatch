851d925ad7aad00f52a9386374752fd2246d2464
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2020-10-08 01:00:40 +0200

    unop: simplify -(x + C) --> -C - x
    Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

--- validation/optim/simplify-neg-add-cte.c
+++ validation/optim/simplify-neg-add-cte.c
@@ -5,7 +5,6 @@ int foo(int x) { return -(x + C) == (-3 - x); }
 /*
  * check-name: simplify-neg-add-cte
  * check-command: test-linearize -Wno-decl $file
- * check-known-to-fail
  *
  * check-output-ignore
  * check-output-contains: ret\\..*\\$1
