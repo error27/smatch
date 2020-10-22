error27/smatch

commit 6f7aa5e84dacec8e27a8d70090bba26a1a1276de

fix implicit K&R argument types
In an old-style function definition, if not explicitly specified,
the type of an argument defaults to 'int'.

Sparse issues an error for such arguments and leaves the type
as 'incomplete'. This can then create a cascade of other warnings.

Fix this by effectively giving the type 'int' to such arguments.

Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

parse.c
@@ -2771,7 +2771,9 @@ static void apply_k_r_types(struct symbol_list *argtypes, struct symbol *fn)
 				goto match;
 		} END_FOR_EACH_PTR(type);
 		sparse_error(arg->pos, "missing type declaration for parameter '%s'", show_ident(arg->ident));
-		continue;
+		type = alloc_symbol(arg->pos, SYM_NODE);
+		type->ident = arg->ident;
+		type->ctype.base_type = &int_ctype;
 match:
 		type->used = 1;
 		/* "char" and "short" promote to "int" */

validation/implicit-KR-arg-type1.c
@@ -1,0 +1,16 @@
+int foo(a, b)
+	int a;
+{
+	if (b)
+		return a;
+}
+
+/*
+ * check-name: implicit-KR-arg-type1
+ * check-command: sparse -Wold-style-definition -Wimplicit-int $file
+ *
+ * check-error-start
+implicit-KR-arg-type1.c:2:9: warning: non-ANSI definition of function 'foo'
+implicit-KR-arg-type1.c:1:12: error: missing type declaration for parameter 'b'
+ * check-error-end
+ */

commit 04c3c1e16a2bfcf9231c52d37611f05741056e6a

warn on all missing parameter types
A warning is given for K&R parameters without type declaration
and these parameters are given an implicit type of int to
avoid several problems with false errors in the next stages.

However, this is only done for K&R functions with the optional
parameter type declarations. If the parameters has no type
declaration at all, no diagnostic is given and the type is left
as incomplete. In consequence, a function defined with a typo like
'int foo(oid)' instead of 'int foo(void)' is left undetected
(even with -Wold-style-definition and -Wstrict-prototypes enabled).

Fix this by:
1) adding the type check to declare_argument() so that
   all parameters have a real type.
2) downgrade the diagnostic to a warning for K&R functions.

Fixes: 6f7aa5e84dacec8e27a8d70090bba26a1a1276de
Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

parse.c
@@ -2713,6 +2713,14 @@ static void declare_argument(struct symbol *sym, struct symbol *fn)
 		sparse_error(sym->pos, "no identifier for function argument");
 		return;
 	}
+	if (sym->ctype.base_type == &incomplete_ctype) {
+		sym->ctype.base_type = &int_ctype;
+
+		if (Wimplicit_int) {
+			sparse_error(sym->pos, "missing type declaration for parameter '%s'",
+				show_ident(sym->ident));
+		}
+	}
 	bind_symbol(sym, sym->ident, NS_SYMBOL);
 }
 
@@ -2804,7 +2812,7 @@ static void apply_k_r_types(struct symbol_list *argtypes, struct symbol *fn)
 				goto match;
 		} END_FOR_EACH_PTR(type);
 		if (Wimplicit_int) {
-			sparse_error(arg->pos, "missing type declaration for parameter '%s'",
+			warning(arg->pos, "missing type declaration for parameter '%s'",
 				show_ident(arg->ident));
 		}
 		type = alloc_symbol(arg->pos, SYM_NODE);

validation/asm-bad0.c
@@ -16,7 +16,7 @@ static void icons(void)
 	asm("template" : [out] "=r" (var): [in] string (0));
 }
 
-static void oexpr(oid)
+static void oexpr(void)
 {
 	asm("template" : [out] "=" (var[) : [in] "r" (0));
 }

validation/bad-type-twice0.c
@@ -7,7 +7,6 @@ static int foo(a)
  * check-name: bad-type-twice0
  *
  * check-error-start
-bad-type-twice0.c:3:16: error: non-scalar type in conditional:
-bad-type-twice0.c:3:16:    incomplete type a
+bad-type-twice0.c:1:16: error: missing type declaration for parameter 'a'
  * check-error-end
  */

validation/badtype2.c
@@ -18,6 +18,7 @@ badtype2.c:2:14: error: got bar
 badtype2.c:3:14: error: Expected ; at end of declaration
 badtype2.c:3:14: error: got foo
 badtype2.c:6:3: error: Trying to use reserved word 'switch' as identifier
+badtype2.c:6:11: error: missing type declaration for parameter 'p'
 badtype2.c:7:3: error: not in switch scope
 badtype2.c:10:1: error: Expected ; at the end of type declaration
 badtype2.c:10:1: error: got }

validation/badtype3.c
@@ -16,6 +16,7 @@ badtype3.c:2:18: warning: identifier list not in definition
 badtype3.c:2:24: error: Expected ) in function declarator
 badtype3.c:2:24: error: got ,
 badtype3.c:5:3: error: Trying to use reserved word 'while' as identifier
+badtype3.c:5:10: error: missing type declaration for parameter 'cur'
 badtype3.c:7:7: error: break/continue not in iterator scope
 badtype3.c:9:3: error: Trying to use reserved word 'return' as identifier
 badtype3.c:9:10: error: Expected ; at end of declaration

validation/implicit-KR-arg-type1.c
@@ -1,7 +1,13 @@
-int foo(a, b)
+static int foo(a, b)
 	int a;
 {
 	if (b)
+		return a;
+}
+
+static int bar(a)
+{
+	if (a)
 		return a;
 }
 
@@ -11,6 +17,7 @@ int foo(a, b)
  *
  * check-error-start
 implicit-KR-arg-type1.c:2:9: warning: non-ANSI definition of function 'foo'
-implicit-KR-arg-type1.c:1:12: error: missing type declaration for parameter 'b'
+implicit-KR-arg-type1.c:1:19: warning: missing type declaration for parameter 'b'
+implicit-KR-arg-type1.c:8:16: error: missing type declaration for parameter 'a'
  * check-error-end
  */