fbd81ed45d5889db28c9d60696048a97dc0ca8cb
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2020-11-26 22:38:21 +0100

    add testscases for some factorization of distributive operations
    Add some testcases for factorizations of:
    	(x * z) + (y * z) --> (x + y) * z
    	(x | z) & (y | z) --> (x & y) | z
    	(x & z) | (y & z) --> (x | y) & z
    	(x & z) ^ (y & z) --> (x ^ y) & z
    and
    	(x << s) | (y << s) --> ((x | y) << s)
    	and same for &, ^, LSR and ASR.
    
    Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

---
+++ validation/optim/fact-xor-shift.c
@@ -0,0 +1,27 @@
+typedef unsigned int uint;
+typedef   signed int sint;
+
+
+uint fact_xor_shl(uint a, uint b, uint s)
+{
+	return ((a << s) ^ (b << s)) == ((a ^ b) << s);
+}
+
+uint fact_xor_lsr(uint a, uint b, uint s)
+{
+	return ((a >> s) ^ (b >> s)) == ((a ^ b) >> s);
+}
+
+sint fact_xor_asr(sint a, sint b, sint s)
+{
+	return ((a >> s) ^ (b >> s)) == ((a ^ b) >> s);
+}
+
+/*
+ * check-name: fact-xor-shift
+ * check-command: test-linearize -Wno-decl $file
+ * check-known-to-fail
+ *
+ * check-output-ignore
+ * check-output-returns: 1
+ */
