e5e5f3e19ae18e182a670f1805e762520a1036ac
Dan Carpenter <dan.carpenter@oracle.com>
2021-01-13 13:06:30 +0300

    param_set: addresses can't be set
    To be honest, I'm hoping that this is old data which got into the database
    and remained there because of recursion...  :P  I haven't investigated as
    well as I should have.  Anyway, addresses can't be set.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- smatch_param_set.c
+++ smatch_param_set.c
@@ -238,6 +238,8 @@ static void print_return_value_param_helper(int return_id, char *return_ranges, 
 		param = get_param_key_from_sm(sm, NULL, &param_name);
 		if (param < 0 || !param_name)
 			continue;
+		if (param_name[0] == '&')
+			continue;
 		if (strcmp(param_name, "$") == 0 ||
 		    is_recursive_member(param_name) ||
 		    is_ignored_kernel_data(param_name)) {
