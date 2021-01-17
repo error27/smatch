error27/smatch

commit 4a5f616407e26efb67013f8267adef2d6e093bf1

cmp: canonicalize sext(x) cmpu C (with C >= SMAX)
A unsigned compare of a sign-extended value against a value
bigger than the original SMAX is equivalent to a signed
compare against 0.

Canonicalize to the signed compare against 0.

Note: ultimately both forms are just a test of the sign bit.

Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

simplify.c
@@ -1149,6 +1149,18 @@ static int simplify_compare_constant(struct instruction *insn, long long value)
 			return replace_pseudo(insn, &insn->src1, def->src);
 		}
 		switch (insn->opcode) {
+		case OP_SET_BE:
+			if (value >= sign_bit(osize)) {
+				replace_binop_value(insn, OP_SET_GE, 0);
+				return replace_pseudo(insn, &insn->src1, def->src);
+			}
+			break;
+		case OP_SET_A:
+			if (value >= sign_bit(osize)) {
+				replace_binop_value(insn, OP_SET_LT, 0);
+				return replace_pseudo(insn, &insn->src1, def->src);
+			}
+			break;
 		case OP_SET_LT: case OP_SET_LE:
 			if (value >= sign_bit(osize))
 				return replace_with_value(insn, 1);

validation/optim/cmp-sext-uimm.c
@@ -19,7 +19,6 @@ int sext_gtu_m3(int x) { return (sext(x) > (NEG - 3)) == (x < 0); }
 /*
  * check-name: cmp-sext-uimm
  * check-command: test-linearize -Wno-decl $file
- * check-known-to-fail
  *
  * check-output-ignore
  * check-output-returns: 1

commit 23756db02e8d8337075b377408af5093c0401de2

fix type of canonicalization of sext + unsigned compare
In commit 4a5f616407e2 ("cmp: canonicalize sext(x) cmpu C (with C >= SMAX)"),
the operand is replaced to avoid a sign extension but the corresponding
type was not updated. Fix this now.

Fixes: 4a5f616407e26efb67013f8267adef2d6e093bf1
Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

simplify.c
@@ -1225,12 +1225,14 @@ static int simplify_compare_constant(struct instruction *insn, long long value)
 		switch (insn->opcode) {
 		case OP_SET_BE:
 			if (value >= sign_bit(osize)) {
+				insn->itype = def->orig_type;
 				replace_binop_value(insn, OP_SET_GE, 0);
 				return replace_pseudo(insn, &insn->src1, def->src);
 			}
 			break;
 		case OP_SET_A:
 			if (value >= sign_bit(osize)) {
+				insn->itype = def->orig_type;
 				replace_binop_value(insn, OP_SET_LT, 0);
 				return replace_pseudo(insn, &insn->src1, def->src);
 			}