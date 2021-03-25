b204ead75fc624928e783da8b6658f3a8a06871c
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2021-03-26 00:41:22 +0100

    additional testcase for remove_merging_phisrc()
    Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

---
+++ validation/optim/multi-phisrc.c
@@ -0,0 +1,24 @@
+void fun(void);
+
+void foo(int p, int a)
+{
+	if (p == p) {
+		switch (p) {
+		case 0:
+			break;
+		case 1:
+			a = 0;
+		}
+	}
+	if (a)
+		fun();
+}
+
+/*
+ * check-name: multi-phisrc
+ * check-command: test-linearize -Wno-decl $file
+ * check-known-to-fail
+ *
+ * check-output-ignore
+ * check-output-excludes: phi
+ */
