7ae82c3a41c71c977e409dd20679bc4aec19961c
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2021-04-17 22:01:05 +0200

    add testcases for AND(x > 0, x <= C) --> x u<= C
    Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

---
+++ validation/optim/range-check2.c
@@ -0,0 +1,15 @@
+#define N	1024
+
+_Bool check_ok(int i)
+{
+	return (i >= 0 && i < N) == (((unsigned int)i) < N);
+}
+
+/*
+ * check-name: range-check2
+ * check-command: test-linearize -Wno-decl $file
+ * check-known-to-fail
+ *
+ * check-output-ignore
+ * check-output-returns: 1
+ */
