6df9a55396d94c92e5c5ccae385a38923e47dbf6
Dan Carpenter <dan.carpenter@oracle.com>
2021-08-31 12:39:10 +0300

    arrays: handle post op offsets
    Sometimes we have code that looks like this:
    
    	foo[idx++] = bar;
    
    But for the purposes of checking if the array offset is correct then it's
    nicer to treat that as:
    
    	foo[idx] = bar;
    
    The ++ will be handled later by other code.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- smatch_helper.c
+++ smatch_helper.c
@@ -700,7 +700,10 @@ struct expression *get_array_offset(struct expression *expr)
 		expr = strip_expr(expr->unop);
 	if (expr->type != EXPR_BINOP || expr->op != '+')
 		return NULL;
-	return strip_parens(expr->right);
+	expr = strip_parens(expr->right);
+	if (expr->type == EXPR_POSTOP)
+		expr = strip_parens(expr->unop);
+	return expr;
 }
 
 const char *show_state(struct smatch_state *state)
