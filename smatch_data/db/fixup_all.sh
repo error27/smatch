801b9efeb7696a44d8054b4acaeebcc926024730
Dan Carpenter <dan.carpenter@oracle.com>
2021-09-01 14:50:55 +0300

    db: keep the return states from strlen and sprintf
    Smatch is able to parse these correctly now.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- smatch_data/db/fixup_all.sh
+++ smatch_data/db/fixup_all.sh
@@ -3,10 +3,5 @@
 db_file=$1
 cat << EOF | sqlite3 $db_file
 
-delete from return_states where function = 'strlen';
-delete from return_states where function = 'strnlen';
-delete from return_states where function = 'sprintf';
-delete from return_states where function = 'snprintf';
-
 EOF
 
