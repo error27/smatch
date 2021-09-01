6e56c650800728ac3e1400c07e4a6e9b1bf15648
Dan Carpenter <dan.carpenter@oracle.com>
2021-09-01 14:52:10 +0300

    db/mtag_info.schema: add a UNIQUE constraint and add an index
    This is a speedup.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- smatch_data/db/build_early_index.sh
+++ smatch_data/db/build_early_index.sh
@@ -36,13 +36,11 @@ CREATE INDEX parameter_name_idx on parameter_name (function);
 CREATE INDEX str_idx on constraints (str);
 CREATE INDEX required_idx on constraints_required (data);
 CREATE INDEX mtag_about_idx on mtag_about (tag);
+CREATE INDEX mtag_info_idx on mtag_info (tag);
 CREATE INDEX mtag_data_idx on mtag_data (tag);
 CREATE INDEX mtag_map_idx1 on mtag_map (tag);
 CREATE INDEX mtag_map_idx2 on mtag_map (container);
 CREATE INDEX sink_index on sink_info (file, sink_name);
 
 EOF
-
-#CREATE INDEX type_size_idx on type_size (type);
-#CREATE INDEX type_val_idx on type_value (type);
 
