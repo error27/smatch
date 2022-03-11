5b636af659d306c518e339165aadae91a36d0588
Dan Carpenter <dan.carpenter@oracle.com>
2022-03-11 10:45:30 +0300

    clear_user_data: delete USER_PTR data
    This doesn't affect normal performance and it's been a while since I had
    to clear out all the user data from my test system.  But when we do then
    we need to clear out pointer info as well.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- smatch_data/db/clear_user_data.sh
+++ smatch_data/db/clear_user_data.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 echo "delete from caller_info where type = 8017; delete from return_states where type = 8017 or type = 9017;" | sqlite3 smatch_db.sqlite
+echo "delete from caller_info where type = 9018; delete from return_states where type = 9019;" | sqlite3 smatch_db.sqlite
+
 
 
