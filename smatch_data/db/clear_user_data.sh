3ef4ed38aca3febf4fb4c234fba20d5077958bdb
Dan Carpenter <dan.carpenter@oracle.com>
2022-04-21 19:33:46 +0300

    db/clear_user_data: clear out just one directory
    We recently had a bug where something was passing bad user data to crypto/
    We fixed the bug, but unfortunately crypto/ appears to Smatch as just an
    endless cycle of recursion.  Once bad user data gets into the recursive
    loop then fixing the caller will not silence the warning.
    
    So now you can do smatch_data/db/clear_user_data.sh crypto/ to clear it
    out.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- smatch_data/db/clear_user_data.sh
+++ smatch_data/db/clear_user_data.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-echo "delete from caller_info where type = 8017; delete from return_states where type = 8017 or type = 9017;" | sqlite3 smatch_db.sqlite
-echo "delete from caller_info where type = 9018; delete from return_states where type = 9019;" | sqlite3 smatch_db.sqlite
+dir=$1
+if [ "$dir" = "" ] ; then
+    echo "$0 all or dir/"
+    exit 1
+fi
 
+if [ "$dir" == "all" ] ; then
+    echo "delete from caller_info where (type = 8017 or type = 9018);" | sqlite3 smatch_db.sqlite
+    echo "delete from return_states where (type = 8017 or type = 9017 or type = 9018);" | sqlite3 smatch_db.sqlite
+else
+    echo "delete from caller_info where (type = 8017 or type = 9018) and file like '$dir%';" | sqlite3 smatch_db.sqlite
+    echo "delete from return_states where (type = 8017 or type = 9017 or type = 9018) and file like '$dir%';" | sqlite3 smatch_db.sqlite
+fi
 
 
