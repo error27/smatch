ff616825a060aef40673aeedc8f18e92fc3a2b48
Dan Carpenter <dan.carpenter@oracle.com>
2022-05-05 14:47:09 +0300

    constants: add bool_true/false
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- smatch_constants.h
+++ smatch_constants.h
@@ -9,6 +9,9 @@ static const sval_t err_min  = { .type = &int_ctype, .value = -4095 };
 static const sval_t err_max  = { .type = &int_ctype, .value = -1 };
 static const sval_t int_max  = { .type = &int_ctype, .value = INT_MAX };
 
+static const sval_t bool_false = { .type = &bool_ctype, .value = 0 };
+static const sval_t bool_true = { .type = &bool_ctype, .value = 1 };
+
 static const sval_t ptr_err_min = { .type = &ptr_ctype, .value = -4095 };
 static const sval_t ptr_err_max = { .type = &ptr_ctype, .value = -1 };
 static const sval_t ptr_null    = { .type = &ptr_ctype, .value = 0 };
