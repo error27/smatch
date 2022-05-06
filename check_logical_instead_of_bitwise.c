486d484cedf0fef0738568f5c18bf46fac9b9075
Dan Carpenter <dan.carpenter@oracle.com>
2022-05-06 16:39:10 +0300

    logical_instead_of_bitwise: add some more warnings
    The important difference is the more flexible check_is_mask().
    
    The problem is trying to figure out which code is bitwise and one trick
    that I tend to use a lot is assuming that 0-1 is boolean.  So, for example,
    if (foo > bar) is boolean or maybe a function returns 0-1 so that's
    probably boolean.
    
    One recent bug used "foo && BIT(0)".  The BIT(0) value is 1 but because of
    the left shift we know it's bitwise and not boolean.  I have instead
    added a check on the left hand side because sometimes that can be a
    comparison.
    
    Then this patch adds some other warnings as well.  But only the "should
    this be a bitwise negate mask?" ever triggers and I decided to disable that
    warning unless --spammy is enabled.  It doesn't trigger many warnings but
    I can't understand most of that code so I'm not sure if it's correct or
    not.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- check_logical_instead_of_bitwise.c
+++ check_logical_instead_of_bitwise.c
@@ -31,22 +31,56 @@ static int is_bitshift(struct expression *expr)
 	return 0;
 }
 
+static int warn_if_both_known_values(struct expression *expr)
+{
+	sval_t left, right;
+
+	if (get_macro_name(expr->pos))
+		return 0;
+
+	if (!get_value(expr->right, &left) ||
+	    !get_value(expr->left, &right))
+		return 0;
+
+	if ((left.value == 0 || left.value == 1) &&
+	    (right.value == 0 || right.value == 1))
+		return 0;
+
+	sm_warning("should this be a bitwise op?");
+	return 1;
+}
+
+static int check_is_mask(struct expression *expr)
+{
+	expr = strip_expr(expr);
+
+	if (expr->type != EXPR_BINOP)
+		return 0;
+	if (expr->op != SPECIAL_LEFTSHIFT &&
+	    expr->op != SPECIAL_RIGHTSHIFT &&
+	    expr->op != '|')
+		return 0;
+	if (implied_not_equal(expr, 0))
+		return 1;
+	return 0;
+}
+
 static void match_logic(struct expression *expr)
 {
-	sval_t sval;
+	struct range_list *rl;
 
 	if (expr->type != EXPR_LOGICAL)
 		return;
 
-	if (get_macro_name(expr->pos))
+	if (warn_if_both_known_values(expr))
 		return;
 
-	if (!get_value(expr->right, &sval)) {
-		if (!get_value(expr->left, &sval))
-			return;
-	}
+	if (!check_is_mask(expr->right))
+		return;
 
-	if (sval.value == 0 || sval.value == 1)
+	/* check for logic, by looking for bool */
+	get_absolute_rl(expr->left, &rl);
+	if (rl_min(rl).value >= 0 && rl_max(rl).value <= 1)
 		return;
 
 	sm_warning("should this be a bitwise op?");
@@ -63,10 +97,62 @@ static void match_assign(struct expression *expr)
 		sm_warning("should this be a bitwise op?");
 }
 
+static int is_bool(struct expression *expr)
+{
+	struct range_list *rl;
+
+	get_absolute_rl(expr, &rl);
+
+	if (rl_min(rl).value != 0 && rl_min(rl).value != 1)
+		return 0;
+	if (rl_max(rl).value != 0 && rl_max(rl).value != 1)
+		return 0;
+
+	return 1;
+}
+
+static void match_assign_mask(struct expression *expr)
+{
+	struct expression *right;
+
+	if (!option_spammy)
+		return;
+
+	if (expr->op != SPECIAL_AND_ASSIGN &&
+	    expr->op != SPECIAL_OR_ASSIGN)
+		return;
+
+	if (is_bool(expr->left))
+		return;
+
+	right = strip_expr(expr->right);
+	if (right->type != EXPR_PREOP || right->op != '!')
+		return;
+	if (is_bool(right->unop))
+		return;
+	sm_warning("should this be a bitwise negate mask?");
+}
+
+static void match_logical_negate(struct expression *expr)
+{
+	sval_t sval;
+
+	if (expr->op != '!')
+		return;
+	expr = strip_expr(expr->unop);
+	if (expr->type != EXPR_BINOP || expr->op != SPECIAL_LEFTSHIFT)
+		return;
+	if (!get_value(expr->left, &sval) || sval.value != 1)
+		return;
+	sm_warning("potential ! vs ~ typo");
+}
+
 void check_logical_instead_of_bitwise(int id)
 {
 	my_id = id;
 
 	add_hook(&match_logic, LOGIC_HOOK);
+	add_hook(&match_logical_negate, OP_HOOK);
 	add_hook(&match_assign, ASSIGNMENT_HOOK);
+	add_hook(&match_assign_mask, ASSIGNMENT_HOOK);
 }
