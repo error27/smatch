f9d6242a9f826bf3363331287c4254b6dfda714b
Christophe JAILLET <christophe.jaillet@wanadoo.fr>
2022-05-26 08:44:39 +0200

    check_freeing_devm: Report the correct function name that triggered the warning
    The warning message has a hard-coded 'kfree' but another function can
    trigger this warning.
    So, report the correct function name to be less puzzling.
    
    Signed-off-by: Christophe JAILLET <christophe.jaillet@wanadoo.fr>
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- check_freeing_devm.c
+++ check_freeing_devm.c
@@ -59,7 +59,7 @@ static void match_free_func(const char *fn, struct expression *expr, void *_arg)
 	if (!get_state_expr(my_id, arg_expr))
 		return;
 	name = expr_to_str(arg_expr);
-	sm_warning("passing devm_ allocated variable to kfree. '%s'", name);
+	sm_warning("passing devm_ allocated variable to %s. '%s'", fn, name);
 	free_string(name);
 }
 
