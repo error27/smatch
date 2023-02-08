f9cf35cf11591a73bca70647cba840ab2a8c0ed0
Dan Carpenter <error27@gmail.com>
2023-02-08 14:33:43 +0300

    db/clear_user_data: clean user data more thoroughly
    There is a bunch of new user data related stuff to clear out now.
    
    Signed-off-by: Dan Carpenter <error27@gmail.com>

--- smatch_data/db/clear_user_data.sh
+++ smatch_data/db/clear_user_data.sh
@@ -6,12 +6,15 @@ if [ "$dir" = "" ] ; then
     exit 1
 fi
 
+USER_TYPE="(type = 8017 or (type >= 9017 and type <= 9019))"
+HOST_TYPE="(type >= 7016 and type <= 7019)"
+
 if [ "$dir" == "all" ] ; then
-    echo "delete from caller_info where (type = 8017 or type = 9018);" | sqlite3 smatch_db.sqlite
-    echo "delete from return_states where (type = 8017 or type = 9017 or type = 9018);" | sqlite3 smatch_db.sqlite
+    echo "delete from caller_info where ${USER_TYPE} or ${HOST_TYPE};" | sqlite3 smatch_db.sqlite
+    echo "delete from return_states where ${USER_TYPE} or ${HOST_TYPE};" | sqlite3 smatch_db.sqlite
 else
-    echo "delete from caller_info where (type = 8017 or type = 9018) and file like '$dir%';" | sqlite3 smatch_db.sqlite
-    echo "delete from return_states where (type = 8017 or type = 9017 or type = 9018) and file like '$dir%';" | sqlite3 smatch_db.sqlite
+    echo "delete from caller_info where ${USER_TYPE} or ${HOST_TYPE} and file like '$dir%';" | sqlite3 smatch_db.sqlite
+    echo "delete from return_states where ${USER_TYPE} or ${HOST_TYPE} or type = 9018) and file like '$dir%';" | sqlite3 smatch_db.sqlite
 fi
 
 
