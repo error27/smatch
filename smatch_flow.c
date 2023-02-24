e144ac13f087afbf455361fb1fb65e3e11bc12c6
Dan Carpenter <error27@gmail.com>
2023-02-24 19:06:23 +0300

    flow: revert previous commit
    Ugh.  The ordering of this is tricky.  To be honest, none of this is
    correct for functions...
    
    The reason that I can't commit this is because the
    __fake_struct_member_assignments clear out old user data.  So if there is
    a USER_DATA_SET which sets a -1 $->foo to user data then the fake
    assignments clear it out.  That's kind of useless.
    
    But really this stuff is totally broken because what about if one return
    path sets the struct members to user data but the others don't?  It doesn't
    work to just do one assignment.
    
    Signed-off-by: Dan Carpenter <error27@gmail.com>

--- smatch_flow.c
+++ smatch_flow.c
@@ -642,12 +642,14 @@ void __split_expr(struct expression *expr)
 		else
 			__pass_to_client(expr, ASSIGNMENT_HOOK);
 
+		// FIXME: the ordering of this is tricky
+		__fake_struct_member_assignments(expr);
+
 		/* Re-examine ->right for inlines.  See the commit message */
 		right = strip_expr(expr->right);
 		if (expr->op == '=' && right->type == EXPR_CALL)
 			__pass_to_client(expr, CALL_ASSIGNMENT_HOOK);
 
-		__fake_struct_member_assignments(expr);
 
 after_assign:
 		if (get_macro_name(right->pos) &&
