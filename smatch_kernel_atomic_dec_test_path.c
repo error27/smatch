b5995120ba3434aa445bebf6bdb98ca9c707cc35
Dan Carpenter <dan.carpenter@linaro.org>
2023-10-06 12:57:27 +0300

    atomic_dec_test_path: hack around assignments
    Normally this code looks like:
    
    	if (refcount_dec_and_test(ref))
    		free();
    
    But sometimes what happens is test people do:
    
    	done = refcount_dec_and_test(ref);
    	if (done)
    		free();
    
    The right way to fix this is to probably make a list of dec_and_test()
    functions and that true means done.  But instead of that, we're just going
    to consider everything after the refcount_dec_and_test() on the
    dec_and_test() path regardless of the return value.  This will probably
    work fine in real life.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@linaro.org>

--- smatch_kernel_atomic_dec_test_path.c
+++ smatch_kernel_atomic_dec_test_path.c
@@ -29,7 +29,11 @@ static void handle_test_functions(struct expression *expr)
 	struct statement *stmt;
 	int count = 0;
 
-	if (expr->type != EXPR_CALL ||
+	while (expr && expr->type == EXPR_ASSIGNMENT && expr->op == '=')
+		expr = strip_expr(expr->right);
+
+	if (!expr ||
+	    expr->type != EXPR_CALL ||
 	    expr->fn->type != EXPR_SYMBOL ||
 	    !expr->fn->symbol_name)
 		return;
@@ -42,9 +46,25 @@ static void handle_test_functions(struct expression *expr)
 			break;
 	}
 
+	/*
+	 * FIXME: This doesn't work for assignments like:
+	 *
+	 * free = dec_and_test(foo);
+	 * if (free)
+	 *	free_thing(x);
+	 *
+	 * Maybe we should just say everything is on an atomic_dec path at this
+	 * point.  That probably works fine.  The other solution would be to
+	 * store the variable and look up the state of it or something...
+	 *
+	 */
+
 	stmt = expr_get_parent_stmt(expr);
-	if (!stmt || stmt->type != STMT_IF)
+	if (!stmt || stmt->type != STMT_IF) {
+		/* See above comment.  I decided to go with the easy option */
+		set_state(my_id, "dec_path", NULL, &zero_path);
 		return;
+	}
 
 	set_true_false_states(my_id, "dec_path", NULL, &zero_path, NULL);
 }
