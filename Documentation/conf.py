3f9b43dcf4975dfc4464d1f0c6053f493018d5bd
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2024-01-04 23:08:36 +0100

    doc: update conf.py for more recent version of sphinx
    Sphinx versions older than 1.7 don't need and don't support 'html_context'.
    
    So, set 1.8 as the minimal version and remove 'html_context' from the config.
    
    Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -21,7 +21,7 @@ import datetime
 
 # -- General configuration ------------------------------------------------
 
-needs_sphinx = '1.7'
+needs_sphinx = '1.8'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -114,12 +114,6 @@ except:
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['sphinx/static']
-
-html_context = {
-    'css_files': [
-        '_static/theme_overrides.css',
-    ],
-}
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
