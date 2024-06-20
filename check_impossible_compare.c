59222b495ff35f0247bf37b0eebcf86e6bbe583e
Dan Carpenter <dan.carpenter@linaro.org>
2024-06-20 12:16:13 +0300

    impossible_compare: silence some false positives
    I think this is to silence warnings on 32 bit where it says:
    
    	if (u32_var > UINT_MAX)
    
    I wrote this some time ago... :(  I wish it were more perfect.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@linaro.org>

--- check_impossible_compare.c
+++ check_impossible_compare.c
@@ -20,6 +20,10 @@
 
 static int my_id;
 
+static const char *okay_macros[] = {
+	"ULONG_MAX",
+};
+
 static const char *kernel_macros[] = {
 	"DP_FFE_PRESET_MAX_LEVEL",
 	"FSMC_THOLD_MASK",
@@ -39,13 +43,11 @@ static const char *kernel_macros[] = {
 
 static const char **allowed_macros;
 
-static bool is_lt_zero(struct expression *expr)
+static bool is_lt_zero(int op, sval_t known)
 {
-	if (expr->type != EXPR_COMPARE)
+	if (op != '<' && op != SPECIAL_UNSIGNED_LT)
 		return false;
-	if (expr->op != '<' && expr->op != SPECIAL_UNSIGNED_LT)
-		return false;
-	if (!expr_is_zero(expr->right))
+	if (known.value != 0)
 		return false;
 	return true;
 }
@@ -95,6 +97,17 @@ static bool is_u64_vs_ulongmax(struct expression *expr)
 	return true;
 }
 
+static bool is_long_vs_UINTMAX(struct range_list *rl, int op, sval_t known)
+{
+	if (op != '>' && op != SPECIAL_UNSIGNED_GT)
+		return false;
+	if (rl_type(rl) != &long_ctype && rl_type(rl) != &ulong_ctype)
+		return false;
+	if (known.value == UINT_MAX)
+		return true;
+	return false;
+}
+
 static bool is_allowed_impossible_limit(struct expression *expr)
 {
 	char *macro;
@@ -120,17 +133,12 @@ static bool is_allowed_impossible_limit(struct expression *expr)
 static void match_condition(struct expression *expr)
 {
 	struct symbol *type;
-	sval_t known;
-	sval_t min, max;
-	struct range_list *rl_left_orig, *rl_right_orig;
-	struct range_list *rl_left, *rl_right;
+	sval_t known, dummy;
+	struct range_list *var_rl, *known_rl, *rl_right, *rl_left;
 	char *name;
+	int op;
 
 	if (expr->type != EXPR_COMPARE)
-		return;
-
-	/* handled by check_unsigned_lt_zero.c */
-	if (is_lt_zero(expr))
 		return;
 
 	type = get_type(expr);
@@ -140,37 +148,39 @@ static void match_condition(struct expression *expr)
 	if (get_macro_name(expr->pos))
 		return;
 
-	/* check that one and only one side is known */
-	if (get_value(expr->left, &known)) {
-		if (get_value(expr->right, &known))
+	op = expr->op;
+	if (get_value(expr->right, &known)) {
+		/* check that one and only one side is known */
+		if (get_value(expr->left, &dummy))
 			return;
-		rl_left_orig = alloc_rl(known, known);
-		rl_left = cast_rl(type, rl_left_orig);
 
-		min = sval_type_min(get_type(expr->right));
-		max = sval_type_max(get_type(expr->right));
-		rl_right_orig = alloc_rl(min, max);
-		rl_right = cast_rl(type, rl_right_orig);
-	} else if (get_value(expr->right, &known)) {
-		rl_right_orig = alloc_rl(known, known);
-		rl_right = cast_rl(type, rl_right_orig);
-
-		min = sval_type_min(get_type(expr->left));
-		max = sval_type_max(get_type(expr->left));
-		rl_left_orig = alloc_rl(min, max);
-		rl_left = cast_rl(type, rl_left_orig);
+		var_rl = alloc_whole_rl(get_type(expr->left));
+	} else if (get_value(expr->left, &known)) {
+		op = flip_comparison(op);
+		var_rl = alloc_whole_rl(get_type(expr->right));
 	} else {
 		return;
 	}
+	known = sval_cast(type, known);
+	known_rl = alloc_rl(known, known);
+	var_rl = cast_rl(type, var_rl);
 
-	if (possibly_true_rl(rl_left, expr->op, rl_right))
+	/* handled by check_unsigned_lt_zero.c */
+	if (is_lt_zero(op, known))
+		return;
+
+	if (possibly_true_rl(var_rl, op, known_rl))
 		return;
 	if (is_u64_vs_ulongmax(expr))
+		return;
+	if (is_long_vs_UINTMAX(var_rl, op, known))
 		return;
 	if (is_allowed_impossible_limit(expr->right))
 		return;
 
 	name = expr_to_str(expr);
+	get_absolute_rl(expr->left, &rl_left);
+	get_absolute_rl(expr->right, &rl_right);
 	sm_warning("impossible condition '(%s) => (%s %s %s)'", name,
 		   show_rl(rl_left), show_special(expr->op), show_rl(rl_right));
 	free_string(name);
@@ -180,6 +190,7 @@ void check_impossible_compare(int id)
 {
 	my_id = id;
 
+	allowed_macros = okay_macros;
 	if (option_project == PROJ_KERNEL)
 		allowed_macros = kernel_macros;
 
