f05a342f92ae9b0fb701579dfc83ccb78d6755fe
Dan Carpenter <dan.carpenter@linaro.org>
2024-10-10 10:44:45 +0300

    locking: update all lock_sock() functions
    I don't understand the lock_sock() stuff but none of them lock what this
    has so update them to say they lock the sk.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@linaro.org>

--- smatch_locking.c
+++ smatch_locking.c
@@ -403,11 +403,11 @@ static struct lock_info lock_table[] = {
 
 	{"gfs2_trans_begin", LOCK, sem, 0, "&$->sd_log_flush_lock", &int_zero, &int_zero},
 
-	{"lock_sock",        LOCK,   spin_lock, 0, "&$->sk_lock.slock"},
-	{"lock_sock_nested", LOCK,   spin_lock, 0, "&$->sk_lock.slock"},
-	{"lock_sock_fast",   LOCK,   spin_lock, 0, "&$->sk_lock.slock"},
+	{"lock_sock",        LOCK,   spin_lock, 0, "$"},
+	{"lock_sock_nested", LOCK,   spin_lock, 0, "$"},
+	{"lock_sock_fast",   LOCK,   spin_lock, 0, "$"},
 	{"__lock_sock",      LOCK,   spin_lock, 0, "$"},
-	{"release_sock",     UNLOCK, spin_lock, 0, "&$->sk_lock.slock"},
+	{"release_sock",     UNLOCK, spin_lock, 0, "$"},
 	{"__release_sock",   UNLOCK, spin_lock, 0, "$"},
 
 	{"lock_task_sighand", LOCK,  spin_lock, 0, "&$->sighand->siglock", &valid_ptr_min_sval, &valid_ptr_max_sval},
