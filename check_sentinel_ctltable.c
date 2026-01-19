02d9aef9071946ecd8c786f803ec45d4ab3b6ef6
Dan Carpenter <dan.carpenter@linaro.org>
2026-01-19 09:11:00 +0300

    sentinal_ctltable: silence false positives
    Turn this off for the zero day bot because it produces a lot of false
    positives.
    
    Also, this should be looking at conditions and not just expressions.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@linaro.org>

--- check_sentinel_ctltable.c
+++ check_sentinel_ctltable.c
@@ -10,35 +10,37 @@ static struct non_null_ctltable_elems non_null_elems[] = {
 	{.name = "->proc_handler", .len = 14},
 };
 
-static int match_ctl_table_array_sentinel(struct expression *expr)
+static void match_ctl_table_array_sentinel(struct expression *expr)
 {
 	char *member_name = NULL;
 
 	if (!expr)
-		return 0;
+		return;
 
 	member_name = get_member_name(expr);
 	if (!member_name)
-		return 0;
+		return;
 
 	if (strncmp(member_name, "(struct ctl_table)", 18) != 0)
-		return 0;
+		return;
 
 	for (int i = 0 ; i < ARRAY_SIZE(non_null_elems) ; ++i) {
 		if (strncmp(member_name + 18, non_null_elems[i].name, non_null_elems[i].len) == 0) {
 			sm_warning ("(struct ctl_table)%s cannot be NULL. Expression : %s",
 				    non_null_elems[i].name, expr_to_str(expr));
-			return 0;
+			return;
 		}
 	}
 
-	return 0;
+	return;
 }
 
 void check_sentinel_ctltable(int id)
 {
 	if (option_project != PROJ_KERNEL)
 		return;
+	if (!option_spammy)
+		return;
 
-	add_hook(&match_ctl_table_array_sentinel, EXPR_HOOK);
+	add_hook(&match_ctl_table_array_sentinel, CONDITION_HOOK);
 }
