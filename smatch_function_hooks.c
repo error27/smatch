4c381b62f2214fd0c878a631233c47a3fe170715
Dan Carpenter <error27@gmail.com>
2026-05-14 17:55:03 +0300

    function_hooks: avoid pointless set_state
    No point in setting the state to what it already is.
    
    Signed-off-by: Dan Carpenter <error27@gmail.com>

--- smatch_function_hooks.c
+++ smatch_function_hooks.c
@@ -934,7 +934,7 @@ static bool fake_a_param_assignment(struct expression *expr, const char *ret_str
 		faked = get_extra_state(left);
 		if (estate_rl(faked)) {
 			rl = rl_intersection(estate_rl(faked), estate_rl(orig));
-			if (rl)
+			if (rl && !rl_equiv(rl, estate_rl(faked)))
 				set_extra_expr_nomod(left, alloc_estate_rl(rl));
 		}
 	}
