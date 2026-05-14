a91d411f2ad05cd7eded57f60629c8bd72d5f139
Dan Carpenter <error27@gmail.com>
2026-05-14 14:48:38 +0300

    essa: handle merges better
    I think just testing == is enough.  I don't think we need to do a
    strcmp().
    
    Signed-off-by: Dan Carpenter <error27@gmail.com>

--- smatch_estate.c
+++ smatch_estate.c
@@ -69,6 +69,8 @@ struct smatch_state *merge_estates(struct smatch_state *s1, struct smatch_state 
 
 	if (estate_new(s1) || estate_new(s2))
 		estate_set_new(tmp);
+	if (get_essa(s1) == get_essa(s2))
+		set_essa(tmp, get_essa(s1));
 
 	return tmp;
 }
