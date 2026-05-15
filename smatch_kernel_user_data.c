52bc1cd55fef3eb48372b4fcf8383664658040f4
Dan Carpenter <error27@gmail.com>
2026-05-15 10:49:27 +0300

    kernel_user_data: is_user_safe() doesn't apply to inlines
    We're marking certain expression as not holding user data, but that
    doesn't work for inlines where it depends on the caller.
    
    Signed-off-by: Dan Carpenter <error27@gmail.com>

--- smatch_kernel_user_data.c
+++ smatch_kernel_user_data.c
@@ -920,11 +920,15 @@ static bool is_ptr_subtract(struct expression *expr)
 
 static void set_user_safe(struct expression *expr)
 {
+	if (__inline_fn)
+		return;
 	expr->user_safe |= (1 << pass_cnt);
 }
 
 static bool is_user_safe(struct expression *expr)
 {
+	if (__inline_fn)
+		return false;
 	return (expr->user_safe & (1 << pass_cnt));
 }
 
