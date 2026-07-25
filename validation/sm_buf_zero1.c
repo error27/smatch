bf2903cf3d13c179c646a3cd3a687cb8c2eb1962
Dan Carpenter <error27@gmail.com>
2026-07-25 17:28:03 +0300

    validation/sm_buf_zero: update the check
    This makes the check more interesting and complicated to reflect the
    changes to ssa_pointer.  :)
    
    Signed-off-by: Dan Carpenter <error27@gmail.com>

--- validation/sm_buf_zero1.c
+++ validation/sm_buf_zero1.c
@@ -5,27 +5,42 @@ struct foo {
 	int a, b, c;
 };
 
-int main(int x)
+int frob(void);
+
+void func(struct foo *p, struct foo *q)
 {
 	struct foo foo;
-	struct foo *p;
+
+	if (frob())
+		p = &foo;
+	else
+		p = NULL;
+
+	q = frob() ? &foo : NULL;
+
+	__smatch_ssa_pointer(&foo);
 
 	memset(&foo, 0, sizeof(foo));
-	p = &foo;
 
-	__smatch_implied(p->a);
+	__smatch_ssa(foo.a);
+	__smatch_ssa(p->a);
+	__smatch_ssa(p);
+
 	__smatch_implied(foo.a);
-
-	return 0;
+	__smatch_implied(p->a);
+	__smatch_implied(q->a);
 }
-
 
 /*
  * check-name: smatch: buf zero #1
  * check-command: smatch -I.. sm_buf_zero1.c
  *
  * check-output-start
-sm_buf_zero1.c:16 main() implied: p->a = '0'
-sm_buf_zero1.c:17 main() implied: foo.a = '0'
+sm_buf_zero1.c:25 func() name ssa_name: 'foo.a => &foo{0}->a'
+sm_buf_zero1.c:26 func() name ssa_name: 'p->a => &foo{0}->a'
+sm_buf_zero1.c:27 func() name ssa_name: 'p => &foo{0}'
+sm_buf_zero1.c:29 func() implied: foo.a = '0'
+sm_buf_zero1.c:30 func() implied: p->a = '0'
+sm_buf_zero1.c:31 func() implied: q->a = '0'
  * check-output-end
  */
